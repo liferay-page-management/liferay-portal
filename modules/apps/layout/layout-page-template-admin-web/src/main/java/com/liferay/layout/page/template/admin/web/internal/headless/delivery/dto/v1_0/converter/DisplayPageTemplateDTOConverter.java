b921d36129391ae7c4d92f0692d702466e8af984
Rubén Pulido <ruben.pulido@liferay.com>
2020-05-04 20:27:36 +0200

    LPS-112929 Use contentSubtypeClassTypeId instead of contentSubtypeName in DisplayPageTemplateDTOConverter

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/converter/DisplayPageTemplateDTOConverter.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/converter/DisplayPageTemplateDTOConverter.java
@@ -14,16 +14,8 @@
 
 package com.liferay.layout.page.template.admin.web.internal.headless.delivery.dto.v1_0.converter;
 
-import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
-import com.liferay.asset.kernel.model.AssetRendererFactory;
-import com.liferay.asset.kernel.model.ClassType;
-import com.liferay.asset.kernel.model.ClassTypeReader;
 import com.liferay.headless.delivery.dto.v1_0.DisplayPageTemplate;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
 
 /**
  * @author Rubén Pulido
@@ -35,50 +27,22 @@ public class DisplayPageTemplateDTOConverter {
 
 		return new DisplayPageTemplate() {
 			{
-				contentSubtypeName = _getClassTypeName(layoutPageTemplateEntry);
 				contentTypeClassName = layoutPageTemplateEntry.getClassName();
 				name = layoutPageTemplateEntry.getName();
+
+				setContentSubtypeClassTypeId(
+					() -> {
+						long classTypeId =
+							layoutPageTemplateEntry.getClassTypeId();
+
+						if (classTypeId == 0) {
+							return null;
+						}
+
+						return classTypeId;
+					});
 			}
 		};
 	}
-
-	private static String _getClassTypeName(
-		LayoutPageTemplateEntry layoutPageTemplateEntry) {
-
-		AssetRendererFactory assetRendererFactory =
-			AssetRendererFactoryRegistryUtil.getAssetRendererFactoryByClassName(
-				layoutPageTemplateEntry.getClassName());
-
-		if ((assetRendererFactory == null) ||
-			!assetRendererFactory.isSupportsClassTypes()) {
-
-			return null;
-		}
-
-		ClassTypeReader classTypeReader =
-			assetRendererFactory.getClassTypeReader();
-
-		ClassType classType = null;
-
-		try {
-			classType = classTypeReader.getClassType(
-				layoutPageTemplateEntry.getClassTypeId(),
-				LocaleUtil.getSiteDefault());
-		}
-		catch (PortalException portalException) {
-			if (_log.isWarnEnabled()) {
-				_log.warn("Unable to get class type", portalException);
-			}
-		}
-
-		if (classType == null) {
-			return null;
-		}
-
-		return classType.getName();
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		DisplayPageTemplateDTOConverter.class);
 
 }
