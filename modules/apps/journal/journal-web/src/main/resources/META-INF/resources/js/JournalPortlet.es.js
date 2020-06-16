38eeb224061cd23c8ff369ebac1580509403bf0a
Victor Galan <victor.galan@liferay.com>
2020-06-16 12:45:26 +0200

    LPS-115491 Pass the initial defaultLanguageId as context instead of firing an event from ddm_form

--- modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/JournalPortlet.es.js
+++ modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/JournalPortlet.es.js
@@ -16,6 +16,7 @@ import {AOP} from 'frontend-js-web';
 import PortletBase from 'frontend-js-web/liferay/PortletBase.es';
 import {delegate, on} from 'metal-dom';
 import {EventHandler} from 'metal-events';
+import {Config} from 'metal-state';
 
 const ACTION_INPUT_NAME = 'javax-portlet-action';
 
@@ -66,15 +67,13 @@ class JournalPortlet extends PortletBase {
 			);
 		}
 
-		this.defaultLanguageId = themeDisplay.getDefaultLanguageId();
-
 		this._localeChangedHandler = Liferay.after(
 			'inputLocalized:localeChanged',
 			this._onLocaleChange.bind(this)
 		);
 
-		this._defaultLocaleChangedHandler = Liferay.after(
-			['inputLocalized:defaultLocaleChanged', 'ddm:default-locale-sync'],
+		this._localeChangedHandler = Liferay.after(
+			'inputLocalized:defaultLocaleChanged',
 			this._onDefaultLocaleChange.bind(this)
 		);
 
@@ -94,7 +93,6 @@ class JournalPortlet extends PortletBase {
 	detached() {
 		this._eventHandler.removeAllListeners();
 		this._localeChangedHandler.detach();
-		this._defaultLocaleChangedHandler.detach();
 	}
 
 	/**
@@ -127,9 +125,6 @@ class JournalPortlet extends PortletBase {
 		if (event.item) {
 			this.defaultLanguageId = event.item.getAttribute('data-value');
 		}
-		else if (event.defaultLanguageId) {
-			this.defaultLanguageId = event.defaultLanguageId;
-		}
 	}
 
 	/**
@@ -137,19 +132,18 @@ class JournalPortlet extends PortletBase {
 	 * @param {Event} event
 	 */
 	_onLocaleChange(event) {
-		const defaultLanguageId = this.defaultLanguageId;
 		const selectedLanguageId = event.item.getAttribute('data-value');
 
 		if (selectedLanguageId) {
 			this._updateLocalizableInput(
 				'descriptionMapAsXML',
-				defaultLanguageId,
+				this.defaultLanguageId,
 				selectedLanguageId
 			);
 
 			this._updateLocalizableInput(
 				'titleMapAsXML',
-				defaultLanguageId,
+				this.defaultLanguageId,
 				selectedLanguageId
 			);
 
@@ -325,6 +319,10 @@ class JournalPortlet extends PortletBase {
 		}
 	}
 }
+
+JournalPortlet.STATE = {
+	defaultLanguageId: Config.string(),
+};
 
 export {JournalPortlet};
 export default JournalPortlet;
