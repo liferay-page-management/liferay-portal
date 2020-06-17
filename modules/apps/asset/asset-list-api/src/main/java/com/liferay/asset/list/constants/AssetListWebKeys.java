97d2596193290b9c581adf3e8725855bbf89ada1
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-06-17 12:48:52 +0200

    LPS-115536 Extract logic related to the content of an asset list to a new display context, and adapt code

--- modules/apps/asset/asset-list-api/src/main/java/com/liferay/asset/list/constants/AssetListWebKeys.java
+++ modules/apps/asset/asset-list-api/src/main/java/com/liferay/asset/list/constants/AssetListWebKeys.java
@@ -19,6 +19,9 @@ package com.liferay.asset.list.constants;
  */
 public class AssetListWebKeys {
 
+	public static final String ASSET_LIST_CONTENT_DISPLAY_CONTEXT =
+		"ASSET_LIST_CONTENT_DISPLAY_CONTEXT";
+
 	public static final String ASSET_LIST_DISPLAY_CONTEXT =
 		"ASSET_LIST_DISPLAY_CONTEXT";
 
@@ -28,6 +31,5 @@ public class AssetListWebKeys {
 		"EDIT_ASSET_LIST_DISPLAY_CONTEXT";
 
 	public static final String ITEM_SELECTOR = "ITEM_SELECTOR";
-
 
 }
