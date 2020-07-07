b1fe3f8907382eb0bca6ec01aede639a0cec7b59
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-07-07 12:35:17 +0100

    LPS-116534 SF - No needed

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -1645,7 +1645,7 @@ public class ContentPageEditorDisplayContext {
 			).put(
 				"title", HtmlUtil.escape(portletItem.getName())
 			).put(
-				"used", _isUsed(portlet, themeDisplay.getPlid())
+				"used", _isUsed(portlet)
 			).build()
 		).collect(
 			Collectors.toList()
@@ -1708,7 +1708,7 @@ public class ContentPageEditorDisplayContext {
 				PortalUtil.getPortletTitle(
 					portlet, servletContext, themeDisplay.getLocale())
 			).put(
-				"used", _isUsed(portlet, themeDisplay.getPlid())
+				"used", _isUsed(portlet)
 			).build()
 		).collect(
 			Collectors.toList()
@@ -1926,14 +1926,14 @@ public class ContentPageEditorDisplayContext {
 		return false;
 	}
 
-	private boolean _isUsed(Portlet portlet, long plid) {
+	private boolean _isUsed(Portlet portlet) {
 		if (portlet.isInstanceable()) {
 			return false;
 		}
 
 		long count =
 			PortletPreferencesLocalServiceUtil.getPortletPreferencesCount(
-				PortletKeys.PREFS_OWNER_TYPE_LAYOUT, plid,
+				PortletKeys.PREFS_OWNER_TYPE_LAYOUT, themeDisplay.getPlid(),
 				PortletIdCodec.encode(
 					portlet.getPortletId(), String.valueOf(CharPool.NUMBER_0)));
 
