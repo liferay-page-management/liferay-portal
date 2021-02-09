ccfa96d37d4a7307a4aeb77fb76b3909e45489e8
Cristina González Castellano <cristina.gonzalez@liferay.com>
2021-02-09 12:32:55 +0100

    LPS-124170 New upgrade process that add the column typeSettings to the table SegmentsExperience.

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/upgrade/SegmentsServiceUpgrade.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/upgrade/SegmentsServiceUpgrade.java
@@ -15,7 +15,6 @@
 package com.liferay.segments.internal.upgrade;
 
 import com.liferay.counter.kernel.service.CounterLocalService;
-import com.liferay.portal.kernel.upgrade.UpgradeCTModel;
 import com.liferay.portal.kernel.upgrade.UpgradeMVCCVersion;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.segments.internal.upgrade.v2_0_0.UpgradeSchema;
@@ -57,12 +56,7 @@ public class SegmentsServiceUpgrade implements UpgradeStepRegistrator {
 			"2.1.0", "2.2.0",
 			new com.liferay.segments.internal.upgrade.v2_2_0.UpgradeSchema());
 
-		registry.register(
-			"2.2.0", "2.3.0",
-			new UpgradeCTModel(
-				"SegmentsEntry", "SegmentsEntryRel", "SegmentsEntryRole",
-				"SegmentsExperience", "SegmentsExperiment",
-				"SegmentsExperimentRel"));
+		registry.register("2.3.0", "2.4.0", new UpgradeSchema());
 	}
 
 	@Reference
