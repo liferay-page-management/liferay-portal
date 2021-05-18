0de9df915ac79b265feca6bb89eabc0c81b7ce7c
ricardocousolr <ricardo.couso@liferay.com>
2021-05-18 13:27:16 +0200

    LPS-130057 Add test to check unpublished content pages can't be publicly searched

---
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/search/test/LayoutPublishedSearchTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.search.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.layout.util.BaseLayoutSearchTestCase;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.search.IndexerRegistry;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.search.test.util.IndexerFixture;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Ricardo Couso
+ */
+@RunWith(Arquillian.class)
+public class LayoutPublishedSearchTest extends BaseLayoutSearchTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		setUpLayoutFixture();
+
+		setUpLayoutIndexerFixture();
+	}
+
+	@Test
+	public void testPublishedPageSearch() throws PortalException {
+		String name = RandomTestUtil.randomString();
+
+		Layout layout = layoutFixture.createLayout(name);
+
+		layoutIndexerFixture.searchNoOne(name);
+
+		publishLayout(layout);
+
+		layoutIndexerFixture.searchOnlyOne(name);
+	}
+
+	protected void setUpLayoutFixture() {
+		layoutFixture = new LayoutFixture(_group);
+	}
+
+	protected void setUpLayoutIndexerFixture() {
+		layoutIndexerFixture = new IndexerFixture<>(Layout.class);
+	}
+
+	@Inject
+	protected IndexerRegistry indexerRegistry;
+
+	protected LayoutFixture layoutFixture;
+	protected IndexerFixture<Layout> layoutIndexerFixture;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+}
