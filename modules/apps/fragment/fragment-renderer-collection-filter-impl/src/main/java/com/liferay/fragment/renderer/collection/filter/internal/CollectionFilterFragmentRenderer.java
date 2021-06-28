6effe0f9e76bbf52b6cf5476f5ba62c40ce9d86d
Pablo Molina <contact@pablomolina.me>
2021-06-28 09:28:43 +0200

    LPS-134609 Add label option to filter fragment

--- modules/apps/fragment/fragment-renderer-collection-filter-impl/src/main/java/com/liferay/fragment/renderer/collection/filter/internal/CollectionFilterFragmentRenderer.java
+++ modules/apps/fragment/fragment-renderer-collection-filter-impl/src/main/java/com/liferay/fragment/renderer/collection/filter/internal/CollectionFilterFragmentRenderer.java
@@ -26,6 +26,7 @@ import com.liferay.fragment.renderer.collection.filter.internal.constants.Collec
 import com.liferay.fragment.util.configuration.FragmentEntryConfigurationParser;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
@@ -45,6 +46,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 import javax.servlet.RequestDispatcher;
@@ -78,14 +80,29 @@ public class CollectionFilterFragmentRenderer implements FragmentRenderer {
 			"content.Language", getClass());
 
 		try {
-			JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
-				StringUtil.read(
-					getClass(),
-					"/com/liferay/fragment/renderer/collection/filter" +
-						"/internal/dependencies/configuration.json"));
+			String configuration = StringUtil.read(
+				getClass(),
+				"/com/liferay/fragment/renderer/collection/filter/internal" +
+					"/dependencies/configuration.json");
+
+			JSONObject configurationJSONObject =
+				JSONFactoryUtil.createJSONObject(configuration);
+
+			String filterPlaceholder = _getFilterPlaceholder(
+				configuration, fragmentRendererContext, resourceBundle);
+
+			JSONObject filterTypeOptionsJSONObject =
+				_filterTypeOptionsJSONObject(configurationJSONObject);
+
+			if ((filterPlaceholder != null) &&
+				(filterTypeOptionsJSONObject != null)) {
+
+				filterTypeOptionsJSONObject.put(
+					"placeholder", filterPlaceholder);
+			}
 
 			return _fragmentEntryConfigurationParser.translateConfiguration(
-				jsonObject, resourceBundle);
+				configurationJSONObject, resourceBundle);
 		}
 		catch (JSONException jsonException) {
 			return StringPool.BLANK;
@@ -126,6 +143,15 @@ public class CollectionFilterFragmentRenderer implements FragmentRenderer {
 		ThemeDisplay themeDisplay =
 			(ThemeDisplay)httpServletRequest.getAttribute(
 				WebKeys.THEME_DISPLAY);
+
+		String label = GetterUtil.getString(
+			_fragmentEntryConfigurationParser.getFieldValue(
+				fragmentEntryLink.getConfiguration(),
+				fragmentEntryLink.getEditableValues(), themeDisplay.getLocale(),
+				"label"));
+
+		httpServletRequest.setAttribute(
+			CollectionFilterFragmentRendererWebKeys.LABEL, label);
 
 		boolean showLabel = GetterUtil.getBoolean(
 			_fragmentEntryConfigurationParser.getFieldValue(
@@ -254,6 +280,62 @@ public class CollectionFilterFragmentRenderer implements FragmentRenderer {
 			ConfigurableUtil.createConfigurable(
 				FFFragmentRendererCollectionFilterConfiguration.class,
 				properties);
+	}
+
+	private JSONObject _filterTypeOptionsJSONObject(
+		JSONObject configurationJSONObject) {
+
+		JSONArray fieldsJSONArray = JSONUtil.getValueAsJSONArray(
+			configurationJSONObject, "JSONArray/fieldSets", "JSONObject/0",
+			"JSONArray/fields");
+
+		if (fieldsJSONArray == null) {
+			return null;
+		}
+
+		for (Object fieldObject : fieldsJSONArray) {
+			JSONObject fieldJSONObject = (JSONObject)fieldObject;
+
+			if (Objects.equals(fieldJSONObject.getString("name"), "label") &&
+				fieldJSONObject.has("typeOptions")) {
+
+				return fieldJSONObject.getJSONObject("typeOptions");
+			}
+		}
+
+		return null;
+	}
+
+	private String _getFilterPlaceholder(
+		String configuration, FragmentRendererContext fragmentRendererContext,
+		ResourceBundle resourceBundle) {
+
+		FragmentEntryLink fragmentEntryLink =
+			fragmentRendererContext.getFragmentEntryLink();
+
+		if (fragmentEntryLink == null) {
+			return null;
+		}
+
+		Object sourceObject = _fragmentEntryConfigurationParser.getFieldValue(
+			configuration, fragmentEntryLink.getEditableValues(),
+			resourceBundle.getLocale(), "source");
+
+		if (Validator.isNull(sourceObject) ||
+			!JSONUtil.isValid((String)sourceObject)) {
+
+			return null;
+		}
+
+		try {
+			JSONObject sourceJSONObject = JSONFactoryUtil.createJSONObject(
+				(String)sourceObject);
+
+			return sourceJSONObject.getString("title");
+		}
+		catch (JSONException jsonException) {
+			return null;
+		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
