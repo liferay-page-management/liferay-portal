1fdea45184d7d51a5ca30a22a5ad5c3043bb0605
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-08-30 15:53:35 +0200

    LPS-138283 Also create a model listener to register the related info collection providers for the different DDMStructures of DLFileEntry. ClassName that is used to register them is DLFileEntryMetadata

---
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/model/listener/DDMStructureModelListener.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.internal.model.listener;
+
+import com.liferay.document.library.kernel.model.DLFileEntryMetadata;
+import com.liferay.document.library.web.internal.info.collection.provider.FileEntryDDMStructureRelatedInfoCollectionProvider;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.info.collection.provider.RelatedInfoItemCollectionProvider;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.repository.RepositoryProvider;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true,
+	service = {ModelListener.class, PortalInstanceLifecycleListener.class}
+)
+public class DDMStructureModelListener
+	extends BaseModelListener<DDMStructure>
+	implements PortalInstanceLifecycleListener {
+
+	@Override
+	public void onAfterCreate(DDMStructure ddmStructure)
+		throws ModelListenerException {
+
+		if (ddmStructure.getClassNameId() != _portal.getClassNameId(
+				DLFileEntryMetadata.class.getName())) {
+
+			return;
+		}
+
+		Map<Long, ServiceRegistration<?>> serviceRegistrations =
+			_serviceRegistrations.computeIfAbsent(
+				ddmStructure.getCompanyId(), key -> new HashMap<>());
+
+		serviceRegistrations.put(
+			ddmStructure.getStructureId(),
+			_bundleContext.registerService(
+				RelatedInfoItemCollectionProvider.class,
+				new FileEntryDDMStructureRelatedInfoCollectionProvider(
+					ddmStructure, _repositoryProvider),
+				null));
+	}
+
+	@Override
+	public void onAfterRemove(DDMStructure ddmStructure)
+		throws ModelListenerException {
+
+		if (ddmStructure.getClassNameId() != _portal.getClassNameId(
+				DLFileEntryMetadata.class.getName())) {
+
+			return;
+		}
+
+		Map<Long, ServiceRegistration<?>> serviceRegistrations =
+			_serviceRegistrations.get(ddmStructure.getCompanyId());
+
+		if (MapUtil.isNotEmpty(serviceRegistrations)) {
+			ServiceRegistration<?> serviceRegistration =
+				serviceRegistrations.remove(ddmStructure.getStructureId());
+
+			if (serviceRegistration != null) {
+				serviceRegistration.unregister();
+			}
+		}
+	}
+
+	@Override
+	public void portalInstanceRegistered(Company company) {
+		Map<Long, ServiceRegistration<?>> serviceRegistrations =
+			new HashMap<>();
+
+		List<DDMStructure> ddmStructures =
+			_ddmStructureLocalService.getClassStructures(
+				company.getCompanyId(),
+				_portal.getClassNameId(DLFileEntryMetadata.class.getName()));
+
+		for (DDMStructure ddmStructure : ddmStructures) {
+			serviceRegistrations.put(
+				ddmStructure.getStructureId(),
+				_bundleContext.registerService(
+					RelatedInfoItemCollectionProvider.class,
+					new FileEntryDDMStructureRelatedInfoCollectionProvider(
+						ddmStructure, _repositoryProvider),
+					null));
+		}
+
+		if (MapUtil.isNotEmpty(serviceRegistrations)) {
+			_serviceRegistrations.put(
+				company.getCompanyId(), serviceRegistrations);
+		}
+	}
+
+	@Override
+	public void portalInstanceUnregistered(Company company) {
+		Map<Long, ServiceRegistration<?>> serviceRegistrations =
+			_serviceRegistrations.remove(company.getCompanyId());
+
+		for (Map.Entry<Long, ServiceRegistration<?>> entry :
+				serviceRegistrations.entrySet()) {
+
+			ServiceRegistration<?> serviceRegistration = entry.getValue();
+
+			if (serviceRegistration != null) {
+				serviceRegistration.unregister();
+			}
+		}
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+	}
+
+	private BundleContext _bundleContext;
+
+	@Reference
+	private DDMStructureLocalService _ddmStructureLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private RepositoryProvider _repositoryProvider;
+
+	private final Map<Long, Map<Long, ServiceRegistration<?>>>
+		_serviceRegistrations = Collections.synchronizedMap(
+			new LinkedHashMap<>());
+
+}
