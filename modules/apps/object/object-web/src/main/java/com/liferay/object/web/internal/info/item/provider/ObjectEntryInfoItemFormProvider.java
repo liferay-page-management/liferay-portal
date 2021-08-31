3cc8d31c7fec02ae5dba46b5427f359935e0fa03
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-08-31 13:48:04 +0100

    LPS-137569 Rename to className and classPK

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/info/item/provider/ObjectEntryInfoItemFormProvider.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/info/item/provider/ObjectEntryInfoItemFormProvider.java
@@ -203,7 +203,7 @@ public class ObjectEntryInfoItemFormProvider
 			}
 		).infoFieldSetEntry(
 			_templateInfoItemFieldSetProvider.getInfoFieldSet(
-				ObjectEntry.class.getName(), String.valueOf(objectDefinitionId))
+				ObjectEntry.class.getName(), objectDefinitionId)
 		).labelInfoLocalizedValue(
 			InfoLocalizedValue.singleValue(objectDefinition.getName())
 		).name(
