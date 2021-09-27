6ce3f5a3b0904e7ab8e8e0777f5b2a57bdd2cc05
Rubén Pulido <ruben.pulido@liferay.com>
2021-09-27 17:56:51 +0200

    LPS-138102 Set collectionStyledLayoutStructureItemIds in fragmentRendererContext

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -1523,6 +1523,12 @@ public class ContentPageEditorDisplayContext {
 				DefaultFragmentRendererContext fragmentRendererContext =
 					new DefaultFragmentRendererContext(fragmentEntryLink);
 
+				fragmentRendererContext.
+					setCollectionStyledLayoutStructureItemIds(
+						LayoutStructureUtil.
+							getCollectionStyledLayoutStructureItemIds(
+								fragmentEntryLink.getFragmentEntryLinkId(),
+								_getLayoutStructure()));
 				fragmentRendererContext.setLocale(themeDisplay.getLocale());
 				fragmentRendererContext.setMode(
 					FragmentEntryLinkConstants.EDIT);
