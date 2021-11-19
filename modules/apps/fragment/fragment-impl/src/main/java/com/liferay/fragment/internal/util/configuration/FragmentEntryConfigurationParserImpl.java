b9e2ad40dc6b7d32c0faa71570393a25dd847d99
Rubén Pulido <ruben.pulido@liferay.com>
2021-11-19 16:11:36 +0100

    LPS-141471 Implement getContextObjectsMap passing segmentsEntryIds to _getInfoListObjectEntry.
    Change existing getContextObjects to call overloaded method.

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/util/configuration/FragmentEntryConfigurationParserImpl.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/util/configuration/FragmentEntryConfigurationParserImpl.java
@@ -199,6 +199,15 @@ public class FragmentEntryConfigurationParserImpl
 	public Map<String, Object> getContextObjects(
 		JSONObject configurationValuesJSONObject, String configuration) {
 
+		return getContextObjectsMap(
+			configurationValuesJSONObject, configuration, new long[] {0});
+	}
+
+	@Override
+	public Map<String, Object> getContextObjectsMap(
+		JSONObject configurationValuesJSONObject, String configuration,
+		long[] segmentsEntryIds) {
+
 		HashMap<String, Object> contextObjects = new HashMap<>();
 
 		List<FragmentConfigurationField> fragmentConfigurationFields =
@@ -228,7 +237,8 @@ public class FragmentEntryConfigurationParserImpl
 					"collectionSelector")) {
 
 				Object contextListObject = _getInfoListObjectEntry(
-					configurationValuesJSONObject.getString(name));
+					configurationValuesJSONObject.getString(name),
+					segmentsEntryIds);
 
 				if (contextListObject != null) {
 					contextObjects.put(
