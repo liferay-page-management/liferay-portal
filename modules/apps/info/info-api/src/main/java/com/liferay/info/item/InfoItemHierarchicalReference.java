19feba96d697c84ff5d3ed38e1b72db1171eedc6
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-11-30 16:25:18 +0100

    LPS-142508 Adds new class to handle an info item hierarchy

---
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/item/InfoItemHierarchicalReference.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.item;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class InfoItemHierarchicalReference extends InfoItemReference {
+
+	public InfoItemHierarchicalReference(
+		String className, InfoItemIdentifier infoItemIdentifier) {
+
+		super(className, infoItemIdentifier);
+	}
+
+	public InfoItemHierarchicalReference(String className, long classPK) {
+		super(className, classPK);
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (!super.equals(object) ||
+			!(object instanceof InfoItemHierarchicalReference)) {
+
+			return false;
+		}
+
+		InfoItemHierarchicalReference infoItemHierarchicalReference =
+			(InfoItemHierarchicalReference)object;
+
+		return Objects.equals(
+			_children, infoItemHierarchicalReference._children);
+	}
+
+	public List<InfoItemHierarchicalReference> getChildren() {
+		return _children;
+	}
+
+	public void setChildren(List<InfoItemHierarchicalReference> children) {
+		_children = children;
+	}
+
+	private List<InfoItemHierarchicalReference> _children = new ArrayList<>();
+
+}
