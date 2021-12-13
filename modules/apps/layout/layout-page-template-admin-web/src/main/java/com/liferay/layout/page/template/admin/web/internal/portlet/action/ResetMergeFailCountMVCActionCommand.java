486f702678a246490780b67ccc4479656c97a7cb
balazssk <balazs.safrany-kovalik@liferay.com>
2021-12-13 09:32:19 +0100

    LPS-143456 rename

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/action/ResetMergeFailCountMVCActionCommand.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/action/ResetMergeFailCountMVCActionCommand.java
@@ -34,11 +34,11 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true,
 	property = {
 		"javax.portlet.name=" + LayoutPageTemplateAdminPortletKeys.LAYOUT_PAGE_TEMPLATES,
-		"mvc.command.name=/layout_page_template_admin/reset_merge_fail_count_and_merge"
+		"mvc.command.name=/layout_page_template_admin/reset_merge_fail_count"
 	},
 	service = MVCActionCommand.class
 )
-public class ResetMergeFailCountAndMergeMVCActionCommand
+public class ResetMergeFailCountMVCActionCommand
 	extends BaseMVCActionCommand {
 
 	@Override
