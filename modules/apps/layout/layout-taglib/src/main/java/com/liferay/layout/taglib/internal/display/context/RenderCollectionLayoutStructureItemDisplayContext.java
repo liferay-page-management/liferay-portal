liferay-page-management/liferay-portal

commit b01d2fa99f1df9b6e83e28de938557d15c859061

LPS-142557 we don't have to do it this way, because it shouldn't matter in this class, BUT, it's a best practice to follow this pattern in case someone else cuts and paste code like this somewhere else where separate threads would cause problems

modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/display/context/RenderCollectionLayoutStructureItemDisplayContext.java
@@ -268,7 +268,7 @@ public class RenderCollectionLayoutStructureItemDisplayContext {
 			return _numberOfItemsToDisplay;
 		}
 
-		_numberOfItemsToDisplay = getTotalNumberOfItems();
+		int numberOfItemsToDisplay = getTotalNumberOfItems();
 
 		if (Validator.isNotNull(
 				_collectionStyledLayoutStructureItem.getPaginationType()) &&
@@ -276,10 +276,12 @@ public class RenderCollectionLayoutStructureItemDisplayContext {
 				_collectionStyledLayoutStructureItem.getPaginationType(),
 				"none")) {
 
-			_numberOfItemsToDisplay = Math.min(
+			numberOfItemsToDisplay = Math.min(
 				_numberOfItemsToDisplay,
 				_collectionStyledLayoutStructureItem.getNumberOfItemsPerPage());
 		}
+
+		_numberOfItemsToDisplay = numberOfItemsToDisplay;
 
 		return _numberOfItemsToDisplay;
 	}

commit 951a506412bec5df1363b3a6a35af8cf2765fd38

LPS-142557 Avoid NPE caused by refactor in b01d2fa99f1df9b6e83e28de938557d15c859061

modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/display/context/RenderCollectionLayoutStructureItemDisplayContext.java
@@ -277,7 +277,7 @@ public class RenderCollectionLayoutStructureItemDisplayContext {
 				"none")) {
 
 			numberOfItemsToDisplay = Math.min(
-				_numberOfItemsToDisplay,
+				numberOfItemsToDisplay,
 				_collectionStyledLayoutStructureItem.getNumberOfItemsPerPage());
 		}
 