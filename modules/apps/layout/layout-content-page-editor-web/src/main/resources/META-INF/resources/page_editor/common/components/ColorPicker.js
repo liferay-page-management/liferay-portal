a56f4ee010421d0988233abab157dac5694c344c
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2022-01-19 09:10:44 +0100

    LPS-145619 Disable token color in the dropdown when two tokens are mutually referenced

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/ColorPicker.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/ColorPicker.js
@@ -110,6 +110,9 @@ export function ColorPicker({
 		}
 	}, [value, config.tokenReuseEnabled]);
 
+	const tokenIsDisabled = (name) =>
+		name === field.name || editedTokenValues?.[name]?.name === field.name;
+
 	const onSetValue = (value, label, name) => {
 		setColor(value);
 		setTokenLabel(label);
@@ -233,9 +236,9 @@ export function ColorPicker({
 								active={activeColorPicker}
 								colors={colors}
 								config={config}
-								fieldName={field.name}
 								label={tokenLabel}
 								onSetActive={setActiveColorPicker}
+								onTokenIsDisabled={tokenIsDisabled}
 								onValueChange={({label, name, value}) =>
 									onSetValue(value, label, name)
 								}
@@ -330,15 +333,9 @@ export function ColorPicker({
 																aria-posinset={
 																	index
 																}
-																disabled={
-																	token.name ===
-																		field.name ||
-																	editedTokenValues?.[
-																		token
-																			.name
-																	]?.name ===
-																		field.name
-																}
+																disabled={tokenIsDisabled(
+																	token.name
+																)}
 																key={token.name}
 																onClick={() =>
 																	onClickAutocompleteItem(
@@ -434,8 +431,8 @@ export function ColorPicker({
 										active={activeColorPicker}
 										colors={colors}
 										config={config}
-										fieldName={field.name}
 										onSetActive={setActiveColorPicker}
+										onTokenIsDisabled={tokenIsDisabled}
 										onValueChange={({label, name, value}) =>
 											onSetValue(value, label, name)
 										}
