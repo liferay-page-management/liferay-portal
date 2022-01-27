d09835b05b68c1eb6848da54a8422f415aacf181
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2022-01-27 18:50:26 +0100

    LPS-146050 Get the input color and convert it from rgb to hexadecimal so that the colorpicker understands it for stylebook

--- modules/apps/style-book/style-book-web/src/main/resources/META-INF/resources/js/style-book-editor/frontend_tokens/ColorFrontendToken.js
+++ modules/apps/style-book/style-book-web/src/main/resources/META-INF/resources/js/style-book-editor/frontend_tokens/ColorFrontendToken.js
@@ -14,7 +14,10 @@
 
 import ClayColorPicker from '@clayui/color-picker';
 import ClayForm, {ClayInput} from '@clayui/form';
-import {ColorPicker} from '@liferay/layout-content-page-editor-web';
+import {
+	ColorPicker,
+	convertRGBtoHex,
+} from '@liferay/layout-content-page-editor-web';
 import {debounce} from 'frontend-js-web';
 import PropTypes from 'prop-types';
 import React, {useEffect, useRef, useState} from 'react';
@@ -47,6 +50,28 @@ export default function ColorFrontendToken({
 				'--style-book-color-picker-color',
 				color
 			);
+		}
+
+		if (color.startsWith('#')) {
+			setCustomColors([color]);
+		}
+		else {
+			const element = document.createElement('div');
+
+			Object.assign(element.style, {background: color, display: 'none'});
+			document.body.appendChild(element);
+
+			const backgroundColor = element.style.background;
+
+			setCustomColors([
+				convertRGBtoHex(
+					backgroundColor
+						? window.getComputedStyle(element).backgroundColor
+						: 'FFFFFF'
+				),
+			]);
+
+			element.parentElement.removeChild(element);
 		}
 	}, [color]);
 
