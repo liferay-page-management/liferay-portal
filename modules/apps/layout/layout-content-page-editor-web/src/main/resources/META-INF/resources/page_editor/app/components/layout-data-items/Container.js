1a4eefa17a0987d7a7fb8517ed5cc8bb537950bb
Sandro Chinea <sandrochinea@gmail.com>
2022-03-22 11:56:55 +0000

    LPS-147896 Add support for setting Flex Wrap from Container in edit mode and take it into account when rendering

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Container.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Container.js
@@ -78,7 +78,13 @@ const Container = React.forwardRef(
 			width,
 		} = itemConfig.styles;
 
-		const {align, contentDisplay, justify, widthType} = itemConfig;
+		const {
+			align,
+			contentDisplay,
+			flexWrap,
+			justify,
+			widthType,
+		} = itemConfig;
 
 		const backgroundImageValue = useBackgroundImageValue(
 			elementId,
@@ -173,6 +179,7 @@ const Container = React.forwardRef(
 					'empty': !item.children.length && !height,
 					[`bg-${backgroundColor}`]:
 						backgroundColor && !backgroundColor.startsWith('#'),
+					[flexWrap]: !!flexWrap,
 					[justify]: !!justify,
 					[`mb-${marginBottom}`]: isValidSpacingOption(marginBottom),
 					[`mt-${marginTop}`]: isValidSpacingOption(marginTop),
