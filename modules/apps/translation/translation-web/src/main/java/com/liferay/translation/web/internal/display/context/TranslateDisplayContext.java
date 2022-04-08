c5eb9a113a3837f6e990bcfe5fbcf429a70b9f4b
Victor Galan <victor.galan@liferay.com>
2022-04-08 11:47:50 +0200

    LPS-145232 Use the uniqueId when possible

--- modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/display/context/TranslateDisplayContext.java
+++ modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/display/context/TranslateDisplayContext.java
@@ -184,7 +184,8 @@ public class TranslateDisplayContext {
 						return stream.map(
 							infoField -> {
 								String infoFieldId =
-									"infoField--" + infoField.getName() + "--";
+									"infoField--" + infoField.getUniqueId() +
+										"--";
 
 								Map<String, Object> editorConfiguration = null;
 
@@ -353,7 +354,8 @@ public class TranslateDisplayContext {
 		InfoField infoField, Locale locale) {
 
 		Collection<InfoFieldValue<Object>> infoFieldValues =
-			_sourceInfoItemFieldValues.getInfoFieldValues(infoField.getName());
+			_sourceInfoItemFieldValues.getInfoFieldValues(
+				infoField.getUniqueId());
 
 		Stream<InfoFieldValue<Object>> stream = infoFieldValues.stream();
 
@@ -377,7 +379,8 @@ public class TranslateDisplayContext {
 		InfoField infoField, Locale locale) {
 
 		Collection<InfoFieldValue<Object>> infoFieldValues =
-			_targetInfoItemFieldValues.getInfoFieldValues(infoField.getName());
+			_targetInfoItemFieldValues.getInfoFieldValues(
+				infoField.getUniqueId());
 
 		Stream<InfoFieldValue<Object>> stream = infoFieldValues.stream();
 
