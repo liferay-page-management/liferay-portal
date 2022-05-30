b867ac2a5b029c51df2a793c59b23ef99055b03c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-05-30 17:02:20 +0200

    LPS-155007 Adds service implementations

--- modules/apps/client-extension/client-extension-service/src/main/java/com/liferay/client/extension/service/impl/ClientExtensionEntryRelLocalServiceImpl.java
+++ modules/apps/client-extension/client-extension-service/src/main/java/com/liferay/client/extension/service/impl/ClientExtensionEntryRelLocalServiceImpl.java
@@ -14,13 +14,21 @@
 
 package com.liferay.client.extension.service.impl;
 
+import com.liferay.client.extension.model.ClientExtensionEntryRel;
 import com.liferay.client.extension.service.base.ClientExtensionEntryRelLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+
+import java.util.Date;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Brian Wing Shun Chan
+ * @author Eudaldo Alonso
  */
 @Component(
 	property = "model.class.name=com.liferay.client.extension.model.ClientExtensionEntryRel",
@@ -28,4 +36,62 @@ import org.osgi.service.component.annotations.Component;
 )
 public class ClientExtensionEntryRelLocalServiceImpl
 	extends ClientExtensionEntryRelLocalServiceBaseImpl {
+
+	@Override
+	public ClientExtensionEntryRel addClientExtensionEntryRel(
+			long userId, long classNameId, long classPK,
+			long clientExtensionEntryId, String type)
+		throws PortalException {
+
+		long clientExtensionEntryRelId = counterLocalService.increment();
+
+		ClientExtensionEntryRel clientExtensionEntryRel =
+			clientExtensionEntryRelPersistence.create(
+				clientExtensionEntryRelId);
+
+		User user = _userLocalService.getUser(userId);
+
+		clientExtensionEntryRel.setCompanyId(user.getCompanyId());
+		clientExtensionEntryRel.setUserId(user.getUserId());
+		clientExtensionEntryRel.setUserName(user.getFullName());
+
+		clientExtensionEntryRel.setCreateDate(new Date());
+		clientExtensionEntryRel.setModifiedDate(new Date());
+		clientExtensionEntryRel.setClassNameId(classNameId);
+		clientExtensionEntryRel.setClassPK(classPK);
+		clientExtensionEntryRel.setClientExtensionEntryId(
+			clientExtensionEntryId);
+		clientExtensionEntryRel.setType(type);
+
+		return clientExtensionEntryRelPersistence.update(
+			clientExtensionEntryRel);
+	}
+
+	@Override
+	public ClientExtensionEntryRel fetchClientExtensionEntryRel(
+		long classNameId, long classPK, String type) {
+
+		return clientExtensionEntryRelPersistence.fetchByC_C_T_First(
+			classNameId, classPK, type, null);
+	}
+
+	@Override
+	public List<ClientExtensionEntryRel> getClientExtensionEntryRels(
+		long classNameId, long classPK, String type, int start, int end) {
+
+		return clientExtensionEntryRelPersistence.findByC_C_T(
+			classNameId, classPK, type, start, end);
+	}
+
+	@Override
+	public int getClientExtensionEntryRelsCount(
+		long classNameId, long classPK, String type) {
+
+		return clientExtensionEntryRelPersistence.countByC_C_T(
+			classNameId, classPK, type);
+	}
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
