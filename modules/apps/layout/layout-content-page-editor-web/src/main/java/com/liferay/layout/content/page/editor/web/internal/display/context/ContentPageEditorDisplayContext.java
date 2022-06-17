d2a1ba915150f9f5cb1f0e485fb465aa76154409
Victor Galan <victor.galan@liferay.com>
2022-06-17 13:03:35 +0200

    LPS-146828 Add parameter to avoid rendering the link to the styles where we don't need it

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -448,8 +448,11 @@ public class ContentPageEditorDisplayContext {
 					String layoutURL = PortalUtil.getLayoutFriendlyURL(
 						themeDisplay.getLayout(), themeDisplay);
 
-					return HttpComponentsUtil.addParameter(
+					layoutURL = HttpComponentsUtil.addParameter(
 						layoutURL, "p_l_mode", Constants.PREVIEW);
+
+					return HttpComponentsUtil.addParameter(
+						layoutURL, "pageEditorPreview", Boolean.TRUE);
 				}
 			).put(
 				"getInfoItemFieldValueURL",
