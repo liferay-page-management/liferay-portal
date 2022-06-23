a8a7b6a8ca21c5cfbfd3efce34a0d49fd235c38f
Pablo Molina <contact@pablomolina.me>
2022-06-23 12:29:54 +0200

    LPS-156518 Typo

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/FragmentEntryFragmentRenderer.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/FragmentEntryFragmentRenderer.java
@@ -283,7 +283,7 @@ public class FragmentEntryFragmentRenderer implements FragmentRenderer {
 			sb.append(fragmentElementId);
 			sb.append("'); var fragmentNamespace = '");
 			sb.append(namespace);
-			sb.append("'); var input = '");
+			sb.append("'; var input = '");
 			sb.append(
 				_getInputJSONObject(
 					fragmentEntryLink, httpServletRequest, infoFormOptional,
