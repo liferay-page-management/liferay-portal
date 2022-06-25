a26b91feca37a9e8cd9940acbb200f42180047e5
Lianne Louie <lianne.louie@liferay.com>
2022-06-24 20:49:55 -0700

    LPS-152888 - change redirect in the specific case where we are viewing in context and we use a related assets portlet (for example: clicking 'next' to scroll through the related assets portlet)

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/util/AssetPublisherHelperImpl.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/util/AssetPublisherHelperImpl.java
@@ -48,6 +48,7 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.module.configuration.ConfigurationException;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
@@ -93,6 +94,8 @@ import java.util.TimeZone;
 import javax.portlet.PortletPreferences;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -627,9 +630,27 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 				if (Validator.isNotNull(viewURL) &&
 					!Objects.equals(viewURL, noSuchEntryRedirect)) {
 
-					viewURL = HttpComponentsUtil.setParameter(
-						viewURL, "redirect",
-						_portal.getCurrentURL(liferayPortletRequest));
+					Portlet portlet = liferayPortletRequest.getPortlet();
+					String currentURL = _portal.getCurrentURL(
+						liferayPortletRequest);
+
+					if (Objects.equals(
+							portlet.getPortletName(),
+							AssetPublisherPortletKeys.RELATED_ASSETS) &&
+						currentURL.contains(
+							liferayPortletRequest.getPortletName())) {
+
+						HttpServletRequest httpServletRequest =
+							liferayPortletRequest.getHttpServletRequest();
+
+						viewURL = HttpComponentsUtil.setParameter(
+							viewURL, "redirect",
+							httpServletRequest.getHeader("Referer"));
+					}
+					else {
+						viewURL = HttpComponentsUtil.setParameter(
+							viewURL, "redirect", currentURL);
+					}
 				}
 			}
 			catch (Exception exception) {
