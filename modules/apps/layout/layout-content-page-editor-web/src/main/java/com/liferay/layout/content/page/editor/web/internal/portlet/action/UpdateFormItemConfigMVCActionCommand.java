8760b5472aa96e91d00d33c0851e3152d8ab3a92
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-07-20 13:25:49 +0200

    LPS-158326 SF buy space

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateFormItemConfigMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateFormItemConfigMVCActionCommand.java
@@ -100,10 +100,6 @@ public class UpdateFormItemConfigMVCActionCommand extends BaseMVCActionCommand {
 			_fragmentCollectionContributorTracker.
 				getFragmentCollectionContributor("INPUTS");
 
-		FragmentEntry fragmentEntry =
-			_fragmentCollectionContributorTracker.getFragmentEntry(
-				"INPUTS-submit-button");
-
 		if (fragmentCollectionContributor == null) {
 			jsonObject.put(
 				"errorMessage",
@@ -111,8 +107,15 @@ public class UpdateFormItemConfigMVCActionCommand extends BaseMVCActionCommand {
 					themeDisplay.getLocale(),
 					"your-form-could-not-be-loaded-because-fragments-are-not-" +
 						"available"));
+
+			return Collections.emptyList();
 		}
-		else if (fragmentEntry == null) {
+
+		FragmentEntry fragmentEntry =
+			_fragmentCollectionContributorTracker.getFragmentEntry(
+				"INPUTS-submit-button");
+
+		if (fragmentEntry == null) {
 			jsonObject.put(
 				"errorMessage",
 				LanguageUtil.format(
@@ -120,26 +123,25 @@ public class UpdateFormItemConfigMVCActionCommand extends BaseMVCActionCommand {
 					"some-fragments-are-missing-x-could-not-be-added-to-your-" +
 						"form-because-they-are-not-available",
 					"submit-button"));
-		}
-		else {
-			FragmentEntryProcessorContext fragmentEntryProcessorContext =
-				new DefaultFragmentEntryProcessorContext(
-					httpServletRequest, httpServletResponse,
-					FragmentEntryLinkConstants.EDIT,
-					LocaleUtil.getMostRelevantLocale());
 
-			ServiceContext serviceContext = ServiceContextFactory.getInstance(
-				httpServletRequest);
-
-			FragmentEntryLink fragmentEntryLink = _addFragmentEntryLink(
-				formItemId, fragmentEntry, fragmentEntryProcessorContext,
-				layoutStructure, segmentsExperienceId, serviceContext,
-				themeDisplay);
-
-			return ListUtil.fromArray(fragmentEntryLink);
+			return Collections.emptyList();
 		}
 
-		return Collections.emptyList();
+		FragmentEntryProcessorContext fragmentEntryProcessorContext =
+			new DefaultFragmentEntryProcessorContext(
+				httpServletRequest, httpServletResponse,
+				FragmentEntryLinkConstants.EDIT,
+				LocaleUtil.getMostRelevantLocale());
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			httpServletRequest);
+
+		FragmentEntryLink fragmentEntryLink = _addFragmentEntryLink(
+			formItemId, fragmentEntry, fragmentEntryProcessorContext,
+			layoutStructure, segmentsExperienceId, serviceContext,
+			themeDisplay);
+
+		return ListUtil.fromArray(fragmentEntryLink);
 	}
 
 	private FragmentEntryLink _addFragmentEntryLink(
