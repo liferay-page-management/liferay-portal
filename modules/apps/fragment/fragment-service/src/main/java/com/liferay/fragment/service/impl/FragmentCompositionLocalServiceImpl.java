34b1970f5a28ef0d39ee56ae05284d3320e37719
Diego Hu <diego.hu@liferay.com>
2022-07-26 11:04:55 +0200

    LPS-155625 Change error messages to Localized messages

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentCompositionLocalServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentCompositionLocalServiceImpl.java
@@ -342,7 +342,7 @@ public class FragmentCompositionLocalServiceImpl
 
 		if (description.length() > descriptionMaxLength) {
 			throw new FragmentCompositionDescriptionException(
-				"Maximum length of description exceeded");
+				"maximum-length-of-description-exceeded");
 		}
 	}
 
@@ -371,7 +371,7 @@ public class FragmentCompositionLocalServiceImpl
 			name.contains(StringPool.SLASH)) {
 
 			throw new FragmentCompositionNameException(
-				"Name contains invalid characters");
+				"name-contains-invalid-characters");
 		}
 
 		int nameMaxLength = ModelHintsUtil.getMaxLength(
@@ -379,7 +379,7 @@ public class FragmentCompositionLocalServiceImpl
 
 		if (name.length() > nameMaxLength) {
 			throw new FragmentCompositionNameException(
-				"Maximum length of name exceeded");
+				"maximum-length-of-name-exceeded");
 		}
 	}
 
