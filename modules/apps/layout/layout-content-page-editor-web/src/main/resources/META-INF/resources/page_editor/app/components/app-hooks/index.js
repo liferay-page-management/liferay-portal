14798741be250c5cdc2b36de3e066f003b3c2192
Pablo Molina <contact@pablomolina.me>
2022-08-02 14:38:26 +0200

    LPS-159329 Extract App hooks

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/app-hooks/index.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {useBackURL} from './useBackURL';
+import {useExtendSession} from './useExtendSession';
+import {useLanguageDirection} from './useLanguageDirection';
+import {usePortletConfigurationListener} from './usePortletConfigurationListener';
+import {useURLParser} from './useURLParser';
+
+const HOOKS = [
+	useExtendSession,
+	useBackURL,
+	useLanguageDirection,
+	usePortletConfigurationListener,
+	useURLParser,
+];
+
+export default function AppHooks() {
+	HOOKS.forEach((hook) => hook());
+
+	return null;
+}
