01eba3a8b968969cb1460c3a0d91b393c8ecac53
Pablo Molina <contact@pablomolina.me>
2022-08-02 14:44:49 +0200

    LPS-159329 Move DisplayPagePreviewItemSelector modal to component

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/App.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/App.js
@@ -12,14 +12,11 @@
  * details.
  */
 
-import {ReactPortal} from '@liferay/frontend-js-react-web';
 import PropTypes from 'prop-types';
-import React, {useMemo} from 'react';
+import React from 'react';
 
 import {StyleBookContextProvider} from '../../plugins/page-design-options/hooks/useStyleBook';
 import {INIT} from '../actions/types';
-import {LAYOUT_TYPES} from '../config/constants/layoutTypes';
-import {config} from '../config/index';
 import {CollectionActiveItemContextProvider} from '../contexts/CollectionActiveItemContext';
 import {ControlsProvider} from '../contexts/ControlsContext';
 import {DisplayPagePreviewItemContextProvider} from '../contexts/DisplayPagePreviewItemContext';
@@ -40,13 +37,6 @@ import Toolbar from './Toolbar';
 import AppHooks from './app-hooks/index';
 
 export default function App({state}) {
-	const displayPagePreviewItemSelectorWrapper = useMemo(
-		() =>
-			config.layoutType === LAYOUT_TYPES.display &&
-			document.getElementById('infoItemSelectorContainer'),
-		[]
-	);
-
 	const initialState = reducer(state, {type: INIT});
 
 	return (
@@ -59,17 +49,7 @@ export default function App({state}) {
 						<EditableProcessorContextProvider>
 							<DisplayPagePreviewItemContextProvider>
 								<WidgetsContextProvider>
-									{displayPagePreviewItemSelectorWrapper ? (
-										<ReactPortal
-											container={
-												displayPagePreviewItemSelectorWrapper
-											}
-										>
-											<DisplayPagePreviewItemSelector
-												dark
-											/>
-										</ReactPortal>
-									) : null}
+									<DisplayPagePreviewItemSelector dark />
 
 									<DragPreview />
 
