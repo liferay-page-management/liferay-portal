7dcfebdae953c223dfd69e73dc6b9000a8b28e50
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-08-05 16:31:41 +0200

    LPS-159886 Use adapted language keys

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateFormItemConfigMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateFormItemConfigMVCActionCommand.java
@@ -253,8 +253,8 @@ public class UpdateFormItemConfigMVCActionCommand extends BaseMVCActionCommand {
 				"errorMessage",
 				_language.format(
 					themeDisplay.getLocale(),
-					"some-fragments-are-missing.-x-fields-do-not-have-an-" +
-						"associated-fragment",
+					"some-fragments-are-missing.-x-fields-may-not-have-an-" +
+						"associated-fragment-or-may-not-be-available-in-master",
 					missingInputTypes.first()));
 		}
 		else if (missingInputTypes.size() > 1) {
@@ -262,8 +262,9 @@ public class UpdateFormItemConfigMVCActionCommand extends BaseMVCActionCommand {
 				"errorMessage",
 				_language.format(
 					themeDisplay.getLocale(),
-					"some-fragments-are-missing.-x-and-x-fields-do-not-have-" +
-						"an-associated-fragment",
+					"some-fragments-are-missing.-x-and-x-fields-may-not-have-" +
+						"an-associated-fragment-or-may-not-be-available-in-" +
+							"master",
 					new String[] {
 						StringUtil.merge(
 							missingInputTypes.headSet(missingInputTypes.last()),
