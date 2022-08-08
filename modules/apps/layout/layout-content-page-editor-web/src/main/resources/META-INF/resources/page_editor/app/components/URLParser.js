2c754fb93bc5697540a60c5bfd6527e071c0eb8a
Pablo Molina <contact@pablomolina.me>
2022-08-08 15:23:53 +0200

    LPS-159980 Fix unknown messageId

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/URLParser.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/URLParser.js
@@ -45,7 +45,8 @@ export default function URLParser() {
 				)
 			) || {fragmentEntryLinkId: null};
 
-			const {itemId} = getFragmentItem(layoutData, fragmentEntryLinkId);
+			const {itemId} =
+				getFragmentItem(layoutData, fragmentEntryLinkId) || {};
 
 			if (itemId) {
 				selectItem(itemId);
