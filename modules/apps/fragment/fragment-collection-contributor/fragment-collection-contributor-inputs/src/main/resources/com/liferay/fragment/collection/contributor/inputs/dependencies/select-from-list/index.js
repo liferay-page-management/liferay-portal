a5c0daf5c46bb49ef4b869df3db116934a3494a8
Pablo Molina <contact@pablomolina.me>
2022-11-08 10:55:51 +0100

    LPS-165872 Use data attribute to store selected option

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/select-from-list/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/select-from-list/index.js
@@ -166,7 +166,7 @@ function createOptionElement(option) {
 	return optionElement;
 }
 
-function setSelectedOption(option) {
+function setSelectedOption(optionElement) {
 	const selectedOption = document.getElementById(
 		`${fragmentEntryLinkNamespace}-option-${valueInputElement.value}`
 	);
@@ -175,12 +175,11 @@ function setSelectedOption(option) {
 		selectedOption.classList.remove('active');
 	}
 
-	option.classList.add('active');
+	optionElement.classList.add('active');
 
-	uiInputElement.value = option.textContent;
-
-	labelInputElement.value = option.textContent;
-	valueInputElement.value = option.id;
+	labelInputElement.value = optionElement.textContent;
+	uiInputElement.value = optionElement.textContent;
+	valueInputElement.value = optionElement.dataset.optionValue;
 
 	closeResultList();
 }
