7b814d00ab3c1e81e8a1541f3a35a3eb7bde42b1
mikellorza <mikel.lorza@liferay.com>
2023-04-12 18:10:59 +0200

    LPS-181172 Add the configuration screen to the menu access configuration

---
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/display/context/MenuAccessConfigurationDisplayContext.java
@@ -0,0 +1,186 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.admin.web.internal.display.context;
+
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.item.selector.ItemSelectorCriterion;
+import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.PortletURLUtil;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portlet.rolesadmin.search.RoleSearch;
+import com.liferay.roles.item.selector.regular.role.RegularRoleItemSelectorCriterion;
+import com.liferay.roles.item.selector.site.role.SiteRoleItemSelectorCriterion;
+import com.liferay.site.configuration.MenuAccessConfigurationProvider;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Mikel Lorza
+ */
+public class MenuAccessConfigurationDisplayContext {
+
+	public MenuAccessConfigurationDisplayContext(
+		ConfigurationProvider configurationProvider,
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse, ItemSelector itemSelector,
+		MenuAccessConfigurationProvider menuAccessConfigurationProvider,
+		Portal portal, RoleLocalService roleLocalService) {
+
+		_configurationProvider = configurationProvider;
+		_httpServletRequest = httpServletRequest;
+		_httpServletResponse = httpServletResponse;
+		_itemSelector = itemSelector;
+		_menuAccessConfigurationProvider = menuAccessConfigurationProvider;
+		_portal = portal;
+		_roleLocalService = roleLocalService;
+
+		_liferayPortletRequest = portal.getLiferayPortletRequest(
+			(PortletRequest)httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_REQUEST));
+		_liferayPortletResponse = portal.getLiferayPortletResponse(
+			(PortletResponse)httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_RESPONSE));
+	}
+
+	public String getEditMenuAccessURL() {
+		return PortletURLBuilder.createActionURL(
+			_liferayPortletResponse
+		).setActionName(
+			"/site_settings/edit_menu_access_configuration"
+		).setRedirect(
+			PortalUtil.getCurrentURL(_httpServletRequest)
+		).buildString();
+	}
+
+	public String getEventName() {
+		return _liferayPortletResponse.getNamespace() + "selectRole";
+	}
+
+	public String getRoleItemSelectorURL() throws ConfigurationException {
+		List<ItemSelectorCriterion> itemSelectorCriteria = new ArrayList<>();
+
+		RegularRoleItemSelectorCriterion regularRoleItemSelectorCriterion =
+			new RegularRoleItemSelectorCriterion();
+
+		regularRoleItemSelectorCriterion.setDesiredItemSelectorReturnTypes(
+			Collections.singletonList(new UUIDItemSelectorReturnType()));
+
+		itemSelectorCriteria.add(regularRoleItemSelectorCriterion);
+
+		String[] roles = getRolesCanSeeControlMenu();
+
+		long[] roleIds = new long[roles.length];
+
+		for (int i = 0; i < roles.length; i++) {
+			roleIds[i] = Long.valueOf(roles[i]);
+		}
+
+		regularRoleItemSelectorCriterion.setCheckedRoleIds(roleIds);
+
+		SiteRoleItemSelectorCriterion siteRoleItemSelectorCriterion =
+			new SiteRoleItemSelectorCriterion();
+
+		siteRoleItemSelectorCriterion.setDesiredItemSelectorReturnTypes(
+			Collections.singletonList(new UUIDItemSelectorReturnType()));
+
+		siteRoleItemSelectorCriterion.setCheckedRoleIds(roleIds);
+
+		itemSelectorCriteria.add(siteRoleItemSelectorCriterion);
+
+		return PortletURLBuilder.create(
+			_itemSelector.getItemSelectorURL(
+				RequestBackedPortletURLFactoryUtil.create(_httpServletRequest),
+				getEventName(),
+				itemSelectorCriteria.toArray(new ItemSelectorCriterion[0]))
+		).buildString();
+	}
+
+	public String[] getRolesCanSeeControlMenu() throws ConfigurationException {
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)_liferayPortletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		return _menuAccessConfigurationProvider.getRolesCanSeeControlMenu(
+			themeDisplay.getScopeGroupId());
+	}
+
+	public SearchContainer<Role> getSearchContainer() throws PortalException {
+		PortletURL currentURL = PortletURLUtil.getCurrent(
+			_liferayPortletRequest, _liferayPortletResponse);
+
+		SearchContainer<Role> searchContainer = new RoleSearch(
+			_liferayPortletRequest, currentURL);
+
+		searchContainer.setEmptyResultsMessage("no-roles-selected");
+
+		List<Role> roles = new ArrayList<>();
+
+		for (String roleId : getRolesCanSeeControlMenu()) {
+			Role role = _roleLocalService.fetchRole(Long.valueOf(roleId));
+
+			if (role != null) {
+				roles.add(role);
+			}
+		}
+
+		searchContainer.setResultsAndTotal(roles);
+
+		return searchContainer;
+	}
+
+	public boolean isShowControlMenuByRole() throws ConfigurationException {
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)_liferayPortletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		return _menuAccessConfigurationProvider.isShowControlMenuByRole(
+			themeDisplay.getScopeGroupId());
+	}
+
+	private final ConfigurationProvider _configurationProvider;
+	private final HttpServletRequest _httpServletRequest;
+	private final HttpServletResponse _httpServletResponse;
+	private final ItemSelector _itemSelector;
+	private final LiferayPortletRequest _liferayPortletRequest;
+	private final LiferayPortletResponse _liferayPortletResponse;
+	private final MenuAccessConfigurationProvider
+		_menuAccessConfigurationProvider;
+	private final Portal _portal;
+	private final RoleLocalService _roleLocalService;
+
+}
---
+++ modules/apps/site/site-admin-web/src/main/resources/META-INF/resources/site_settings/menu_access.jsp
@@ -0,0 +1,204 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+MenuAccessConfigurationDisplayContext menuAccessDisplayContext = (MenuAccessConfigurationDisplayContext)request.getAttribute(MenuAccessConfigurationDisplayContext.class.getName());
+%>
+
+<liferay-util:buffer
+	var="removeRoleIcon"
+>
+	<clay:icon
+		symbol="times-circle"
+	/>
+</liferay-util:buffer>
+
+<liferay-ui:error exception="<%= ConfigurationException.class %>" message="there-was-an-error-processing-one-or-more-of-the-configurations" />
+
+<clay:sheet-section>
+	<clay:content-row
+		containerElement="h3"
+		cssClass="sheet-subtitle"
+	>
+		<clay:content-col
+			expand="<%= true %>"
+		>
+			<span class="heading-text"><liferay-ui:message key="control-menu" /></span>
+		</clay:content-col>
+	</clay:content-row>
+
+	<clay:content-row>
+		<clay:content-col>
+			<clay:checkbox
+				checked="<%= menuAccessDisplayContext.isShowControlMenuByRole() %>"
+				id='<%= liferayPortletResponse.getNamespace() + "showControlMenuByRole" %>'
+				label='<%= LanguageUtil.get(request, "show-control-menu-by-role-name") %>'
+				name='<%= liferayPortletResponse.getNamespace() + "showControlMenuByRole" %>'
+			/>
+		</clay:content-col>
+	</clay:content-row>
+</clay:sheet-section>
+
+<clay:sheet-section>
+	<clay:content-row
+		containerElement="h3"
+		cssClass="sheet-subtitle"
+	>
+		<clay:content-col
+			expand="<%= true %>"
+		>
+			<span class="heading-text"><liferay-ui:message key="roles-can-see-control-menu-name" /></span>
+		</clay:content-col>
+
+		<clay:content-col>
+			<clay:button
+				aria-label='<%= LanguageUtil.get(request, "select") %>'
+				cssClass="modify-link"
+				displayType="secondary"
+				id='<%= liferayPortletResponse.getNamespace() + "selectRoleLink" %>'
+				label='<%= LanguageUtil.get(request, "select") %>'
+				small="<%= true %>"
+				title='<%= LanguageUtil.get(request, "select") %>'
+			/>
+		</clay:content-col>
+	</clay:content-row>
+
+	<clay:content-row>
+		<clay:content-col
+			expand="<%= true %>"
+		>
+			<liferay-ui:search-container
+				compactEmptyResultsMessage="<%= true %>"
+				headerNames="title,null"
+				id="roleSearchContainer"
+				searchContainer="<%= menuAccessDisplayContext.getSearchContainer() %>"
+			>
+				<liferay-ui:search-container-row
+					className="com.liferay.portal.kernel.model.Role"
+					escapedModel="<%= true %>"
+					keyProperty="roleId"
+					modelVar="role"
+				>
+					<liferay-ui:search-container-column-text
+						name="title"
+						truncate="<%= true %>"
+						value="<%= HtmlUtil.escape(role.getTitle(locale)) %>"
+					/>
+
+					<liferay-ui:search-container-column-text>
+						<clay:button
+							aria-label='<%= LanguageUtil.get(request, "remove") %>'
+							borderless="<%= true %>"
+							cssClass="lfr-portal-tooltip modify-link"
+							data-rowId="<%= role.getRoleId() %>"
+							displayType="secondary"
+							icon="times-circle"
+							monospaced="<%= true %>"
+							title='<%= LanguageUtil.get(request, "remove") %>'
+							type="button"
+						/>
+					</liferay-ui:search-container-column-text>
+				</liferay-ui:search-container-row>
+
+				<liferay-ui:search-iterator
+					markupView="lexicon"
+					paginate="<%= false %>"
+				/>
+			</liferay-ui:search-container>
+		</clay:content-col>
+	</clay:content-row>
+</clay:sheet-section>
+
+<aui:script use="liferay-search-container">
+	const bindModifyLink = function (config) {
+		const searchContainer = config.searchContainer;
+
+		searchContainer.get('contentBox').delegate(
+			'click',
+			(event) => {
+				const link = event.currentTarget;
+
+				searchContainer.deleteRow(
+					link.ancestor('tr'),
+					link.getAttribute('data-rowId')
+				);
+			},
+			'.modify-link'
+		);
+	};
+
+	const bindSelectLink = function (config) {
+		const searchContainer = config.searchContainer;
+
+		const selectLink = document.getElementById(config.linkId);
+
+		selectLink.addEventListener('click', (event) => {
+			let searchContainerData = searchContainer.getData();
+
+			if (!searchContainerData.length) {
+				searchContainerData = [];
+			}
+			else {
+				searchContainerData = searchContainerData.split(',');
+			}
+
+			Liferay.Util.openSelectionModal({
+				onSelect: function (selectedItems) {
+					if (!selectedItems || !selectedItems.length) {
+						return;
+					}
+					selectedItems.map((item) => {
+						if (!searchContainerData.includes(item.id)) {
+							const rowColumns = [
+								Liferay.Util.escape(item.name),
+								'<button aria-label="<%= LanguageUtil.get(request, "remove") %>" class="btn btn-monospaced btn-outline-borderless btn-outline-secondary float-right lfr-portal-tooltip modify-link" data-rowId="' +
+									item.id +
+									'" title="<%= LanguageUtil.get(request, "remove") %>" type="button"><%= UnicodeFormatter.toString(removeRoleIcon) %></button>',
+							];
+
+							searchContainer.addRow(rowColumns, item.id);
+						}
+					});
+
+					searchContainer.updateDataStore();
+				},
+				selectEventName: config.id,
+				selectedData: searchContainerData,
+				title: config.title,
+				url: config.uri.toString(),
+				multiple: true,
+			});
+		});
+	};
+
+	const roleConfig = {
+		id: '<%= menuAccessDisplayContext.getEventName() %>>',
+		idAttr: 'roleid',
+		inputId: '<portlet:namespace />roleSearchContainerPrimaryKeys',
+		linkId: '<portlet:namespace />selectRoleLink',
+		searchContainer: Liferay.SearchContainer.get(
+			'<portlet:namespace />roleSearchContainer'
+		),
+		title: '<liferay-ui:message arguments="role" key="select-x" />',
+		titleAttr: 'roletitle',
+		uri: '<%= menuAccessDisplayContext.getRoleItemSelectorURL() %>',
+	};
+
+	bindModifyLink(roleConfig);
+	bindSelectLink(roleConfig);
+</aui:script>
