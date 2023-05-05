aee7279e0bba8d7245f683c27fc87a026df53473
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-05-05 13:27:47 +0100

    LPS-183735 Auto

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/http/FragmentEntryServiceHttp.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/http/FragmentEntryServiceHttp.java
@@ -1291,6 +1291,47 @@ public class FragmentEntryServiceHttp {
 
 	public static com.liferay.fragment.model.FragmentEntry updateFragmentEntry(
 			HttpPrincipal httpPrincipal, long fragmentEntryId,
+			boolean cacheable)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				FragmentEntryServiceUtil.class, "updateFragmentEntry",
+				_updateFragmentEntryParameterTypes33);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, fragmentEntryId, cacheable);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.PortalException) {
+
+					throw (com.liferay.portal.kernel.exception.PortalException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (com.liferay.fragment.model.FragmentEntry)returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	public static com.liferay.fragment.model.FragmentEntry updateFragmentEntry(
+			HttpPrincipal httpPrincipal, long fragmentEntryId,
 			long fragmentCollectionId, String name, String css, String html,
 			String js, boolean cacheable, String configuration, String icon,
 			long previewFileEntryId, int status)
@@ -1299,7 +1340,7 @@ public class FragmentEntryServiceHttp {
 		try {
 			MethodKey methodKey = new MethodKey(
 				FragmentEntryServiceUtil.class, "updateFragmentEntry",
-				_updateFragmentEntryParameterTypes33);
+				_updateFragmentEntryParameterTypes34);
 
 			MethodHandler methodHandler = new MethodHandler(
 				methodKey, fragmentEntryId, fragmentCollectionId, name, css,
@@ -1344,7 +1385,7 @@ public class FragmentEntryServiceHttp {
 		try {
 			MethodKey methodKey = new MethodKey(
 				FragmentEntryServiceUtil.class, "updateFragmentEntry",
-				_updateFragmentEntryParameterTypes34);
+				_updateFragmentEntryParameterTypes35);
 
 			MethodHandler methodHandler = new MethodHandler(
 				methodKey, fragmentEntryId, fragmentCollectionId, name, css,
@@ -1386,7 +1427,7 @@ public class FragmentEntryServiceHttp {
 		try {
 			MethodKey methodKey = new MethodKey(
 				FragmentEntryServiceUtil.class, "updateFragmentEntry",
-				_updateFragmentEntryParameterTypes35);
+				_updateFragmentEntryParameterTypes36);
 
 			MethodHandler methodHandler = new MethodHandler(
 				methodKey, fragmentEntryId, name);
@@ -1543,18 +1584,20 @@ public class FragmentEntryServiceHttp {
 	private static final Class<?>[] _updateFragmentEntryParameterTypes32 =
 		new Class[] {long.class, long.class};
 	private static final Class<?>[] _updateFragmentEntryParameterTypes33 =
+		new Class[] {long.class, boolean.class};
+	private static final Class<?>[] _updateFragmentEntryParameterTypes34 =
 		new Class[] {
 			long.class, long.class, String.class, String.class, String.class,
 			String.class, boolean.class, String.class, String.class, long.class,
 			int.class
 		};
-	private static final Class<?>[] _updateFragmentEntryParameterTypes34 =
+	private static final Class<?>[] _updateFragmentEntryParameterTypes35 =
 		new Class[] {
 			long.class, long.class, String.class, String.class, String.class,
 			String.class, boolean.class, String.class, String.class, long.class,
 			String.class, int.class
 		};
-	private static final Class<?>[] _updateFragmentEntryParameterTypes35 =
+	private static final Class<?>[] _updateFragmentEntryParameterTypes36 =
 		new Class[] {long.class, String.class};
 
 }
