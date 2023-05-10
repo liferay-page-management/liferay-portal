1b2ef7cd7c3958c7772818e4b0dc3b3bb0907a45
Rubén Pulido <ruben.pulido@liferay.com>
2023-05-11 01:33:52 +0200

    LPS-181931 When creating a page use taxonomyCategoryReferences within taxonomyCategoryBriefs to associate categories to the page.

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/SitePageResourceImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/SitePageResourceImpl.java
@@ -14,6 +14,8 @@
 
 package com.liferay.headless.delivery.internal.resource.v1_0;
 
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
 import com.liferay.client.extension.constants.ClientExtensionEntryConstants;
 import com.liferay.client.extension.service.ClientExtensionEntryRelLocalService;
 import com.liferay.client.extension.type.CET;
@@ -39,6 +41,8 @@ import com.liferay.headless.delivery.dto.v1_0.SiteMapSettings;
 import com.liferay.headless.delivery.dto.v1_0.SitePage;
 import com.liferay.headless.delivery.dto.v1_0.SitePageNavigationMenuSettings;
 import com.liferay.headless.delivery.dto.v1_0.StyleBook;
+import com.liferay.headless.delivery.dto.v1_0.TaxonomyCategoryBrief;
+import com.liferay.headless.delivery.dto.v1_0.TaxonomyCategoryReference;
 import com.liferay.headless.delivery.dto.v1_0.util.CustomFieldsUtil;
 import com.liferay.headless.delivery.internal.odata.entity.v1_0.SitePageEntityModel;
 import com.liferay.headless.delivery.resource.v1_0.SitePageResource;
@@ -79,6 +83,7 @@ import com.liferay.portal.kernel.search.filter.TermFilter;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.LayoutService;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
@@ -127,6 +132,7 @@ import com.liferay.style.book.service.StyleBookEntryLocalService;
 
 import java.io.Serializable;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -478,7 +484,78 @@ public class SitePageResourceImpl extends BaseSitePageResourceImpl {
 
 		Long[] assetCategoryIds = {};
 
-		if (sitePage.getTaxonomyCategoryIds() != null) {
+		TaxonomyCategoryBrief[] taxonomyCategoryBriefs =
+			sitePage.getTaxonomyCategoryBriefs();
+
+		if (taxonomyCategoryBriefs != null) {
+			List<TaxonomyCategoryReference> taxonomyCategoryReferences =
+				new ArrayList<>();
+
+			for (TaxonomyCategoryBrief taxonomyCategoryBrief :
+					taxonomyCategoryBriefs) {
+
+				TaxonomyCategoryReference taxonomyCategoryReference =
+					taxonomyCategoryBrief.getTaxonomyCategoryReference();
+
+				if (taxonomyCategoryReference != null) {
+					taxonomyCategoryReferences.add(taxonomyCategoryReference);
+				}
+			}
+
+			List<Long> assetCategoryIdsList = new ArrayList<>();
+
+			for (TaxonomyCategoryReference taxonomyCategoryReference :
+					taxonomyCategoryReferences) {
+
+				long assetCategoryGroupId = groupId;
+
+				String siteKey = taxonomyCategoryReference.getSiteKey();
+
+				if (siteKey != null) {
+					Group group = _groupLocalService.fetchGroup(
+						contextCompany.getCompanyId(), siteKey);
+
+					if (group == null) {
+						if (_log.isWarnEnabled()) {
+							_log.warn(
+								StringBundler.concat(
+									"Could not find group for company ID ",
+									contextCompany.getCompanyId(),
+									" and site key ", siteKey));
+						}
+
+						continue;
+					}
+
+					assetCategoryGroupId = group.getGroupId();
+				}
+
+				String externalReferenceCode =
+					taxonomyCategoryReference.getExternalReferenceCode();
+
+				AssetCategory assetCategory =
+					_assetCategoryLocalService.
+						fetchAssetCategoryByExternalReferenceCode(
+							externalReferenceCode, assetCategoryGroupId);
+
+				if (assetCategory == null) {
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							StringBundler.concat(
+								"Could not find category for external ",
+								"reference code ", externalReferenceCode,
+								" and group ID ", assetCategoryGroupId));
+					}
+
+					continue;
+				}
+
+				assetCategoryIdsList.add(assetCategory.getCategoryId());
+			}
+
+			assetCategoryIds = assetCategoryIdsList.toArray(new Long[0]);
+		}
+		else if (sitePage.getTaxonomyCategoryIds() != null) {
 			assetCategoryIds = sitePage.getTaxonomyCategoryIds();
 		}
 
@@ -1123,6 +1200,9 @@ public class SitePageResourceImpl extends BaseSitePageResourceImpl {
 	private static final EntityModel _entityModel = new SitePageEntityModel();
 
 	@Reference
+	private AssetCategoryLocalService _assetCategoryLocalService;
+
+	@Reference
 	private CETManager _cetManager;
 
 	@Reference
@@ -1143,6 +1223,9 @@ public class SitePageResourceImpl extends BaseSitePageResourceImpl {
 
 	@Reference
 	private FriendlyURLEntryLocalService _friendlyURLEntryLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
 
 	@Reference
 	private JSONFactory _jsonFactory;
