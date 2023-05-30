efcfc031f7173fdbc981dc09df87c4b51a157cc5
Yang Cao <yang.cao@liferay.com>
2023-05-30 15:11:18 +0800

    LPS-179416 Add test to post asset library structured content with default erc and uuid

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/StructuredContentResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/StructuredContentResourceTest.java
@@ -434,6 +434,14 @@ public class StructuredContentResourceTest
 
 	@Override
 	@Test
+	public void testPostAssetLibraryStructuredContent() throws Exception {
+		super.testPostAssetLibraryStructuredContent();
+
+		_testPostAssetLibraryStructuredContentWithDefaultERCAndUUID();
+	}
+
+	@Override
+	@Test
 	public void testPostSiteStructuredContent() throws Exception {
 		super.testPostSiteStructuredContent();
 
@@ -1123,6 +1131,27 @@ public class StructuredContentResourceTest
 			"dependencies/" + fileName);
 
 		return StringUtil.read(inputStream);
+	}
+
+	private void _testPostAssetLibraryStructuredContentWithDefaultERCAndUUID()
+		throws Exception {
+
+		StructuredContent randomStructuredContent = randomStructuredContent();
+
+		randomStructuredContent.setExternalReferenceCode("");
+
+		randomStructuredContent.setUuid("");
+
+		StructuredContent postStructuredContent =
+			testPostAssetLibraryStructuredContent_addStructuredContent(
+				randomStructuredContent);
+
+		Assert.assertNotNull(postStructuredContent.getExternalReferenceCode());
+		Assert.assertNotNull(postStructuredContent.getUuid());
+		Assert.assertEquals(
+			postStructuredContent.getExternalReferenceCode(),
+			postStructuredContent.getUuid());
+		assertValid(postStructuredContent);
 	}
 
 	private static final String[] _COMPLETE_STRUCTURED_CONTENT_OPTIONS = {
