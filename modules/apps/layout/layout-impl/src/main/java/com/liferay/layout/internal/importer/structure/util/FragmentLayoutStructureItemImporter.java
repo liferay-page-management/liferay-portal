f2d423178874344e009d1b1e99b3d393cd2eeab9
Victor Galan <victor.galan@liferay.com>
2023-06-21 16:43:41 +0200

    LPS-187869 Avoid null pointer exception

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/importer/structure/util/FragmentLayoutStructureItemImporter.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/importer/structure/util/FragmentLayoutStructureItemImporter.java
@@ -889,6 +889,10 @@ public class FragmentLayoutStructureItemImporter
 		LayoutStructureItemImporterContext layoutStructureItemImporterContext,
 		Map<String, Object> onResultMap, String resultType) {
 
+		if (onResultMap == null) {
+			return;
+		}
+
 		JSONObject resultJSONObject = _jsonFactory.createJSONObject();
 
 		configJSONObject.put(resultType, resultJSONObject);
