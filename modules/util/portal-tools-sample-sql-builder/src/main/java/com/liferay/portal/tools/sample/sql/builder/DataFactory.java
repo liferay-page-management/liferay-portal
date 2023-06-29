c3a0e86e7983473efd07b00c494af79e6b73d526
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-06-29 14:40:01 +0200

    LPS-189124 Take into account the correct name of method

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -7107,6 +7107,9 @@ public class DataFactory {
 				else if (name.equals("CIWarehouseItemId")) {
 					name = "CommerceInventoryWarehouseItemId";
 				}
+				else if (name.equals("CmExternalReferenceCode")) {
+					name = "CMExternalReferenceCode";
+				}
 				else if (name.equals("CPDSpecificationOptionValueId")) {
 					name = "CPDefinitionSpecificationOptionValueId";
 				}
