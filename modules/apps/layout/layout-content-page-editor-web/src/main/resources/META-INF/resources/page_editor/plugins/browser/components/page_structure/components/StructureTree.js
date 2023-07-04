672bf3f88694244918e7f78da620729aac710e18
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2023-07-04 12:18:27 +0200

    LPS-185796 Add span with sr-only so that the screen reader correctly reads the node name

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/browser/components/page_structure/components/StructureTree.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/browser/components/page_structure/components/StructureTree.js
@@ -413,6 +413,8 @@ export default function PageStructureSidebar() {
 									hoverItem(item.id);
 								}}
 							>
+								<span className="sr-only">{item.name}</span>
+
 								<StructureTreeNode
 									node={item}
 									setEditingNodeId={setEditingNodeId}
@@ -424,6 +426,10 @@ export default function PageStructureSidebar() {
 									<ClayTreeView.Item
 										actions={<ItemActions item={item} />}
 									>
+										<span className="sr-only">
+											{item.name}
+										</span>
+
 										<StructureTreeNode
 											node={item}
 											setEditingNodeId={setEditingNodeId}
