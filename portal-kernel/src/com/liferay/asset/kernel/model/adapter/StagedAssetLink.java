4f698ce44bc58ebf005d6445895cf306b69c3d02
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-07-30 20:28:01 +0100

    LPS-192105 Deprecated old classes

--- portal-kernel/src/com/liferay/asset/kernel/model/adapter/StagedAssetLink.java
+++ portal-kernel/src/com/liferay/asset/kernel/model/adapter/StagedAssetLink.java
@@ -10,7 +10,10 @@ import com.liferay.portal.kernel.model.StagedModel;
 
 /**
  * @author Máté Thurzó
+ * @deprecated As of Cavanaugh (7.4.x), replaced by {@link
+ *             com.liferay.asset.link.model.adapter.StagedAssetLink}
  */
+@Deprecated
 public interface StagedAssetLink extends AssetLink, StagedModel {
 
 	public String getEntry1ClassName();
