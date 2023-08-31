06e4c9d53dcc5ddf474ef22cf6aa560306f331d0
Carolina Barbosa <carolina.barbosa@liferay.com>
2023-08-31 08:56:16 -0300

    LPS-195029 Remove feature flag for: LPS-182586 As an object admin, I want to define the output field of my object validation

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldLocalServiceImpl.java
@@ -1083,10 +1083,7 @@ public class ObjectFieldLocalServiceImpl
 				objectField.getObjectFieldId());
 		}
 
-		if (FeatureFlagManagerUtil.isEnabled("LPS-187846")) {
-			_objectValidationRuleLocalService.unassociateObjectField(
-				objectField);
-		}
+		_objectValidationRuleLocalService.unassociateObjectField(objectField);
 
 		_objectViewLocalService.unassociateObjectField(objectField);
 
