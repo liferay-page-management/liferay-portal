b1a6010fe0a04562cd750cd0c1f562ee0f329fa3
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2023-09-27 15:33:54 +0200

    LPS-197435 Migrate selectEditableValues to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/selectors/selectEditableValues.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/selectors/selectEditableValues.ts
@@ -3,9 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import type {FragmentEntryLinkMap} from '../actions/addFragmentEntryLinks';
+
 export default function selectEditableValues(
-	{fragmentEntryLinks},
-	fragmentEntryLinkId
+	{fragmentEntryLinks}: {fragmentEntryLinks: FragmentEntryLinkMap},
+	fragmentEntryLinkId: string
 ) {
 	const fragmentEntryLink = fragmentEntryLinks[fragmentEntryLinkId];
 
