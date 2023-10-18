440d140504f1db001797bc029fb900c7d1c6485c
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2023-10-18 19:54:07 +0200

    LPS-199340 Add openCategorySelectionModal to open a modal to select categories

---
+++ modules/apps/frontend-js/frontend-js-components-web/src/main/resources/META-INF/resources/commands/openCategorySelectionModal.ts
@@ -0,0 +1,58 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {addParams, navigate, openSelectionModal} from 'frontend-js-web';
+
+type Category = {
+	categoryId: string;
+	className: string;
+	classNameId: string;
+	classPK: string;
+	nodePath: string;
+	title: string;
+	value: string;
+	vocabularyId: number;
+};
+
+export default function openCategorySelectionModal({
+	portletNamespace,
+	redirectURL,
+	selectCategoryURL,
+	title,
+}: {
+	portletNamespace: string;
+	redirectURL: string;
+	selectCategoryURL: string;
+	title?: string;
+}) {
+	openSelectionModal({
+		buttonAddLabel: Liferay.Language.get('select'),
+		height: '70vh',
+		iframeBodyCssClass: '',
+		multiple: true,
+		onSelect: (selectedItems: Record<string, Category>) => {
+			if (!Object.keys(selectedItems).length) {
+				return;
+			}
+
+			let url = redirectURL;
+
+			const assetCategories = Object.keys(selectedItems);
+
+			assetCategories.forEach((assetCategory) => {
+				url = addParams(
+					`${portletNamespace}assetCategoryId=${selectedItems[assetCategory].categoryId}`,
+					url
+				);
+			});
+
+			navigate(url);
+		},
+		selectEventName: `${portletNamespace}selectedAssetCategory`,
+		size: 'md',
+		title: title || Liferay.Language.get('filter-by-categories'),
+		url: selectCategoryURL,
+	});
+}
--- modules/apps/frontend-js/frontend-js-components-web/src/main/resources/META-INF/resources/index.ts
+++ modules/apps/frontend-js/frontend-js-components-web/src/main/resources/META-INF/resources/index.ts
@@ -9,6 +9,7 @@ export {default as BetaButton} from './beta_indicator/BetaButton';
 export {default as ClientExtension} from './ClientExtension';
 export {default as CodeMirrorKeyboardMessage} from './code_mirror_keyboard_message/CodeMirrorKeyboardMessage';
 
+export {default as openCategorySelectionModal} from './commands/openCategorySelectionModal';
 export {default as openTagSelectionModal} from './commands/openTagSelectionModal';
 
 export {default as useId} from './hooks/useId';
