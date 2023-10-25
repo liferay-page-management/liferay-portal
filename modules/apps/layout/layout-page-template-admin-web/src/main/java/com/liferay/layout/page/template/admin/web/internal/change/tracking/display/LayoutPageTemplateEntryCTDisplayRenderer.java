cf9517f2a6c4f38ad2ee83c893b36f17ff804705
pei-jung <pei-jung.lan@liferay.com>
2023-10-24 18:08:06 -0700

    LPS-198982 Update LayoutPageTemplateEntry edit url to include tabs1 param

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/change/tracking/display/LayoutPageTemplateEntryCTDisplayRenderer.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/change/tracking/display/LayoutPageTemplateEntryCTDisplayRenderer.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -8,6 +8,7 @@ package com.liferay.layout.page.template.admin.web.internal.change.tracking.disp
 import com.liferay.change.tracking.spi.display.BaseCTDisplayRenderer;
 import com.liferay.change.tracking.spi.display.CTDisplayRenderer;
 import com.liferay.layout.page.template.admin.constants.LayoutPageTemplateAdminPortletKeys;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
@@ -42,6 +43,30 @@ public class LayoutPageTemplateEntryCTDisplayRenderer
 				PortletRequest.RENDER_PHASE)
 		).setMVCRenderCommandName(
 			"/layout_page_template_admin/edit_layout_page_template_entry"
+		).setTabs1(
+			() -> {
+				if ((layoutPageTemplateEntry.getType() ==
+						LayoutPageTemplateEntryTypeConstants.BASIC) ||
+					(layoutPageTemplateEntry.getType() ==
+						LayoutPageTemplateEntryTypeConstants.WIDGET_PAGE)) {
+
+					return "page-templates";
+				}
+
+				if (layoutPageTemplateEntry.getType() ==
+						LayoutPageTemplateEntryTypeConstants.DISPLAY_PAGE) {
+
+					return "display-page-templates";
+				}
+
+				if (layoutPageTemplateEntry.getType() ==
+						LayoutPageTemplateEntryTypeConstants.MASTER_LAYOUT) {
+
+					return "master-layouts";
+				}
+
+				return null;
+			}
 		).setParameter(
 			"layoutPageTemplateEntryId",
 			layoutPageTemplateEntry.getLayoutPageTemplateEntryId()
