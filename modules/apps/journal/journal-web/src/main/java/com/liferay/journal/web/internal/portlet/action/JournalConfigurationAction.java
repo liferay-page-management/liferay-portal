3e815a02ba0f383d6fe75d8d926c8f2e9f7c8bfc
Jürgen Kappler <jurgen.kappler@liferay.com>
2023-11-02 08:43:57 +0100

    LPS-199890 We are not receiving all the items at once with the new pattern, so it will fail. That's why we remove the validations, just as with those with workflow that have never been taken into account.

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/JournalConfigurationAction.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/JournalConfigurationAction.java
@@ -152,14 +152,16 @@ public class JournalConfigurationAction
 			ActionResponse actionResponse)
 		throws Exception {
 
-		validateEmail(actionRequest, "emailArticleAdded");
-		validateEmail(actionRequest, "emailArticleApprovalDenied");
-		validateEmail(actionRequest, "emailArticleApprovalGranted");
-		validateEmail(actionRequest, "emailArticleApprovalRequested");
-		validateEmail(actionRequest, "emailArticleExpired");
-		validateEmail(actionRequest, "emailArticleReview");
-		validateEmail(actionRequest, "emailArticleUpdated");
-		validateEmailFrom(actionRequest);
+		if (!FeatureFlagManagerUtil.isEnabled("LPS-197692")) {
+			validateEmail(actionRequest, "emailArticleAdded");
+			validateEmail(actionRequest, "emailArticleApprovalDenied");
+			validateEmail(actionRequest, "emailArticleApprovalGranted");
+			validateEmail(actionRequest, "emailArticleApprovalRequested");
+			validateEmail(actionRequest, "emailArticleExpired");
+			validateEmail(actionRequest, "emailArticleReview");
+			validateEmail(actionRequest, "emailArticleUpdated");
+			validateEmailFrom(actionRequest);
+		}
 
 		super.processAction(portletConfig, actionRequest, actionResponse);
 	}
