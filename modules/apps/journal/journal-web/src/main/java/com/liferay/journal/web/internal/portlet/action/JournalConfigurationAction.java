4f4914a65960d938f2deb99a322b6ca56c9c8387
Pablo Molina <contact@pablomolina.me>
2023-11-08 15:25:20 +0100

    LPS-200398 Add ItemSelector to the request

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/JournalConfigurationAction.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/JournalConfigurationAction.java
@@ -5,6 +5,7 @@
 
 package com.liferay.journal.web.internal.portlet.action;
 
+import com.liferay.item.selector.ItemSelector;
 import com.liferay.journal.configuration.JournalGroupServiceConfiguration;
 import com.liferay.journal.constants.JournalPortletKeys;
 import com.liferay.journal.web.internal.configuration.JournalWebConfiguration;
@@ -48,6 +49,9 @@ public class JournalConfigurationAction
 	@Override
 	public String getJspPath(HttpServletRequest httpServletRequest) {
 		if (FeatureFlagManagerUtil.isEnabled("LPS-197692")) {
+			httpServletRequest.setAttribute(
+				ItemSelector.class.getName(), _itemSelector);
+
 			return "/configuration_browse.jsp";
 		}
 
@@ -175,6 +179,9 @@ public class JournalConfigurationAction
 		_journalWebConfiguration = ConfigurableUtil.createConfigurable(
 			JournalWebConfiguration.class, properties);
 	}
+
+	@Reference
+	private ItemSelector _itemSelector;
 
 	private volatile JournalWebConfiguration _journalWebConfiguration;
 
