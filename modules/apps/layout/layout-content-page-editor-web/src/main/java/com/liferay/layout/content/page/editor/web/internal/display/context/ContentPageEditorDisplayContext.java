38870bde503574af808f4c552b23dcf7ea77bda2
diegohu97 <diego.hu@liferay.com>
2023-11-10 10:50:55 +0100

    LPS-201067 Add locale to the layout name

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -1543,7 +1543,7 @@ public class ContentPageEditorDisplayContext {
 				).setBackURL(
 					themeDisplay.getURLCurrent()
 				).setParameter(
-					"backURLTitle", layout.getName()
+					"backURLTitle", layout.getName(themeDisplay.getLocale())
 				).setParameter(
 					"groupId", layout.getGroupId()
 				).setParameter(
