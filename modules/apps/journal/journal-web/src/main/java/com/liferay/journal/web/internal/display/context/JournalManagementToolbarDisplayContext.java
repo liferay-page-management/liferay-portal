ddc922e1a2054019534d1bd9bdabc0501e978e9e
Rubén Pulido <ruben.pulido@liferay.com>
2023-11-13 14:04:07 +0100

    LPS-201091 Selecting "all" within the navigation filter should not reset the status filter, since this is done through status "any"

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalManagementToolbarDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalManagementToolbarDisplayContext.java
@@ -577,8 +577,6 @@ public class JournalManagementToolbarDisplayContext
 					"navigationMine", (Boolean)null
 				).setParameter(
 					"navigationRecent", (Boolean)null
-				).setParameter(
-					"status", (String)null
 				).buildPortletURL()
 			).setLabel(
 				LanguageUtil.get(httpServletRequest, "all")
