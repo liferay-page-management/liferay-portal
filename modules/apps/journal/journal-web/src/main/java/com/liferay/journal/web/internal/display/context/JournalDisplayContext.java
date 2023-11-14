e4af2602697a48dd93b7a82f61a50aed7ddec0b4
Rubén Pulido <ruben.pulido@liferay.com>
2023-11-14 02:21:19 +0100

    LPS-201091 If no search has been performed, display only items in current folder (excluding descendants)

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1470,6 +1470,12 @@ public class JournalDisplayContext {
 				_getAssetTagNamesFilter(), BooleanClauseOccur.MUST);
 		}
 
+		if (!isSearch()) {
+			booleanFilter.addTerm(
+				Field.FOLDER_ID, String.valueOf(getFolderId()),
+				BooleanClauseOccur.MUST);
+		}
+
 		if ((isNavigationMine() || isNavigationRecent()) &&
 			(_themeDisplay.getUserId() > 0)) {
 
