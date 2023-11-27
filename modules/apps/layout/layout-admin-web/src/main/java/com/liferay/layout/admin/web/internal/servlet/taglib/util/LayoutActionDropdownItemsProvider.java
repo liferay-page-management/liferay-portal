08094d3fffdc9f153e56135a307409e1ed514dd7
diegohu97 <diego.hu@liferay.com>
2023-11-27 13:29:51 +0100

    LPS-202702 Add context title to back button when export page for translation from dropdown

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/servlet/taglib/util/LayoutActionDropdownItemsProvider.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/servlet/taglib/util/LayoutActionDropdownItemsProvider.java
@@ -496,6 +496,9 @@ public class LayoutActionDropdownItemsProvider {
 
 						return portletDisplay.getId();
 					}
+				).setParameter(
+					"backURLTitle",
+					LanguageUtil.get(_themeDisplay.getLocale(), "pages")
 				).buildString());
 			dropdownItem.setIcon("upload");
 			dropdownItem.setLabel(
