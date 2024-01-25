07ff2a9442ad867179bd06786cb4606d3efb521a
NemethNorbert <norbert.nemeth@liferay.com>
2024-01-25 16:29:42 +0100

    LPD-8428 Add test for checking Undo an Redo button functionality

--- modules/test/playwright/tests/layout-content-page-editor-web/undoRedo.spec.ts
+++ modules/test/playwright/tests/layout-content-page-editor-web/undoRedo.spec.ts
@@ -79,3 +79,84 @@ test('View Undo interaction state is cleared after refreshing the page', async (
 
 	await apiHelpers.headlessSite.deleteSite(site.id);
 });
+
+test('Undo and Redo buttons work as expected', async ({
+	apiHelpers,
+	page,
+	pageEditorPage,
+}) => {
+	await page.goto('/');
+
+	// Create a site
+
+	const site = await apiHelpers.headlessSite.createSite(getRandomId());
+
+	// Create a page with a Heading fragment
+
+	const tabsId = getRandomId();
+
+	const fragmentDefinition = getFragmentDefinition(
+		tabsId,
+		'BASIC_COMPONENT-tabs'
+	);
+
+	const layout = await apiHelpers.headlessDelivery.createSitePage(
+		site.id,
+		getRandomId(),
+		getPageDefinition([fragmentDefinition])
+	);
+
+	// Go to edit mode of page
+
+	await pageEditorPage.goToEditMode(site, layout);
+
+	// assert undo button is visible
+
+	const undoButton = page.getByTitle('Undo');
+	const redoButton = page.getByTitle('Redo');
+	const tabsFragment = page.locator(`.lfr-layout-structure-item-${tabsId}`);
+
+	expect(undoButton).toBeVisible();
+	expect(redoButton).toBeVisible();
+
+	// select the fragment
+
+	await tabsFragment.click();
+
+	// Go to Styles panel and set text to Align Center
+
+	await pageEditorPage.goToConfigurationTab('General');
+	await page.getByLabel('Number of Tabs', {exact: true}).fill('5');
+	await tabsFragment.click();
+
+	await expect(tabsFragment.getByText('Tab 5')).toBeVisible();
+
+	// delete tabs fragment
+
+	await page.keyboard.press('Backspace');
+
+	await expect(tabsFragment).not.toBeAttached();
+
+	// assert undo button is enabled redo button is disabled
+
+	await expect(undoButton).toBeEnabled();
+	await expect(redoButton).toBeDisabled();
+
+	await undoButton.click();
+
+	// assert tabsfragment its present and configuration is not lost
+
+	await expect(tabsFragment).toBeAttached();
+	await expect(tabsFragment.getByText('Tab 5')).toBeVisible();
+
+	// assert Undo button is disabled and Redo button is enabled
+
+	await undoButton.click();
+
+	await expect(undoButton).toBeDisabled();
+	await expect(redoButton).toBeEnabled();
+
+	// Delete the site
+
+	await apiHelpers.headlessSite.deleteSite(site.id);
+});
