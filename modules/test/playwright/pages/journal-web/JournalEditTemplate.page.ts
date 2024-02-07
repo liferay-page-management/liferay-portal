487e897b0015262aa3299524a0eabe18eaeab921
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-02-07 23:49:57 +0100

    LPD-17215 Adds new Journal Template for edit page

---
+++ modules/test/playwright/pages/journal-web/JournalEditTemplate.page.ts
@@ -0,0 +1,33 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Locator, Page} from '@playwright/test';
+
+import {JournalPage} from './Journal.page';
+
+export class JournalEditTemplatePage {
+	readonly elementsPanel: Locator;
+	readonly page: Page;
+	readonly saveButton: Locator;
+
+	journalPage: JournalPage;
+
+	constructor(page: Page) {
+		this.page = page;
+
+		this.elementsPanel = page.getByTitle('Elements', {exact: true});
+		this.journalPage = new JournalPage(page);
+		this.saveButton = page.getByRole('button', {exact: true, name: 'Save'});
+	}
+
+	async goto() {
+		await this.journalPage.goToCreateNewTemplate();
+	}
+
+	async gotoElements() {
+		await this.journalPage.goToCreateNewTemplate();
+		await this.elementsPanel.click();
+	}
+}
