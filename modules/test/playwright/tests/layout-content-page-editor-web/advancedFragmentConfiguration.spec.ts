87c8bb9336d65e98dd5748b00878f5c176730fe3
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2024-03-14 09:37:16 +0100

    LPD-20242 Migrate test to check that the advanced configuration of a fragment appears in its corresponding tab

--- modules/test/playwright/tests/layout-content-page-editor-web/advancedFragmentConfiguration.spec.ts
+++ modules/test/playwright/tests/layout-content-page-editor-web/advancedFragmentConfiguration.spec.ts
@@ -7,9 +7,11 @@ import {expect, mergeTests} from '@playwright/test';
 
 import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
 import {applicationsMenuPageTest} from '../../fixtures/applicationsMenuPageTest';
+import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
 import {isolatedSiteTest} from '../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../fixtures/loginTest';
 import getRandomString from '../../utils/getRandomString';
+import {PORTLET_URLS} from '../../utils/portletUrls';
 import {pageEditorPagesTest} from './fixtures/pageEditorPagesTest';
 import getContainerDefinition from './utils/getContainerDefinition';
 import getFragmentDefinition from './utils/getFragmentDefinition';
@@ -19,6 +21,9 @@ import getWidgetDefinition from './utils/getWidgetDefinition';
 export const test = mergeTests(
 	apiHelpersTest,
 	applicationsMenuPageTest,
+	featureFlagsTest({
+		'LPS-178052': true,
+	}),
 	loginTest(),
 	isolatedSiteTest,
 	pageEditorPagesTest
@@ -177,4 +182,107 @@ test('checks that the fragment is hidden from Site Search Results', async ({
 	// Check that there are no results
 
 	await expect(searchResults).not.toBeVisible();
+});
+
+test('checks that the advanced configuration of a fragment appears in its corresponding tab', async ({
+	apiHelpers,
+	page,
+	pageEditorPage,
+	site,
+}) => {
+
+	// Go to fragment editor
+
+	await page.goto(
+		`/group${site.friendlyUrlPath}${PORTLET_URLS.fragmentPage}`
+	);
+
+	// Add a new fragment set
+
+	await page.getByTitle('Add Fragment Set').click();
+
+	await page.getByPlaceholder('Name').fill('my fragment set');
+
+	await page.getByRole('button', {name: 'Save'}).click();
+
+	// Add a new fragment inside the new fragment set
+
+	await page.getByRole('button', {name: 'Add'}).click();
+
+	await page.getByRole('button', {name: 'Next'}).click();
+
+	await page.getByLabel('Name').fill('my fragment');
+
+	await page.getByText('Add', {exact: true}).click();
+
+	const sucessMessage = await page.getByText(
+		'Success:Your request completed successfully.'
+	);
+
+	await sucessMessage.waitFor();
+
+	// Add a configuration for the fragment
+
+	await page.getByRole('tab', {name: 'Configuration'}).click();
+
+	// Edit config: first delete the current config and then add the new config
+
+	const codeMirror = await page.locator('.CodeMirror-scroll').last();
+
+	codeMirror.waitFor();
+
+	codeMirror.click({timeout: 1000});
+
+	await page.getByText('fieldSets').click();
+
+	const config = {
+		configurationRole: 'advanced',
+		fields: [
+			{
+				dataType: 'string',
+				defaultValue: '1',
+				label: 'Advanced Config Field',
+				name: 'advancedConfigField',
+				type: 'select',
+				typeOptions: {
+					validValues: [{label: '1', value: '1'}],
+				},
+			},
+		],
+		label: 'Advanced Config Fieldset',
+	};
+
+	await page.keyboard.type(JSON.stringify(config));
+
+	await page.waitForTimeout(3000);
+
+	await page.getByRole('button', {name: 'Publish'}).click({timeout: 3000});
+
+	await sucessMessage.waitFor();
+
+	// Create a content page with the fragment previously created
+
+	const fragmentDefinition = getFragmentDefinition(
+		getRandomString(),
+		'my-fragment',
+		{
+			advancedConfigField: '1',
+		}
+	);
+
+	await pageEditorPage.createPageWithFragmentAndGoToEditMode({
+		apiHelpers,
+		fragment: fragmentDefinition,
+		site,
+	});
+
+	await page.getByTitle('Browser').click();
+
+	await page.getByLabel('Select My Fragment').click();
+
+	await pageEditorPage.goToConfigurationTab('Advanced');
+
+	await expect(
+		page.getByLabel('Advanced Config Field', {exact: true})
+	).toBeVisible();
 });
