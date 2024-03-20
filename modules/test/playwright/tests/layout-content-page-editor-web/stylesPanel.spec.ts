6a7b5571fdc43363117b1a29e7ded5f3cea02f50
Sandro Chinea <sandrochinea@gmail.com>
2024-03-20 23:14:50 +0000

    LPD-20906 Wait for saved message in page methods

--- modules/test/playwright/tests/layout-content-page-editor-web/stylesPanel.spec.ts
+++ modules/test/playwright/tests/layout-content-page-editor-web/stylesPanel.spec.ts
@@ -57,7 +57,7 @@ const COLOR_PICKER_PALETTES = [
 	},
 ];
 
-test('allows changing, resetting spacing and verify that the save icon appears when changes are made', async ({
+test('allows changing and resetting spacing', async ({
 	apiHelpers,
 	page,
 	pageEditorPage,
@@ -82,9 +82,9 @@ test('allows changing, resetting spacing and verify that the save icon appears w
 
 	await pageEditorPage.goToEditMode(layout, site.friendlyUrlPath);
 
-	const savedIcon = await page.getByLabel('Saved');
+	// Check Saved icon is not visible at the beggining
 
-	expect(savedIcon).not.toBeVisible();
+	await expect(page.getByLabel('Saved')).not.toBeVisible();
 
 	// Change Margin Top with custom value and check change is applied
 
@@ -112,13 +112,6 @@ test('allows changing, resetting spacing and verify that the save icon appears w
 	expect(await pageEditorPage.getFragmentStyle(headingId, 'marginTop')).toBe(
 		'0px'
 	);
-
-	expect(savedIcon).toBeVisible();
-	expect(
-		page.getByText(
-			'Changes have been saved. Page editor will autosave new changes.'
-		)
-	).toBeVisible();
 });
 
 test('renders all selectors with correct default values', async ({
