9ea4de5af1449cdc2c401bfb7c5759b7313a3c98
Sandro Chinea <sandrochinea@gmail.com>
2024-04-08 15:24:45 +0100

    LPD-22633 Tests need to explicitly use login fixture

--- modules/test/playwright/tests/frontend-data-set-views-web/settings.spec.ts
+++ modules/test/playwright/tests/frontend-data-set-views-web/settings.spec.ts
@@ -205,7 +205,8 @@ export const fragmentTest = mergeTests(
 		'LPS-178052': true,
 	}),
 	fdsFragmentPageTest,
-	isolatedSiteTest
+	isolatedSiteTest,
+	loginTest()
 );
 
 fragmentTest.describe('Data Set Default Visualization Mode in fragment', () => {
