fe62f927a16d84cb436097fc1b10d4ebdb7aaeec
Attila Bakay <attila.bakay@liferay.com>
2024-04-26 15:08:40 +0200

    LPD-24298 Update usages

--- modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/exception/DuplicateLayoutPageTemplateCollectionExternalReferenceCodeException.java
+++ modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/exception/DuplicateLayoutPageTemplateCollectionExternalReferenceCodeException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.layout.page.template.exception;
 
 import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeException;
@@ -9,20 +10,27 @@ import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeExcepti
 /**
  * @author Brian Wing Shun Chan
  */
-public class DuplicateLayoutPageTemplateCollectionExternalReferenceCodeException extends DuplicateExternalReferenceCodeException {
+public class DuplicateLayoutPageTemplateCollectionExternalReferenceCodeException
+	extends DuplicateExternalReferenceCodeException {
 
 	public DuplicateLayoutPageTemplateCollectionExternalReferenceCodeException() {
 	}
 
-	public DuplicateLayoutPageTemplateCollectionExternalReferenceCodeException(String msg) {
+	public DuplicateLayoutPageTemplateCollectionExternalReferenceCodeException(
+		String msg) {
+
 		super(msg);
 	}
 
-	public DuplicateLayoutPageTemplateCollectionExternalReferenceCodeException(String msg, Throwable throwable) {
+	public DuplicateLayoutPageTemplateCollectionExternalReferenceCodeException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
-	public DuplicateLayoutPageTemplateCollectionExternalReferenceCodeException(Throwable throwable) {
+	public DuplicateLayoutPageTemplateCollectionExternalReferenceCodeException(
+		Throwable throwable) {
+
 		super(throwable);
 	}
 
