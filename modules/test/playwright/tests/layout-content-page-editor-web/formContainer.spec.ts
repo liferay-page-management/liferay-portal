25f5c0750a19cb76322acb7f4c009659a252d5c4
Sandro Chinea <sandrochinea@gmail.com>
2024-06-25 11:46:09 +0100

    LPD-28916 Add test to cover LPP-47869 related bug

--- modules/test/playwright/tests/layout-content-page-editor-web/formContainer.spec.ts
+++ modules/test/playwright/tests/layout-content-page-editor-web/formContainer.spec.ts
@@ -61,3 +61,47 @@ test('allow selecting fields from main object and relationships in fields modal'
 	await expect(form.getByText('Lemon Size')).toBeVisible();
 	await expect(form.getByText('Lemon Basket Color')).toBeVisible();
 });
+
+test('shows correct options in picklist field selected as title in related object', async ({
+	apiHelpers,
+	page,
+	pageEditorPage,
+	wemSite,
+}) => {
+
+	// Create a page with a Form fragment
+
+	const formId = getRandomString();
+
+	const formDefinition = getFormContainerDefinition({
+		id: formId,
+	});
+
+	const layout = await apiHelpers.headlessDelivery.createSitePage({
+		pageDefinition: getPageDefinition([formDefinition]),
+		siteId: wemSite.id,
+		title: getRandomString(),
+	});
+
+	// Go to edit mode
+
+	await pageEditorPage.goto(layout, wemSite.friendlyUrlPath);
+
+	// Map the form to Lemon Basket object, select fields and publish
+
+	await pageEditorPage.mapFormFragment(formId, 'Lemon', [
+		'Lemon Size',
+		'Lemon Basket to Lemons',
+	]);
+
+	await pageEditorPage.publishPage();
+
+	// Go to view mode and check picklist values
+
+	await page.goto(`/web${wemSite.friendlyUrlPath}${layout.friendlyUrlPath}`);
+
+	await page.getByLabel('Lemon Basket to Lemons').click();
+
+	await expect(page.getByText('Plastic', {exact: true})).toBeVisible();
+	await expect(page.getByText('Carton', {exact: true})).toBeVisible();
+});
