d06b5960899b47567e8bb9f1e5022056e48963a3
Victor Galan <victor.galan@liferay.com>
2024-07-15 11:32:57 +0200

    LPD-31389 This doesn't work for objects

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/experience/components/ExperienceToolbarSection.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/experience/components/ExperienceToolbarSection.js
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {isNullOrUndefined} from '@liferay/layout-js-components-web';
 import React, {useEffect, useMemo} from 'react';
 
 import {loadReducer} from '../../../app/actions';
@@ -97,7 +96,8 @@ export default function ExperienceToolbarSectionWrapper() {
 	}, [dispatch]);
 
 	if (
-		isNullOrUndefined(availableSegmentsExperiences) ||
+		!availableSegmentsExperiences ||
+		!Object.keys(availableSegmentsExperiences).length ||
 		config.singleSegmentsExperienceMode
 	) {
 		return null;
