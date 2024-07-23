a9e5f715bd48214d24eee1cc2f8c0779757b1955
Rubén Pulido <ruben.pulido@liferay.com>
2024-07-23 17:48:42 +0200

    LPD-31852 Remove since navigation item of type submenu is going to be included in _testGetNavigationMenuWithSubmenuAndPageNavigationItems

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/NavigationMenuResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/NavigationMenuResourceTest.java
@@ -52,7 +52,6 @@ import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.portal.util.PropsValues;
-import com.liferay.site.navigation.menu.item.layout.constants.SiteNavigationMenuItemTypeConstants;
 import com.liferay.site.navigation.model.SiteNavigationMenuItem;
 import com.liferay.site.navigation.service.SiteNavigationMenuItemLocalService;
 
@@ -222,7 +221,6 @@ public class NavigationMenuResourceTest
 			JournalArticle.class.getName(), journalArticle.getTitle(),
 			"structuredContent", false);
 
-		_testGetNavigationMenuWithSubmenu();
 	}
 
 	@Override
@@ -722,56 +720,6 @@ public class NavigationMenuResourceTest
 			Assert.assertEquals(title, navigationMenuItem.getName());
 			Assert.assertFalse(navigationMenuItem.getUseCustomName());
 		}
-	}
-
-	private void _testGetNavigationMenuWithSubmenu() throws Exception {
-		NavigationMenu postNavigationMenu =
-			testGetNavigationMenu_addNavigationMenu();
-
-		String nameEnUS = RandomTestUtil.randomString();
-		String nameEsES = RandomTestUtil.randomString();
-
-		SiteNavigationMenuItem siteNavigationMenuItem =
-			_siteNavigationMenuItemLocalService.addSiteNavigationMenuItem(
-				null, TestPropsValues.getUserId(), testGroup.getGroupId(),
-				postNavigationMenu.getId(), 0,
-				SiteNavigationMenuItemTypeConstants.NODE,
-				UnicodePropertiesBuilder.create(
-					true
-				).put(
-					"defaultLanguageId", "en_US"
-				).put(
-					"name_en_US", nameEnUS
-				).put(
-					"name_es_ES", nameEsES
-				).buildString(),
-				ServiceContextTestUtil.getServiceContext(
-					testGroup.getGroupId(), TestPropsValues.getUserId()));
-
-		NavigationMenuResource navigationMenuResource =
-			_buildNavigationMenuResource();
-
-		NavigationMenu getNavigationMenu =
-			navigationMenuResource.getNavigationMenu(
-				postNavigationMenu.getId());
-
-		assertValid(getNavigationMenu);
-
-		NavigationMenuItem navigationMenuItem =
-			getNavigationMenu.getNavigationMenuItems()[0];
-
-		Assert.assertEquals(
-			siteNavigationMenuItem.getSiteNavigationMenuItemId(),
-			GetterUtil.getLong(navigationMenuItem.getId()));
-		Assert.assertEquals(nameEnUS, navigationMenuItem.getName());
-
-		Map<String, String> nameI18nMap = navigationMenuItem.getName_i18n();
-
-		Assert.assertEquals(nameEnUS, nameI18nMap.get("en-US"));
-		Assert.assertEquals(nameEsES, nameI18nMap.get("es-ES"));
-
-		Assert.assertEquals("navigationMenu", navigationMenuItem.getType());
-		Assert.assertFalse(navigationMenuItem.getUseCustomName());
 	}
 
 	private void _testGetSiteNavigationMenusPage(
