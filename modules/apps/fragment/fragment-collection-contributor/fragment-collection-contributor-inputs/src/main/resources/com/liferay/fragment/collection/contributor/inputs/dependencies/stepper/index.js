2e87c659ff4ae138e01bea4870a4166fbead63b5
Sandro Chinea <sandrochinea@gmail.com>
2024-07-30 11:56:17 +0100

    LPD-31770 Add Stepper fragment

---
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/stepper/index.js
@@ -0,0 +1,63 @@
+const ACTIVE_STEP_ID_SESSION_KEY = `stepperFragment-${fragmentNamespace}-activeStepId`;
+
+function getStepIdFromSession() {
+	return Liferay.Util.SessionStorage.getItem(
+		ACTIVE_STEP_ID_SESSION_KEY,
+		Liferay.Util.SessionStorage.TYPES.PERSONALIZATION
+	);
+}
+
+function saveStepIdInSession(stepId) {
+	Liferay.Util.SessionStorage.setItem(
+		ACTIVE_STEP_ID_SESSION_KEY,
+		stepId,
+		Liferay.Util.SessionStorage.TYPES.PERSONALIZATION
+	);
+}
+
+function setActiveStep(step) {
+	if (step.classList.contains('active')) {
+		return;
+	}
+
+	// Deactivate current active step if it exists
+
+	const activeStep = fragmentElement.querySelector('li.active');
+
+	activeStep?.classList.remove('active');
+
+	// Set new active step, save id in session if it's edit mode
+
+	step.classList.add('active');
+
+	if (layoutMode === 'edit') {
+		saveStepIdInSession(step.id);
+	}
+}
+
+function main() {
+
+	// Set initial active step, get it from session if it's edit mode
+
+	let activeStep = fragmentElement.querySelector('li');
+
+	const sessionStepId = getStepIdFromSession();
+
+	if (layoutMode === 'edit' && sessionStepId) {
+		activeStep = document.getElementById(sessionStepId);
+	}
+
+	setActiveStep(activeStep);
+
+	// Change active step on button click
+
+	const steps = fragmentElement.querySelectorAll('li');
+
+	for (const step of steps) {
+		step.querySelector('button').addEventListener('click', () =>
+			setActiveStep(step)
+		);
+	}
+}
+
+main();
