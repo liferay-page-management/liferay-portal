35602c89d76effa75d9f6abdc35166433d1fbcac
Sandro Chinea <sandrochinea@gmail.com>
2024-08-21 11:46:30 +0100

    LPD-31696 Add tests to cover multistep configuration and step change

--- modules/test/playwright/tests/layout-content-page-editor-web/formContainer.spec.ts
+++ modules/test/playwright/tests/layout-content-page-editor-web/formContainer.spec.ts
@@ -166,7 +166,7 @@ test.describe('Relationships', () => {
 	});
 });
 
-test.describe('Multistep', () => {
+test.describe('Multistep', {tag: '@LPD-10727'}, () => {
 	test('Can add and configure a Stepper fragment', async ({
 		apiHelpers,
 		page,
@@ -237,5 +237,252 @@ test.describe('Multistep', () => {
 		await expect(
 			page.locator('.multi-step-icon[data-multi-step-icon="1"]')
 		).not.toBeVisible();
+	});
+
+	test('Can configure multistep options for a form container', async ({
+		apiHelpers,
+		page,
+		pageEditorPage,
+		pageManagementSite,
+	}) => {
+
+		// Get the id of Lemon object from the site initializer
+
+		const {id: objectDefinitionId} =
+			await apiHelpers.objectAdmin.getObjectDefinitionByExternalReferenceCode(
+				LEMON_OBJECT_ERC
+			);
+
+		// Create a page with a form container
+
+		const formId = getRandomString();
+
+		const formDefinition = getFormContainerDefinition({
+			id: formId,
+			objectDefinitionId,
+		});
+
+		const layout = await apiHelpers.headlessDelivery.createSitePage({
+			pageDefinition: getPageDefinition([formDefinition]),
+			siteId: pageManagementSite.id,
+			title: getRandomString(),
+		});
+
+		// Go to edit mode
+
+		await pageEditorPage.goto(layout, pageManagementSite.friendlyUrlPath);
+
+		// Check steps are not displayed
+
+		await expect(page.locator('.page-editor__form-step')).toHaveCount(0);
+
+		// Change to Multi Step and check first step is displayed
+
+		await pageEditorPage.changeFragmentConfiguration({
+			fieldLabel: 'Form Type',
+			fragmentId: formId,
+			tab: 'General',
+			value: 'Multi Step',
+		});
+
+		await expect(page.locator('.page-editor__form-step')).toHaveCount(2);
+
+		await expect(
+			page.locator('.page-editor__form-step').nth(0)
+		).toBeVisible();
+
+		await expect(
+			page.locator('.page-editor__form-step').nth(1)
+		).not.toBeVisible();
+
+		// Check option to display all steps and check it works
+
+		await pageEditorPage.changeFragmentConfiguration({
+			fieldLabel: 'Display All Steps in Edit Mode',
+			fragmentId: formId,
+			tab: 'General',
+			value: true,
+		});
+
+		await expect(
+			page.locator('.page-editor__form-step').nth(0)
+		).toBeVisible();
+
+		await expect(
+			page.locator('.page-editor__form-step').nth(1)
+		).toBeVisible();
+
+		// Change number of steps and check it works
+
+		await pageEditorPage.changeFragmentConfiguration({
+			fieldLabel: 'Number of Steps',
+			fragmentId: formId,
+			tab: 'General',
+			value: '3',
+		});
+
+		await expect(page.locator('.page-editor__form-step')).toHaveCount(3);
+	});
+
+	test('Can change step with the stepper fragment', async ({
+		apiHelpers,
+		page,
+		pageEditorPage,
+		pageManagementSite,
+	}) => {
+
+		// Get the id of Lemon object from the site initializer
+
+		const {id: objectDefinitionId} =
+			await apiHelpers.objectAdmin.getObjectDefinitionByExternalReferenceCode(
+				LEMON_OBJECT_ERC
+			);
+
+		// Definition for the Stepper fragment
+
+		const stepperFragment = getFragmentDefinition({
+			id: getRandomString(),
+			key: 'INPUTS-stepper',
+		});
+
+		// Create a form with two steps and the Stepper
+
+		const headingDefinition = getFragmentDefinition({
+			id: getRandomString(),
+			key: 'BASIC_COMPONENT-heading',
+		});
+
+		const buttonDefinition = getFragmentDefinition({
+			id: getRandomString(),
+			key: 'BASIC_COMPONENT-button',
+		});
+
+		const formDefinition = getFormContainerDefinition({
+			id: getRandomString(),
+			objectDefinitionId,
+			pageElements: [stepperFragment],
+			steps: [[headingDefinition], [buttonDefinition]],
+		});
+
+		const layout = await apiHelpers.headlessDelivery.createSitePage({
+			pageDefinition: getPageDefinition([formDefinition]),
+			siteId: pageManagementSite.id,
+			title: getRandomString(),
+		});
+
+		// Go to edit mode
+
+		await pageEditorPage.goto(layout, pageManagementSite.friendlyUrlPath);
+
+		// Check step change works properly
+
+		const button = page.locator(
+			'.lfr-layout-structure-item-basic-component-button'
+		);
+
+		const heading = page.locator(
+			'.lfr-layout-structure-item-basic-component-heading'
+		);
+
+		await expect(button).not.toBeVisible();
+		await expect(heading).toBeVisible();
+
+		const stepButtons = await page.locator('.multi-step-icon').all();
+		await stepButtons[1].click();
+
+		await expect(button).toBeVisible();
+		await expect(heading).not.toBeVisible();
+	});
+
+	test('Step change affects only desired form', async ({
+		apiHelpers,
+		page,
+		pageManagementSite,
+	}) => {
+
+		// Get the id of Lemon object from the site initializer
+
+		const {id: objectDefinitionId} =
+			await apiHelpers.objectAdmin.getObjectDefinitionByExternalReferenceCode(
+				LEMON_OBJECT_ERC
+			);
+
+		// Definition for the Steppers fragment
+
+		const stepperFragment1 = getFragmentDefinition({
+			id: getRandomString(),
+			key: 'INPUTS-stepper',
+		});
+
+		const stepperFragment2 = getFragmentDefinition({
+			id: getRandomString(),
+			key: 'INPUTS-stepper',
+		});
+
+		// Create two forms with two steps and the Stepper
+
+		const headingDefinition = getFragmentDefinition({
+			id: getRandomString(),
+			key: 'BASIC_COMPONENT-heading',
+		});
+
+		const buttonDefinition = getFragmentDefinition({
+			id: getRandomString(),
+			key: 'BASIC_COMPONENT-button',
+		});
+
+		const form1Definition = getFormContainerDefinition({
+			id: getRandomString(),
+			objectDefinitionId,
+			pageElements: [stepperFragment1],
+			steps: [[headingDefinition], []],
+		});
+
+		const form2Definition = getFormContainerDefinition({
+			id: getRandomString(),
+			objectDefinitionId,
+			pageElements: [stepperFragment2],
+			steps: [[buttonDefinition], []],
+		});
+
+		const layout = await apiHelpers.headlessDelivery.createSitePage({
+			pageDefinition: getPageDefinition([
+				form1Definition,
+				form2Definition,
+			]),
+			siteId: pageManagementSite.id,
+			title: getRandomString(),
+		});
+
+		// Go to view mode of page
+
+		await page.goto(
+			`/web${pageManagementSite.friendlyUrlPath}${layout.friendlyUrlPath}`
+		);
+
+		// Check step change affects only desired form
+
+		const button = page.locator(
+			'.lfr-layout-structure-item-basic-component-button'
+		);
+
+		const heading = page.locator(
+			'.lfr-layout-structure-item-basic-component-heading'
+		);
+
+		await expect(button).toBeVisible();
+		await expect(heading).toBeVisible();
+
+		const firstForm = page
+			.locator('.lfr-layout-structure-item-form')
+			.first();
+
+		const firstFormSteps = await firstForm
+			.locator('.multi-step-icon')
+			.all();
+		await firstFormSteps[1].click();
+
+		await expect(heading).not.toBeVisible();
+		await expect(button).toBeVisible();
 	});
 });
