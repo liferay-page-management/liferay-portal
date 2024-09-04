f4b2ea08e59dfdcb92a49e3f7b358ffb705ef8d1
Sandro Chinea <sandrochinea@gmail.com>
2024-09-04 10:27:04 +0100

    LPD-34363 We don't need recursion

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/getStepperChild.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/getStepperChild.ts
@@ -3,23 +3,20 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {FormLayoutDataItem} from '../../types/layout_data/FormLayoutDataItem';
 import {LayoutData, LayoutDataItem} from '../../types/layout_data/LayoutData';
 import {FragmentEntryLinkMap} from '../actions/addFragmentEntryLinks';
 
 export function getStepperChild(
-	parent: LayoutDataItem,
+	form: FormLayoutDataItem,
 	layoutData: LayoutData,
 	fragmentEntryLinks: FragmentEntryLinkMap
 ): LayoutDataItem | null {
-	for (const childId of parent.children) {
+	for (const childId of form.children) {
 		const child = layoutData.items[childId];
 
-		if (!child) {
+		if (!child || !('fragmentEntryLinkId' in child.config)) {
 			continue;
-		}
-
-		if (!('fragmentEntryLinkId' in child.config)) {
-			return getStepperChild(child, layoutData, fragmentEntryLinks);
 		}
 
 		const fragment = fragmentEntryLinks[child.config.fragmentEntryLinkId];
