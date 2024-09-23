2a2a90c7539c39ae3cb8bc09b0c69242d4a2791a
Rubén Pulido <ruben.pulido@liferay.com>
2024-09-23 16:39:05 +0200

    LPD-36963 Expose issue through playwright test checking that with workflow disabled, pages in published status can be searched in the Page Tree and in a Widget Page, and pages in draft status can be searched in the Page Tree but not in a Widget Page.

--- modules/test/playwright/tests/layout-admin-web/pagesSearch.spec.ts
+++ modules/test/playwright/tests/layout-admin-web/pagesSearch.spec.ts
@@ -29,6 +29,134 @@ const test = mergeTests(
 );
 
 test(
+	'With workflow disabled for Content Pages, checks pages in published status can be searched in the Page Tree and in a Widget Page, and pages in draft status can be searched in the Page Tree but not in a Widget Page',
+	{
+		tag: '@LPD-36963',
+	},
+	async ({apiHelpers, page, pagesAdminPage, site, widgetPagePage}) => {
+
+		// Create a content page in draft status
+
+		await pagesAdminPage.goto(site.friendlyUrlPath);
+
+		const draftPageTitle = getRandomString();
+
+		await pagesAdminPage.createNewPage({
+			draft: true,
+			name: draftPageTitle,
+		});
+
+		// Create a content page in published status
+
+		const publishedPageTitle = getRandomString();
+
+		await apiHelpers.headlessDelivery.createSitePage({
+			siteId: site.id,
+			title: publishedPageTitle,
+		});
+
+		// Navigate to a product menu section where pages are not listed, for example fragments
+
+		await page.goto(
+			`/group${site.friendlyUrlPath}${PORTLET_URLS.fragments}`
+		);
+
+		// Open the Product Menu
+
+		await openProductMenu(page);
+
+		const productMenu = page.getByLabel('Product Menu');
+
+		// Open tree if it's not already open
+
+		if (!(await productMenu.locator('.treeview').isVisible())) {
+			await productMenu
+				.getByRole('button', {exact: true, name: 'Page Tree'})
+				.click();
+
+			await productMenu.locator('.treeview').waitFor();
+		}
+
+		// Check all pages are listed
+
+		await expect(productMenu.getByText(draftPageTitle)).toBeVisible();
+		await expect(productMenu.getByText(publishedPageTitle)).toBeVisible();
+
+		// Enter search term in page tree matching draft page title
+
+		const pageTreeSearchInput = page.getByPlaceholder(
+			'Start typing to find a page.'
+		);
+
+		await pageTreeSearchInput.fill(draftPageTitle);
+
+		// Check only draft page is listed
+
+		await expect(
+			productMenu.getByText(publishedPageTitle)
+		).not.toBeVisible();
+		await expect(
+			productMenu.getByRole('link', {name: draftPageTitle})
+		).toBeVisible();
+		await expect(productMenu.getByText('Page not found')).not.toBeVisible();
+
+		// Enter search term in page tree matching published page title
+
+		await pageTreeSearchInput.fill(publishedPageTitle);
+
+		// Check only published page is listed
+
+		await expect(
+			productMenu.getByRole('link', {name: publishedPageTitle})
+		).toBeVisible();
+		await expect(productMenu.getByText('Page not found')).not.toBeVisible();
+		await expect(page.getByText(draftPageTitle)).not.toBeVisible();
+
+		// Create a widget page with search bar and search results portlets
+
+		await pagesAdminPage.goto(site.friendlyUrlPath);
+
+		const layout = await apiHelpers.jsonWebServicesLayout.addLayout({
+			groupId: site.id,
+			title: getRandomString(),
+		});
+
+		await page.goto(`/web${site.friendlyUrlPath}${layout.friendlyURL}`);
+
+		await widgetPagePage.addPortlet('Search Bar', 'Search');
+		await widgetPagePage.addPortlet('Search Results', 'Search');
+
+		await page.goto(`/web${site.friendlyUrlPath}${layout.friendlyURL}`);
+
+		const widgetPageSearchInput = page.getByPlaceholder('Search...');
+
+		// Enter search term in search bar within widget page matching draft page title
+
+		await widgetPageSearchInput.fill(draftPageTitle);
+		await page.keyboard.press('Enter');
+
+		// Check no pages are listed
+
+		await expect(
+			page.getByText(
+				`No results were found that matched the keywords: ${draftPageTitle}`
+			)
+		).toBeVisible();
+
+		// Enter search term in search bar within widget page matching published page title
+
+		await widgetPageSearchInput.fill(publishedPageTitle);
+		await page.keyboard.press('Enter');
+
+		// Check only published page is listed
+
+		await expect(
+			page.getByText(`1 Result for ${publishedPageTitle}`)
+		).toBeVisible();
+	}
+);
+
+test(
 	'With workflow enabled for Content Pages, checks pages in published status can be searched in the Page Tree and in a Widget Page, and pages in draft, pending and rejected status can be searched in the Page Tree but not in a Widget Page',
 	{
 		tag: '@LPD-36963',
