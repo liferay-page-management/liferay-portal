0d39a213c17b2901606add47f42ee81647504363
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2024-09-24 18:06:10 +0200

    LPD-37350 Remove beta feature label in duplicate action

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/topper/TopperItemActions.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/topper/TopperItemActions.js
@@ -145,7 +145,6 @@ export default function TopperItemActions({disabled, item}) {
 						})
 					),
 				icon: 'copy',
-				isBetaFeature: true,
 				label: Liferay.Language.get('duplicate'),
 			});
 
