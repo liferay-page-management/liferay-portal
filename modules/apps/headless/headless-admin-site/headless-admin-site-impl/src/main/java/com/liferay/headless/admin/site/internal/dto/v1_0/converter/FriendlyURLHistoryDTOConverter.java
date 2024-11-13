08eba27609e918c97547ab6c10cb06c43ebe0ff5
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-11-13 19:21:58 +0000

    LPD-41559 Adds dto converted for FriendlyURLHistory

---
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/FriendlyURLHistoryDTOConverter.java
@@ -0,0 +1,44 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.site.internal.dto.v1_0.converter;
+
+import com.liferay.friendly.url.model.FriendlyURLEntry;
+import com.liferay.headless.admin.site.dto.v1_0.FriendlyUrlHistory;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	property = "dto.class.name=com.liferay.portal.kernel.model.Layout",
+	service = DTOConverter.class
+)
+public class FriendlyURLHistoryDTOConverter
+	implements DTOConverter<FriendlyURLEntry, FriendlyUrlHistory> {
+
+	@Override
+	public String getContentType() {
+		return FriendlyUrlHistory.class.getSimpleName();
+	}
+
+	@Override
+	public FriendlyUrlHistory toDTO(
+			DTOConverterContext dtoConverterContext,
+			FriendlyURLEntry friendlyURLEntry)
+		throws Exception {
+
+		return new FriendlyUrlHistory() {
+			{
+				setFriendlyUrlPath_i18n(
+					friendlyURLEntry::getLanguageIdToUrlTitleMap);
+			}
+		};
+	}
+
+}
