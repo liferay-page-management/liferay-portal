0d8ab97ba6aacecdf8e1a90a8b6b2045b301f711
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-11-14 07:32:28 +0000

    LPD-41559 Implements GET method

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/FriendlyUrlHistoryResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/FriendlyUrlHistoryResourceImpl.java
@@ -5,9 +5,20 @@
 
 package com.liferay.headless.admin.site.internal.resource.v1_0;
 
+import com.liferay.friendly.url.model.FriendlyURLEntry;
+import com.liferay.friendly.url.service.FriendlyURLEntryLocalService;
+import com.liferay.headless.admin.site.dto.v1_0.FriendlyUrlHistory;
 import com.liferay.headless.admin.site.resource.v1_0.FriendlyUrlHistoryResource;
+import com.liferay.layout.friendly.url.LayoutFriendlyURLEntryHelper;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.pagination.Page;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -19,4 +30,48 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class FriendlyUrlHistoryResourceImpl
 	extends BaseFriendlyUrlHistoryResourceImpl {
+
+	@Override
+	public Page<FriendlyUrlHistory>
+			getSiteSiteByExternalReferenceCodeSitePageFriendlyUrlHistoryPage(
+				String siteExternalReferenceCode,
+				String sitePageExternalReferenceCode)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
+			throw new UnsupportedOperationException();
+		}
+
+		Group group = groupLocalService.getGroupByExternalReferenceCode(
+			siteExternalReferenceCode, contextCompany.getCompanyId());
+
+		Layout layout = _layoutLocalService.getLayoutByExternalReferenceCode(
+			sitePageExternalReferenceCode, group.getGroupId());
+
+		return Page.of(
+			transform(
+				_friendlyURLEntryLocalService.getFriendlyURLEntries(
+					group.getGroupId(),
+					_layoutFriendlyURLEntryHelper.getClassNameId(
+						layout.isPrivateLayout()),
+					layout.getPlid()),
+				friendlyURLEntry -> _friendlyURLHistoryDTOConverter.toDTO(
+					friendlyURLEntry)));
+	}
+
+	@Reference
+	private FriendlyURLEntryLocalService _friendlyURLEntryLocalService;
+
+	@Reference(
+		target = "(component.name=com.liferay.headless.admin.site.internal.dto.v1_0.converter.FriendlyURLHistoryDTOConverter)"
+	)
+	private DTOConverter<FriendlyURLEntry, FriendlyUrlHistory>
+		_friendlyURLHistoryDTOConverter;
+
+	@Reference
+	private LayoutFriendlyURLEntryHelper _layoutFriendlyURLEntryHelper;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
 }
