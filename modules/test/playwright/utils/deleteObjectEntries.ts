970c7e8853ba5c3e45722d5db3e6b31eca582d3e
Sandro Chinea <sandrochinea@gmail.com>
2024-11-22 11:18:31 +0000

    LPD-42633 This method is generic

---
+++ modules/test/playwright/utils/deleteObjectEntries.ts
@@ -0,0 +1,39 @@
+import {expect} from '@playwright/test';
+
+import {ApiHelpers} from '../helpers/ApiHelpers';
+
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+export async function deleteObjectEntries({
+	apiHelpers,
+	entityName,
+	excludeERC = [],
+	scopeKey,
+}: {
+	apiHelpers: ApiHelpers;
+	entityName: string;
+	excludeERC?: string[];
+	scopeKey: Site['key'];
+}) {
+	const {items: entries} =
+		await apiHelpers.objectEntry.getObjectDefinitionObjectEntriesByScope(
+			`c/${entityName}`,
+			scopeKey
+		);
+
+	for (const {externalReferenceCode} of entries) {
+		if (excludeERC.includes(externalReferenceCode)) {
+			continue;
+		}
+
+		await expect(
+			await apiHelpers.objectEntry.deleteObjectEntryByExternalReferenceCode(
+				`c/${entityName}`,
+				scopeKey,
+				externalReferenceCode
+			)
+		).toBeOK();
+	}
+}
