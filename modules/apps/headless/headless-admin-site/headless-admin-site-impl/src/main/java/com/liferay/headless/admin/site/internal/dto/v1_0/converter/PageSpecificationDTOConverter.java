4e381e48d95b8563a51034b8ef530701459be02e
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-11-27 12:31:22 +0100

    LPD-41551 Add client extensions, logic copied from PageDefinitionDTOConverter

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/PageSpecificationDTOConverter.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/PageSpecificationDTOConverter.java
@@ -5,25 +5,34 @@
 
 package com.liferay.headless.admin.site.internal.dto.v1_0.converter;
 
+import com.liferay.client.extension.constants.ClientExtensionEntryConstants;
+import com.liferay.client.extension.model.ClientExtensionEntryRel;
+import com.liferay.client.extension.service.ClientExtensionEntryRelLocalService;
 import com.liferay.document.library.kernel.service.DLAppService;
+import com.liferay.headless.admin.site.dto.v1_0.ClientExtension;
 import com.liferay.headless.admin.site.dto.v1_0.ItemExternalReference;
 import com.liferay.headless.admin.site.dto.v1_0.PageSpecification;
 import com.liferay.headless.admin.site.dto.v1_0.Settings;
 import com.liferay.headless.admin.site.dto.v1_0.StyleBook;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.model.ColorScheme;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.Theme;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.service.ThemeLocalService;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 import com.liferay.style.book.model.StyleBookEntry;
 import com.liferay.style.book.service.StyleBookEntryLocalService;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
@@ -67,7 +76,74 @@ public class PageSpecificationDTOConverter
 		};
 	}
 
+	private ClientExtension _getClientExtension(
+		ClientExtensionEntryRel clientExtensionEntryRel) {
+
+		if (clientExtensionEntryRel == null) {
+			return null;
+		}
+
+		return new ClientExtension() {
+			{
+				setClientExtensionConfig(
+					() -> _getClientExtensionConfig(clientExtensionEntryRel));
+				setExternalReferenceCode(
+					clientExtensionEntryRel::getCETExternalReferenceCode);
+			}
+		};
+	}
+
+	private ClientExtension _getClientExtension(
+		long classNameId, long classPK, String type) {
+
+		return _getClientExtension(
+			_clientExtensionEntryRelLocalService.fetchClientExtensionEntryRel(
+				classNameId, classPK, type));
+	}
+
+	private Map<String, String> _getClientExtensionConfig(
+		ClientExtensionEntryRel clientExtensionEntryRel) {
+
+		if (clientExtensionEntryRel == null) {
+			return null;
+		}
+
+		UnicodeProperties unicodeProperties = UnicodePropertiesBuilder.fastLoad(
+			clientExtensionEntryRel.getTypeSettings()
+		).build();
+
+		if (unicodeProperties.isEmpty()) {
+			return null;
+		}
+
+		Map<String, String> clientExtensionConfig = new HashMap<>();
+
+		for (Map.Entry<String, String> entry : unicodeProperties.entrySet()) {
+			clientExtensionConfig.put(entry.getKey(), entry.getValue());
+		}
+
+		return clientExtensionConfig;
+	}
+
+	private ClientExtension[] _getClientExtensions(
+		long classNameId, long classPK, String type) {
+
+		ClientExtension[] clientExtensions = TransformUtil.transformToArray(
+			_clientExtensionEntryRelLocalService.getClientExtensionEntryRels(
+				classNameId, classPK, type),
+			clientExtensionEntryRel -> _getClientExtension(
+				clientExtensionEntryRel),
+			ClientExtension.class);
+
+		if (ArrayUtil.isEmpty(clientExtensions)) {
+			return null;
+		}
+
+		return clientExtensions;
+	}
+
 	private Settings _setSettings(Layout layout) throws Exception {
+		long classNameId = _portal.getClassNameId(Layout.class.getName());
 		UnicodeProperties unicodeProperties =
 			layout.getTypeSettingsProperties();
 
@@ -102,12 +178,23 @@ public class PageSpecificationDTOConverter
 					});
 				setFavIcon(
 					() -> {
-						if (layout.getFaviconFileEntryId() == 0) {
+						ClientExtension clientExtension = _getClientExtension(
+							classNameId, layout.getPlid(),
+							ClientExtensionEntryConstants.TYPE_THEME_FAVICON);
+
+						if (clientExtension != null) {
+							return clientExtension;
+						}
+
+						long faviconFileEntryId =
+							layout.getFaviconFileEntryId();
+
+						if (faviconFileEntryId == 0) {
 							return null;
 						}
 
 						FileEntry fileEntry = _dlAppService.getFileEntry(
-							layout.getFaviconFileEntryId());
+							faviconFileEntryId);
 
 						if (fileEntry == null) {
 							return null;
@@ -122,6 +209,14 @@ public class PageSpecificationDTOConverter
 							}
 						};
 					});
+				setGlobalCSSClientExtensions(
+					() -> _getClientExtensions(
+						classNameId, layout.getPlid(),
+						ClientExtensionEntryConstants.TYPE_GLOBAL_CSS));
+				setGlobalJSClientExtensions(
+					() -> _getClientExtensions(
+						classNameId, layout.getPlid(),
+						ClientExtensionEntryConstants.TYPE_GLOBAL_JS));
 				setJavascript(
 					() -> unicodeProperties.getProperty("javascript", null));
 				setMasterPageExternalReferenceCode(
@@ -161,6 +256,10 @@ public class PageSpecificationDTOConverter
 							}
 						};
 					});
+				setThemeCSSClientExtension(
+					() -> _getClientExtension(
+						classNameId, layout.getPlid(),
+						ClientExtensionEntryConstants.TYPE_THEME_CSS));
 				setThemeName(
 					() -> {
 						if (Validator.isNull(layout.getThemeId())) {
@@ -198,9 +297,17 @@ public class PageSpecificationDTOConverter
 
 						return themeSettingsUnicodeProperties;
 					});
+				setThemeSpritemapClientExtension(
+					() -> _getClientExtension(
+						classNameId, layout.getPlid(),
+						ClientExtensionEntryConstants.TYPE_THEME_SPRITEMAP));
 			}
 		};
 	}
+
+	@Reference
+	private ClientExtensionEntryRelLocalService
+		_clientExtensionEntryRelLocalService;
 
 	@Reference
 	private DLAppService _dlAppService;
@@ -208,6 +315,9 @@ public class PageSpecificationDTOConverter
 	@Reference
 	private LayoutPageTemplateEntryLocalService
 		_layoutPageTemplateEntryLocalService;
+
+	@Reference
+	private Portal _portal;
 
 	@Reference
 	private StyleBookEntryLocalService _styleBookEntryLocalService;
