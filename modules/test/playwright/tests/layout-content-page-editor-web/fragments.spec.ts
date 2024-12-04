ca3d85bbef032f876e68a2f1aac56623310fd1fb
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-12-04 21:13:46 +0000

    LPD-43559 Migrates MenuDisplay.testcase test to playwright

--- modules/test/playwright/tests/layout-content-page-editor-web/fragments.spec.ts
+++ modules/test/playwright/tests/layout-content-page-editor-web/fragments.spec.ts
@@ -24,6 +24,8 @@ import {clickAndExpectToBeVisible} from '../../utils/clickAndExpectToBeVisible';
 import dragAndDropElement from '../../utils/dragAndDropElement';
 import getGlobalSiteId from '../../utils/getGlobalSiteId';
 import getRandomString from '../../utils/getRandomString';
+import {hoverAndExpectToBeVisible} from '../../utils/hoverAndExpectToBeVisible';
+import {performLogout} from '../../utils/performLogin';
 import getBasicWebContentStructureId, {
 	getWebContentStructureId,
 } from '../../utils/structured-content/getBasicWebContentStructureId';
@@ -56,6 +58,15 @@ const test = mergeTests(
 	objectPagesTest,
 	pageEditorPagesTest,
 	pageManagementSiteTest
+);
+
+const testWithPrivatePages = mergeTests(
+	test,
+	featureFlagsTest({
+		'LPD-38869': true,
+		'LPD-39304': true,
+		'LPS-178052': true,
+	})
 );
 
 const ENTER_KEY = 'Enter';
@@ -1277,6 +1288,296 @@ test.describe('Localization Select Fragment', () => {
 			)
 		).toBeVisible();
 	});
+});
+
+testWithPrivatePages.describe('Menu Display Fragment', () => {
+	async function changeSource(
+		menuDisplayId: string,
+		name: string,
+		page: Page,
+		pageEditorPage: PageEditorPage
+	) {
+		await pageEditorPage.selectFragment(menuDisplayId);
+
+		const iframe = page.frameLocator('iframe[title="Select"]');
+
+		await clickAndExpectToBeVisible({
+			autoClick: true,
+			target: iframe.getByText(name),
+			trigger: page.getByLabel('Change Source'),
+		});
+
+		await iframe.getByRole('button', {name: 'Select This Level'}).click();
+
+		await pageEditorPage.waitForChangesSaved();
+	}
+
+	testWithPrivatePages(
+		'Can configure sublevels, display style, selected item color and hovered item color',
+		{
+			tag: ['@LPS-120091', '@LPS-140988'],
+		},
+		async ({apiHelpers, page, pageEditorPage, site}) => {
+
+			// Add layouts
+
+			const parentLayoutTitle = getRandomString();
+
+			const parentLayout =
+				await apiHelpers.jsonWebServicesLayout.addLayout({
+					groupId: site.id,
+					title: parentLayoutTitle,
+				});
+
+			const childLayoutTitle = getRandomString();
+
+			const childLayout =
+				await apiHelpers.jsonWebServicesLayout.addLayout({
+					groupId: site.id,
+					parentLayoutId: parentLayout.layoutId,
+					title: childLayoutTitle,
+				});
+
+			const grandChildLayoutLayoutTitle = getRandomString();
+
+			await apiHelpers.jsonWebServicesLayout.addLayout({
+				groupId: site.id,
+				parentLayoutId: childLayout.layoutId,
+				title: grandChildLayoutLayoutTitle,
+			});
+
+			// Add layout with menu display fragment and go to edit mode
+
+			const menuDisplayId = getRandomString();
+
+			const layout = await apiHelpers.headlessDelivery.createSitePage({
+				pageDefinition: getPageDefinition([
+					getFragmentDefinition({
+						fragmentConfig: {
+							sublevels: -1,
+						},
+						id: menuDisplayId,
+						key: 'com.liferay.fragment.renderer.menu.display.internal.MenuDisplayFragmentRenderer',
+					}),
+				]),
+				siteId: site.id,
+				title: getRandomString(),
+			});
+
+			await pageEditorPage.goto(layout, site.friendlyUrlPath);
+
+			// Assert layouts in menud display fragment
+
+			const menuDisplay = page.locator(
+				'.lfr-layout-structure-item-com-liferay-fragment-renderer-menu-display-internal-menudisplayfragmentrenderer'
+			);
+
+			await expect(
+				menuDisplay.getByText(parentLayoutTitle)
+			).toBeVisible();
+
+			await hoverAndExpectToBeVisible({
+				autoClick: false,
+				target: menuDisplay.getByText(childLayoutTitle),
+				trigger: menuDisplay.locator('.nav-link', {
+					hasText: parentLayoutTitle,
+				}),
+			});
+
+			await expect(
+				menuDisplay.getByText(grandChildLayoutLayoutTitle)
+			).toBeVisible();
+
+			// Configure levels
+
+			await pageEditorPage.changeFragmentConfiguration({
+				fieldLabel: 'Sublevels',
+				fragmentId: menuDisplayId,
+				tab: 'General',
+				value: '1',
+			});
+
+			// Assert layouts in menud display fragment
+
+			await hoverAndExpectToBeVisible({
+				autoClick: false,
+				target: menuDisplay.getByText(childLayoutTitle),
+				trigger: menuDisplay.locator('.nav-link', {
+					hasText: parentLayoutTitle,
+				}),
+			});
+
+			await expect(
+				menuDisplay.getByText(grandChildLayoutLayoutTitle)
+			).not.toBeVisible();
+
+			// Change displayStyle
+
+			await pageEditorPage.changeFragmentConfiguration({
+				fieldLabel: 'Display Style',
+				fragmentId: menuDisplayId,
+				tab: 'General',
+				value: 'Stacked',
+			});
+
+			// Assert layouts in menud display fragment
+
+			await expect(
+				menuDisplay.getByText(parentLayoutTitle)
+			).toBeVisible();
+
+			await expect(menuDisplay.getByText(childLayoutTitle)).toBeVisible();
+
+			await expect(
+				menuDisplay.getByText(grandChildLayoutLayoutTitle)
+			).not.toBeVisible();
+
+			// Change color style
+
+			await pageEditorPage.changeFragmentConfiguration({
+				fieldLabel: 'Selected Item Color',
+				fragmentId: menuDisplayId,
+				tab: 'Styles',
+				value: 'Success',
+				valueFromStylebook: true,
+			});
+
+			await pageEditorPage.changeFragmentConfiguration({
+				fieldLabel: 'Hovered Item Color',
+				fragmentId: menuDisplayId,
+				tab: 'Styles',
+				value: 'Warning',
+				valueFromStylebook: true,
+			});
+
+			// Assert color style in edit mode
+
+			await menuDisplay.getByText(parentLayoutTitle).hover();
+
+			await expect(menuDisplay.getByText(parentLayoutTitle)).toHaveCSS(
+				'color',
+				'rgb(185, 80, 0)'
+			);
+
+			// Publish page
+
+			await pageEditorPage.publishPage();
+
+			// logout and assert guest user can see menu fragment
+
+			await performLogout(page);
+
+			await page.goto(
+				`/web${site.friendlyUrlPath}${layout.friendlyUrlPath}`
+			);
+
+			await expect(
+				menuDisplay.getByText(parentLayoutTitle)
+			).toBeVisible();
+
+			// Assert color style in view mode
+
+			await expect(menuDisplay.getByText(parentLayoutTitle)).toHaveCSS(
+				'color',
+				'rgb(28, 28, 36)'
+			);
+		}
+	);
+
+	testWithPrivatePages(
+		'Can select a navigation menu from existing ones',
+		{
+			tag: '@LPS-120091',
+		},
+		async ({apiHelpers, page, pageEditorPage, site}) => {
+
+			// Add public and private layouts
+
+			const publicLayoutTitle = getRandomString();
+
+			await apiHelpers.jsonWebServicesLayout.addLayout({
+				groupId: site.id,
+				title: publicLayoutTitle,
+			});
+
+			const privateLayoutTitle = getRandomString();
+
+			await apiHelpers.jsonWebServicesLayout.addLayout({
+				groupId: site.id,
+				privateLayout: 'true',
+				title: privateLayoutTitle,
+			});
+
+			// Add navigation menu
+
+			const siteNavigationMenuName = getRandomString();
+
+			await apiHelpers.jsonWebServicesSiteNavigationMenu.addSiteNavigationMenu(
+				site.id,
+				siteNavigationMenuName
+			);
+
+			// Add layout with menu display fragment and go to edit mode
+
+			const menuDisplayId = getRandomString();
+
+			const layout = await apiHelpers.headlessDelivery.createSitePage({
+				pageDefinition: getPageDefinition([
+					getFragmentDefinition({
+						fragmentConfig: {
+							sublevels: -1,
+						},
+						id: menuDisplayId,
+						key: 'com.liferay.fragment.renderer.menu.display.internal.MenuDisplayFragmentRenderer',
+					}),
+				]),
+				siteId: site.id,
+				title: getRandomString(),
+			});
+
+			await pageEditorPage.goto(layout, site.friendlyUrlPath);
+
+			// Assert public page
+
+			const menuDisplay = page.locator(
+				'.lfr-layout-structure-item-com-liferay-fragment-renderer-menu-display-internal-menudisplayfragmentrenderer'
+			);
+
+			await expect(
+				menuDisplay.getByText(publicLayoutTitle)
+			).toBeVisible();
+
+			// Select private pages hierarchy
+
+			await changeSource(
+				menuDisplayId,
+				'Private Pages Hierarchy',
+				page,
+				pageEditorPage
+			);
+
+			// Assert private page
+
+			await expect(
+				menuDisplay.getByText(privateLayoutTitle)
+			).toBeVisible();
+
+			// Select navigation menu
+
+			await changeSource(
+				menuDisplayId,
+				siteNavigationMenuName,
+				page,
+				pageEditorPage
+			);
+
+			// Assert navigation item
+
+			await expect(
+				menuDisplay.getByText('There are no menu items to display')
+			).toBeVisible();
+		}
+	);
 });
 
 test.describe('Multiselect Fragment', () => {
