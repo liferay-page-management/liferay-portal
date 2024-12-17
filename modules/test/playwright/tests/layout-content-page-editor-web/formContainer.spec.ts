e213d3a4432aee428fe3dabc5e10c53d7a396586
Victor Galan <victor.galan@liferay.com>
2024-12-17 15:50:01 +0100

    LPD-44659 Fix locators

--- modules/test/playwright/tests/layout-content-page-editor-web/formContainer.spec.ts
+++ modules/test/playwright/tests/layout-content-page-editor-web/formContainer.spec.ts
@@ -1541,7 +1541,10 @@ test.describe('Form Localization', () => {
 				.frameLocator('iframe[title="editor"]')
 				.getByText('rich text english')
 		).toBeVisible();
-		await expect(page.getByText('text english')).toBeVisible();
+
+		await expect(page.locator('input.ddm-field-text')).toHaveValue(
+			'text english'
+		);
 
 		await clickAndExpectToBeVisible({
 			autoClick: true,
@@ -1557,7 +1560,9 @@ test.describe('Form Localization', () => {
 				.frameLocator('iframe[title="editor"]')
 				.getByText('rich text español')
 		).toBeVisible();
-		await expect(page.getByText('text español')).toBeVisible();
+		await expect(page.locator('input.ddm-field-text')).toHaveValue(
+			'text español'
+		);
 	});
 
 	test('Shows a warning modal when the page is published and there is a Localization Select fragment but no localizable fields', async ({
