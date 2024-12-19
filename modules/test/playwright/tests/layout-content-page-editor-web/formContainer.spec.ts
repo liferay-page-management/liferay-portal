4bcee6477ae657487916ea79021fb8e90c254d1d
Victor Galan <victor.galan@liferay.com>
2024-12-19 13:07:03 +0100

    LPD-44869 Adapt test

--- modules/test/playwright/tests/layout-content-page-editor-web/formContainer.spec.ts
+++ modules/test/playwright/tests/layout-content-page-editor-web/formContainer.spec.ts
@@ -5572,10 +5572,11 @@ test.describe('Edit mode form errors', () => {
 
 			await masterPagesPage.editMaster(layoutPageTemplateEntryName);
 
-			await masterPagesPage.configureAllowedFragments(
-				['Checkbox', 'Date'],
-				'Form Components'
-			);
+			await masterPagesPage.configureAllowedFragments({
+				fragmentNames: ['Checkbox', 'Date'],
+				mode: 'unselect',
+				prefilter: 'Form Components',
+			});
 
 			await pageEditorPage.publishPage();
 
