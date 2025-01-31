3299d08f9cf5840e83becfc76381c50877302e38
Sandro Chinea <sandrochinea@gmail.com>
2025-01-31 14:44:29 +0000

    LPD-47346 Add empty state and new styles for collection items

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout_data_items/CollectionItemWithControls.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout_data_items/CollectionItemWithControls.js
@@ -7,12 +7,13 @@ import classNames from 'classnames';
 import React, {useContext} from 'react';
 
 import useSetRef from '../../../common/hooks/useSetRef';
+import {config} from '../../config';
 import {CollectionItemContext} from '../../contexts/CollectionItemContext';
 import getLayoutDataItemTopperUniqueClassName from '../../utils/getLayoutDataItemTopperUniqueClassName';
 import TopperEmpty from '../topper/TopperEmpty';
 
 const CollectionItemWithControls = React.forwardRef(({children, item}, ref) => {
-	const {collectionConfig, collectionItem} = useContext(
+	const {collectionConfig, collectionItem, collectionItemIndex} = useContext(
 		CollectionItemContext
 	);
 	const title =
@@ -48,13 +49,53 @@ const CollectionItemWithControls = React.forwardRef(({children, item}, ref) => {
 						)}
 						ref={setRef}
 					>
-						<div className="page-editor__collection-item__border">
-							<p className="page-editor__collection-item__title">
+						<div
+							className={classNames(
+								'page-editor__collection-item__border',
+								{
+									'position-static':
+										Liferay.FeatureFlags['LPD-18221'],
+								}
+							)}
+						>
+							<p
+								className={classNames(
+									'page-editor__collection-item__title',
+									{
+										'c-m-0 c-p-4':
+											Liferay.FeatureFlags['LPD-18221'],
+									}
+								)}
+							>
 								{title ||
 									Liferay.Language.get(
 										'sample-collection-item'
 									)}
 							</p>
+
+							{Liferay.FeatureFlags['LPD-18221'] &&
+							collectionItemIndex === 0 ? (
+								<div className="c-mb-4 c-mx-4 d-flex flex-column page-editor__no-fragments-state">
+									<img
+										className="c-mb-3 page-editor__no-fragments-state__image"
+										src={`${config.imagesPath}/collection-item-empty-state.svg`}
+									/>
+
+									<p className="d-flex flex-column page-editor__no-fragments-state__message">
+										<span>
+											{Liferay.Language.get(
+												'drag-and-drop-fragments-or-widgets-here'
+											)}
+										</span>
+
+										<span>
+											{Liferay.Language.get(
+												'all-components-placed-here-will-be-dynamically-replicated-across-the-collection'
+											)}
+										</span>
+									</p>
+								</div>
+							) : null}
 						</div>
 					</div>
 				) : (
