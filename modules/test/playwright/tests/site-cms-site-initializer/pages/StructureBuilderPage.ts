4f69e5b3c397932f7370e1695faad6007742dc5a
Sandro Chinea <sandrochinea@gmail.com>
2025-03-04 08:21:59 +0000

    LPD-50134 Adapt existing and add new playwright test

--- modules/test/playwright/tests/site-cms-site-initializer/pages/StructureBuilderPage.ts
+++ modules/test/playwright/tests/site-cms-site-initializer/pages/StructureBuilderPage.ts
@@ -31,6 +31,7 @@ export class StructureBuilderPage {
 	readonly page: Page;
 
 	private readonly labelInput: Locator;
+	private readonly nameInput: Locator;
 	private readonly publishButton: Locator;
 	private readonly saveButton: Locator;
 
@@ -38,6 +39,7 @@ export class StructureBuilderPage {
 		this.page = page;
 
 		this.labelInput = this.page.getByLabel('Structure Label');
+		this.nameInput = this.page.getByLabel('Structure Name');
 		this.publishButton = this.page.getByRole('button', {name: 'Publish'});
 		this.saveButton = this.page.getByRole('button', {name: 'Save'});
 	}
@@ -69,23 +71,41 @@ export class StructureBuilderPage {
 		});
 	}
 
-	async changeStructureLabel(label: string) {
-		await this.labelInput.fill(label);
+	async changeFieldSettings({name}: {name?: string}) {
+		if (name) {
+			await this.page.getByLabel('Field Name').fill(name);
 
-		await this.page.locator('span.label-item').click();
+			await this.page.getByRole('tab', {name: 'General'}).click();
+		}
+	}
+
+	async changeStructureSettings({
+		label,
+		name,
+	}: {
+		label?: string;
+		name?: string;
+	}) {
+		if (label) {
+			await this.labelInput.fill(label);
+			await this.page.getByRole('tab', {name: 'General'}).click();
+		}
+
+		if (name) {
+			await this.nameInput.fill(name);
+			await this.page.getByRole('tab', {name: 'General'}).click();
+		}
 	}
 
 	async deleteField({label, nth = 0}: {label: string; nth?: number}) {
 		const count = await this.page
-			.locator('.treeview-link', {
-				hasText: label,
-			})
+			.locator('.treeview-item')
+			.getByLabel(label, {exact: true})
 			.count();
 
 		const treeItem = this.page
-			.locator('.treeview-link', {
-				hasText: label,
-			})
+			.locator('.treeview-item')
+			.getByLabel(label, {exact: true})
 			.nth(nth);
 
 		if (treeItem) {
@@ -93,14 +113,14 @@ export class StructureBuilderPage {
 
 			await clickAndExpectToBeVisible({
 				autoClick: true,
-				target: this.page.getByRole('menuitem', {name: 'Delete Field'}),
+				target: this.page.getByRole('menuitem', {name: 'Delete'}),
 				trigger: treeItem.getByLabel('Field Options'),
 			});
 
 			await expect(
-				this.page.locator('.treeview-link', {
-					hasText: label,
-				})
+				this.page
+					.locator('.treeview-item')
+					.getByLabel(label, {exact: true})
 			).toHaveCount(count - 1);
 		}
 	}
@@ -136,7 +156,8 @@ export class StructureBuilderPage {
 			this.page.waitForResponse(
 				(response) =>
 					response.url().includes('object-definitions') &&
-					response.status() === 200
+					response.status() === 200,
+				{timeout: 5000}
 			),
 			await save(),
 		]);
@@ -144,5 +165,15 @@ export class StructureBuilderPage {
 		const {id} = await response.json();
 
 		return {id};
+	}
+
+	async selectField({label, nth}: {label: string; nth?: number}) {
+		await clickAndExpectToBeVisible({
+			target: this.page.locator('.breadcrumb-link', {hasText: label}),
+			trigger: this.page
+				.locator('.treeview-item')
+				.getByLabel(label, {exact: true})
+				.nth(nth),
+		});
 	}
 }
