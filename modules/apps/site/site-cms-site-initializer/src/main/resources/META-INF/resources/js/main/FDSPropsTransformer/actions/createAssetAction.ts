a962af073c9a42c3f488692d62988ac7e043d79d
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-03-12 17:12:09 +0100

    LPD-51391 Add createAssetAction for files and contents

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/FDSPropsTransformer/actions/createAssetAction.ts
@@ -0,0 +1,25 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {openModal} from 'frontend-js-components-web';
+
+import CreationModalContent from '../../components/modal/CreationModalContent';
+
+type AssetData = {
+	action: 'createAsset';
+	redirect: string;
+	title: string;
+};
+
+export default function createAssetAction(data: AssetData) {
+	openModal({
+		contentComponent: ({closeModal}: {closeModal: () => void}) =>
+			CreationModalContent({
+				...data,
+				closeModal,
+			}),
+		size: 'sm',
+	});
+}
