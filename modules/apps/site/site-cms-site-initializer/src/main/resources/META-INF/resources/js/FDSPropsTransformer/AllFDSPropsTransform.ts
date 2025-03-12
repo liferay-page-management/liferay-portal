451b6824249ac7579b8fe3669c1a5f9927471ee3
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-03-12 17:20:21 +0100

    LPD-51391 Add PropsTransform for all section with createAsset action

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/FDSPropsTransformer/AllFDSPropsTransform.ts
@@ -0,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import createAssetAction from './actions/createAssetAction';
+import addOnClickToCreationMenuItems from './utils/addOnClickToCreationMenuItems';
+
+const ACTIONS = {
+	createAsset: createAssetAction,
+};
+
+export default function AllFDSPropsTransformer({
+	creationMenu,
+	...otherProps
+}: {
+	creationMenu: any;
+	otherProps: any;
+}) {
+	return {
+		...otherProps,
+		creationMenu: {
+			...creationMenu,
+			primaryItems: addOnClickToCreationMenuItems(
+				creationMenu.primaryItems,
+				ACTIONS
+			),
+		},
+	};
+}
