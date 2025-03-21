fdca61ddd644177fb127c83a2db2ea5d3bdbed5a
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-03-21 23:39:47 +0100

    LPD-51647 Add error feedback to picklist input

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/PicklistPicker.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/PicklistPicker.tsx
@@ -8,7 +8,8 @@ import {Option, Picker} from '@clayui/core';
 import {ClayDropDownWithItems} from '@clayui/drop-down';
 import ClayForm, {ClayInput} from '@clayui/form';
 import ClayIcon from '@clayui/icon';
-import {useId} from 'frontend-js-components-web';
+import classNames from 'classnames';
+import {FieldFeedback, useId} from 'frontend-js-components-web';
 import {sub} from 'frontend-js-web';
 import React, {useEffect, useState} from 'react';
 
@@ -25,6 +26,7 @@ type Picklist = {
 export default function PicklistPicker({field}: {field: Field}) {
 	const selectField = field as SingleSelectField | MultiselectField;
 
+	const [hasError] = useState<boolean>(false);
 	const [picklists, setPicklists] = useState<Picklist[]>([]);
 	const [selectedKey, setSelectedKey] = useState<React.Key>(
 		selectField.picklistId
@@ -35,7 +37,8 @@ export default function PicklistPicker({field}: {field: Field}) {
 
 	const isPublished = publishedFields.has(field.uuid);
 
-	const id = useId();
+	const feedbackId = useId();
+	const pickerId = selectField.picklistInputUuid;
 
 	useEffect(() => {
 		PicklistService.getPicklists().then((picklists) =>
@@ -44,10 +47,10 @@ export default function PicklistPicker({field}: {field: Field}) {
 	}, []);
 
 	return (
-		<ClayForm.Group className="mb-2">
+		<ClayForm.Group className={classNames('mb-2', {'has-error': hasError})}>
 			<ClayInput.Group className="align-items-end">
 				<ClayInput.GroupItem>
-					<label htmlFor={id}>
+					<label htmlFor={pickerId}>
 						{Liferay.Language.get('picklist')}
 
 						<ClayIcon
@@ -57,12 +60,13 @@ export default function PicklistPicker({field}: {field: Field}) {
 					</label>
 
 					<Picker
+						aria-describedby={feedbackId}
 						aria-label={sub(
 							Liferay.Language.get('select-x'),
 							Liferay.Language.get('picklist')
 						)}
 						disabled={isPublished || !picklists.length}
-						id={id}
+						id={pickerId}
 						items={picklists}
 						onSelectionChange={(selectedKey: React.Key) => {
 							dispatch({
@@ -113,6 +117,15 @@ export default function PicklistPicker({field}: {field: Field}) {
 					)}
 				</ClayInput.GroupItem>
 			</ClayInput.Group>
+
+			{hasError ? (
+				<FieldFeedback
+					errorMessage={Liferay.Language.get(
+						'this-field-is-required'
+					)}
+					id={feedbackId}
+				/>
+			) : null}
 		</ClayForm.Group>
 	);
 }
