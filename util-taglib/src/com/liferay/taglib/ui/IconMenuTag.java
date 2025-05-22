ec72d7aa89a13b618e36523637fb5dcf4d6346c9
Georgel Pop <georgel.pop@liferay.com>
2025-05-22 11:46:41 +0200

    LPD-56213 Fix NPE mixing both solutions from LPD-22229 and LPS-195987 for more details please see 31f3f93fc03d859b9c8bc1ea86ee538c378cdd5f

--- util-taglib/src/com/liferay/taglib/ui/IconMenuTag.java
+++ util-taglib/src/com/liferay/taglib/ui/IconMenuTag.java
@@ -7,6 +7,7 @@ package com.liferay.taglib.ui;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.servlet.DirectRequestDispatcherFactoryUtil;
 import com.liferay.portal.kernel.servlet.FileAvailabilityUtil;
 import com.liferay.portal.kernel.servlet.taglib.aui.ScriptData;
 import com.liferay.portal.kernel.theme.PortletDisplay;
@@ -20,12 +21,14 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.taglib.BaseBodyTagSupport;
 import com.liferay.taglib.aui.ScriptTag;
+import com.liferay.taglib.servlet.PipingServletResponseFactory;
 import com.liferay.taglib.util.PortalIncludeUtil;
 import com.liferay.taglib.util.TagResourceBundleUtil;
 
 import java.util.Map;
 import java.util.Objects;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
@@ -289,13 +292,21 @@ public class IconMenuTag extends BaseBodyTagSupport implements BodyTag {
 
 		JspWriter jspWriter = pageContext.getOut();
 
+		ServletContext servletContext =
+			(ServletContext)httpServletRequest.getAttribute(WebKeys.CTX);
+
+		boolean servletContextIsNull = false;
+
+		if (servletContext == null) {
+			servletContextIsNull = true;
+			servletContext = pageContext.getServletContext();
+		}
+
 		if ((iconCount != null) && (iconCount.getValue() >= 1) &&
 			((singleIcon == null) || _showWhenSingleIcon)) {
 
 			if (!FileAvailabilityUtil.isAvailable(
-					(ServletContext)httpServletRequest.getAttribute(
-						WebKeys.CTX),
-					getStartPage()) ||
+					servletContext, getStartPage()) ||
 				!Objects.equals(_markupView, "lexicon")) {
 
 				if (_showExpanded) {
@@ -424,7 +435,19 @@ public class IconMenuTag extends BaseBodyTagSupport implements BodyTag {
 			else {
 				setAttributes();
 
-				PortalIncludeUtil.include(pageContext, getStartPage());
+				if (servletContextIsNull) {
+					RequestDispatcher requestDispatcher =
+						DirectRequestDispatcherFactoryUtil.getRequestDispatcher(
+							pageContext.getServletContext(), getStartPage());
+
+					requestDispatcher.include(
+						httpServletRequest,
+						PipingServletResponseFactory.
+							createPipingServletResponse(pageContext));
+				}
+				else {
+					PortalIncludeUtil.include(pageContext, getStartPage());
+				}
 			}
 		}
 
@@ -434,9 +457,7 @@ public class IconMenuTag extends BaseBodyTagSupport implements BodyTag {
 			((singleIcon == null) || _showWhenSingleIcon)) {
 
 			if (!FileAvailabilityUtil.isAvailable(
-					(ServletContext)httpServletRequest.getAttribute(
-						WebKeys.CTX),
-					getEndPage()) ||
+					servletContext, getEndPage()) ||
 				!Objects.equals(_markupView, "lexicon")) {
 
 				jspWriter.write("</ul>");
@@ -452,7 +473,19 @@ public class IconMenuTag extends BaseBodyTagSupport implements BodyTag {
 				}
 			}
 			else {
-				PortalIncludeUtil.include(pageContext, getEndPage());
+				if (servletContextIsNull) {
+					RequestDispatcher requestDispatcher =
+						DirectRequestDispatcherFactoryUtil.getRequestDispatcher(
+							pageContext.getServletContext(), getEndPage());
+
+					requestDispatcher.include(
+						httpServletRequest,
+						PipingServletResponseFactory.
+							createPipingServletResponse(pageContext));
+				}
+				else {
+					PortalIncludeUtil.include(pageContext, getEndPage());
+				}
 			}
 		}
 
