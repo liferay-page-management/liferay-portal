c9f73012548408b92f28a354828d6576a1d94182
Victor Galan <victor.galan@liferay.com>
2025-05-23 11:23:10 +0200

    LPD-56146 Add SpacesList fragment in structure display page

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/BaseSectionDisplayContext.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/BaseSectionDisplayContext.java
@@ -182,9 +182,9 @@ public abstract class BaseSectionDisplayContext {
 							themeDisplay.getPathMain(),
 							GroupConstants.CMS_FRIENDLY_URL,
 							"/add_structured_content_item?objectDefinitionId=",
-							objectDefinition.getObjectDefinitionId(),
-							"&plid=", themeDisplay.getPlid(),
-							"&redirect=", themeDisplay.getURLCurrent()));
+							objectDefinition.getObjectDefinitionId(), "&plid=",
+							themeDisplay.getPlid(), "&redirect=",
+							themeDisplay.getURLCurrent()));
 					dropdownItem.putData(
 						"title",
 						objectDefinition.getLabel(themeDisplay.getLocale()));
