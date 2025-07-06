a040c7be5c7436df59fb944c5e96a287f7dccc0d
Alberto Chaparro <alberto.chaparro@liferay.com>
2025-07-06 12:57:38 +0200

    LPD-55438 Support optional references

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/util/LayoutUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/util/LayoutUtil.java
@@ -452,15 +452,6 @@ public class LayoutUtil {
 			return;
 		}
 
-		ClientExtensionEntryRel clientExtensionEntryRel =
-			ClientExtensionEntryRelLocalServiceUtil.
-				fetchClientExtensionEntryRelByExternalReferenceCode(
-					cetExternalReferenceCode, layout.getCompanyId());
-
-		if (clientExtensionEntryRel != null) {
-			return;
-		}
-
 		ClientExtensionEntryRelLocalServiceUtil.deleteClientExtensionEntryRels(
 			PortalUtil.getClassNameId(Layout.class), layout.getPlid(), type);
 
