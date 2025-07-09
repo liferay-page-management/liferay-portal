9a586998628521f1418948dbc19c4c056f0c0239
Sandro Chinea <sandrochinea@gmail.com>
2025-07-09 10:48:50 +0100

    LPD-60065 Add waitFor pattern

--- modules/apps/site/site-cms-site-initializer/test/js/main_view/spaces/NewSpace.test.tsx
+++ modules/apps/site/site-cms-site-initializer/test/js/main_view/spaces/NewSpace.test.tsx
@@ -4,7 +4,7 @@
  */
 
 import '@testing-library/jest-dom/extend-expect';
-import {render, screen} from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 
@@ -48,11 +48,13 @@ describe('NewSpace', () => {
 		expect(
 			screen.getByRole('heading', {name: 'create-a-space'})
 		).toBeInTheDocument();
+
 		expect(
 			screen.getByText(
 				'spaces-are-essential-for-organizing-defining-and-managing-your-content-and-files'
 			)
 		).toBeInTheDocument();
+
 		expect(
 			screen.getByRole('link', {name: /Test Message/})
 		).toHaveAttribute('href', 'https://learn.liferay.com/test-url');
@@ -74,9 +76,11 @@ describe('NewSpace', () => {
 			'test'
 		);
 
-		expect(
-			screen.getByRole('button', {name: 'continue'})
-		).not.toBeDisabled();
+		await waitFor(() => {
+			expect(
+				screen.getByRole('button', {name: 'continue'})
+			).not.toBeDisabled();
+		});
 	});
 
 	it('submits form with correct values', async () => {
@@ -99,7 +103,9 @@ describe('NewSpace', () => {
 			spaceDescription
 		);
 
-		expect(apiPostSpy).not.toHaveBeenCalled();
+		await waitFor(() => {
+			expect(apiPostSpy).not.toHaveBeenCalled();
+		});
 
 		await userEvent.click(
 			screen.getByRole('button', {
@@ -107,17 +113,19 @@ describe('NewSpace', () => {
 			})
 		);
 
-		expect(apiPostSpy).toHaveBeenCalledTimes(1);
-		expect(apiPostSpy).toHaveBeenCalledWith(
-			'/o/headless-asset-library/v1.0/asset-libraries',
-			{
-				description: spaceDescription,
-				name: spaceName,
-				settings: {
-					logoColor: 'outline-0',
-				},
-			}
-		);
+		await waitFor(() => {
+			expect(apiPostSpy).toHaveBeenCalledTimes(1);
+			expect(apiPostSpy).toHaveBeenCalledWith(
+				'/o/headless-asset-library/v1.0/asset-libraries',
+				{
+					description: spaceDescription,
+					name: spaceName,
+					settings: {
+						logoColor: 'outline-0',
+					},
+				}
+			);
+		});
 	});
 
 	it('submits form with custom color', async () => {
@@ -151,14 +159,16 @@ describe('NewSpace', () => {
 			})
 		);
 
-		expect(apiPostSpy).toHaveBeenCalledWith(
-			'/o/headless-asset-library/v1.0/asset-libraries',
-			expect.objectContaining({
-				settings: {
-					logoColor: 'outline-1',
-				},
-			})
-		);
+		await waitFor(() => {
+			expect(apiPostSpy).toHaveBeenCalledWith(
+				'/o/headless-asset-library/v1.0/asset-libraries',
+				expect.objectContaining({
+					settings: {
+						logoColor: 'outline-1',
+					},
+				})
+			);
+		});
 	});
 
 	describe('hasErrors', () => {
@@ -175,7 +185,9 @@ describe('NewSpace', () => {
 
 			await userEvent.click(continueButton);
 
-			expect(apiPostSpy).not.toHaveBeenCalled();
+			await waitFor(() => {
+				expect(apiPostSpy).not.toHaveBeenCalled();
+			});
 
 			await userEvent.type(spaceNameInput, 'space');
 
@@ -183,11 +195,13 @@ describe('NewSpace', () => {
 
 			await userEvent.clear(spaceNameInput);
 
-			expect(
-				screen.getByRole('button', {
-					name: 'continue',
-				})
-			).toBeDisabled();
+			await waitFor(() => {
+				expect(
+					screen.getByRole('button', {
+						name: 'continue',
+					})
+				).toBeDisabled();
+			});
 		});
 
 		it('shows error message when space name is numeric', async () => {
@@ -208,11 +222,15 @@ describe('NewSpace', () => {
 				})
 			);
 
-			expect(apiPostSpy).not.toHaveBeenCalled();
+			await waitFor(() => {
+				expect(apiPostSpy).not.toHaveBeenCalled();
+			});
 
-			expect(
-				screen.getByText('please-enter-a-nonnumeric-name')
-			).toBeInTheDocument();
+			await waitFor(() => {
+				expect(
+					screen.getByText('please-enter-a-nonnumeric-name')
+				).toBeInTheDocument();
+			});
 		});
 
 		it('shows error message when space name is equal to null', async () => {
@@ -233,9 +251,15 @@ describe('NewSpace', () => {
 				})
 			);
 
-			expect(apiPostSpy).not.toHaveBeenCalled();
+			await waitFor(() => {
+				expect(apiPostSpy).not.toHaveBeenCalled();
+			});
 
-			expect(screen.getByText('name-cannot-be-null')).toBeInTheDocument();
+			await waitFor(() => {
+				expect(
+					screen.getByText('name-cannot-be-null')
+				).toBeInTheDocument();
+			});
 		});
 
 		it('shows error message when space name has an invalid character', async () => {
@@ -256,13 +280,17 @@ describe('NewSpace', () => {
 				})
 			);
 
-			expect(apiPostSpy).not.toHaveBeenCalled();
+			await waitFor(() => {
+				expect(apiPostSpy).not.toHaveBeenCalled();
+			});
 
-			expect(
-				screen.getByText(
-					'name-cannot-contain-the-following-invalid-characters-x'
-				)
-			).toBeInTheDocument();
+			await waitFor(() => {
+				expect(
+					screen.getByText(
+						'name-cannot-contain-the-following-invalid-characters-x'
+					)
+				).toBeInTheDocument();
+			});
 		});
 
 		it('shows error message when space name is more than 150 characters long', async () => {
@@ -283,11 +311,15 @@ describe('NewSpace', () => {
 				})
 			);
 
-			expect(apiPostSpy).not.toHaveBeenCalled();
+			await waitFor(() => {
+				expect(apiPostSpy).not.toHaveBeenCalled();
+			});
 
-			expect(
-				screen.getByText('please-enter-no-more-than-x-characters')
-			).toBeInTheDocument();
+			await waitFor(() => {
+				expect(
+					screen.getByText('please-enter-no-more-than-x-characters')
+				).toBeInTheDocument();
+			});
 		});
 	});
 });
