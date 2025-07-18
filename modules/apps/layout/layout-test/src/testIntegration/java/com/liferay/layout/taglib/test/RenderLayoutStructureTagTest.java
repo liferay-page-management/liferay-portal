cbdafd96cf54043e66bcca28e4dc2bb9fc275439
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-07-18 12:50:48 +0200

    LPD-61012 Prevent flaky tests because of using a random field name that could cause a FreeMarker error when rendering the template

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/taglib/test/RenderLayoutStructureTagTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/taglib/test/RenderLayoutStructureTagTest.java
@@ -2473,7 +2473,18 @@ public class RenderLayoutStructureTagTest {
 		MockObject mockObject = new MockObject(
 			RandomTestUtil.randomLong(), false, true);
 
-		InfoField<TextInfoFieldType> infoField = _getInfoField(false);
+		InfoField<TextInfoFieldType> infoField = InfoField.builder(
+		).infoFieldType(
+			TextInfoFieldType.INSTANCE
+		).namespace(
+			RandomTestUtil.randomString()
+		).name(
+			"fieldName"
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.singleValue(RandomTestUtil.randomString())
+		).localizable(
+			true
+		).build();
 
 		String value = RandomTestUtil.randomString();
 
@@ -2502,7 +2513,7 @@ public class RenderLayoutStructureTagTest {
 					).put(
 						"externalReferenceCode", RandomTestUtil.randomString()
 					).put(
-						"fieldId", infoField.getName()
+						"fieldId", "fieldName"
 					)),
 				"BASIC_COMPONENT-heading", draftLayout,
 				_segmentsExperienceLocalService.
@@ -2511,9 +2522,8 @@ public class RenderLayoutStructureTagTest {
 			TemplateEntry templateEntry = TemplateTestUtil.addTemplateEntry(
 				MockObject.class.getName(), "0", RandomTestUtil.randomString(),
 				RandomTestUtil.randomString(),
-				StringBundler.concat(
-					"<#if (", infoField.getName(), ".getData())??><p>${",
-					infoField.getName(), ".getData()}</p></#if>"),
+				"<#if (fieldName.getData())??><p>${fieldName.getData()}</p>" +
+					"</#if>",
 				_serviceContext);
 
 			ContentLayoutTestUtil.addFragmentEntryLinkToLayout(
