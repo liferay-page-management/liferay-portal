13f417717cd8c1ede189274b115e2efe94837929
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-07-23 10:31:38 +0200

    LPD-61267 Delete the template after the test due to bug LPD-60090

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageTemplateResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageTemplateResourceTest.java
@@ -1271,6 +1271,10 @@ public class PageTemplateResourceTest extends BasePageTemplateResourceTestCase {
 
 		_assertWidgetPageSpecifications(
 			pageTemplate.getPageSpecifications(), widgetPageSpecification);
+
+		_layoutPageTemplateEntryLocalService.deleteLayoutPageTemplateEntry(
+			layoutPageTemplateEntry.getExternalReferenceCode(),
+			testGroup.getGroupId());
 	}
 
 	private void _testPatchSiteSiteByExternalReferenceCodePageTemplateWithPageSpecifications()
