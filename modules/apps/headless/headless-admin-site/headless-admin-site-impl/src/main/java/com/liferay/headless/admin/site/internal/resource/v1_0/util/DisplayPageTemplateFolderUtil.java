d2b9ff82239e9c491e8c6c2bf357cc8d15a98dc3
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-07-31 10:05:48 +0200

    LPD-61754 Extract the creation logic of a display page template folder to reuse, no changes

---
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/util/DisplayPageTemplateFolderUtil.java
@@ -0,0 +1,99 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.site.internal.resource.v1_0.util;
+
+import com.liferay.headless.admin.site.dto.v1_0.DisplayPageTemplateFolder;
+import com.liferay.headless.common.spi.service.context.ServiceContextBuilder;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateCollectionTypeConstants;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateConstants;
+import com.liferay.layout.page.template.model.LayoutPageTemplateCollection;
+import com.liferay.layout.page.template.service.LayoutPageTemplateCollectionServiceUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.Validator;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Objects;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class DisplayPageTemplateFolderUtil {
+
+	public static LayoutPageTemplateCollection addLayoutPageTemplateCollection(
+			DisplayPageTemplateFolder displayPageTemplateFolder, long groupId,
+			HttpServletRequest httpServletRequest)
+		throws Exception {
+
+		return LayoutPageTemplateCollectionServiceUtil.
+			addLayoutPageTemplateCollection(
+				displayPageTemplateFolder.getExternalReferenceCode(), groupId,
+				getParentLayoutPageTemplateCollectionId(
+					displayPageTemplateFolder, groupId),
+				displayPageTemplateFolder.getKey(),
+				displayPageTemplateFolder.getName(),
+				displayPageTemplateFolder.getDescription(),
+				LayoutPageTemplateCollectionTypeConstants.DISPLAY_PAGE,
+				_getServiceContext(
+					displayPageTemplateFolder, groupId, httpServletRequest));
+	}
+
+	public static long getParentLayoutPageTemplateCollectionId(
+			DisplayPageTemplateFolder displayPageTemplateFolder, long groupId)
+		throws Exception {
+
+		long parentLayoutPageTemplateCollectionId =
+			LayoutPageTemplateConstants.
+				PARENT_LAYOUT_PAGE_TEMPLATE_COLLECTION_ID_DEFAULT;
+
+		if (Validator.isNull(
+				displayPageTemplateFolder.
+					getParentDisplayPageTemplateFolderExternalReferenceCode())) {
+
+			return parentLayoutPageTemplateCollectionId;
+		}
+
+		LayoutPageTemplateCollection parentLayoutPageTemplateCollection =
+			LayoutPageTemplateCollectionServiceUtil.
+				fetchLayoutPageTemplateCollection(
+					displayPageTemplateFolder.
+						getParentDisplayPageTemplateFolderExternalReferenceCode(),
+					groupId);
+
+		if (parentLayoutPageTemplateCollection != null) {
+			if (!Objects.equals(
+					LayoutPageTemplateCollectionTypeConstants.DISPLAY_PAGE,
+					parentLayoutPageTemplateCollection.getType())) {
+
+				throw new UnsupportedOperationException();
+			}
+
+			parentLayoutPageTemplateCollectionId =
+				parentLayoutPageTemplateCollection.
+					getLayoutPageTemplateCollectionId();
+		}
+
+		return parentLayoutPageTemplateCollectionId;
+	}
+
+	private static ServiceContext _getServiceContext(
+		DisplayPageTemplateFolder displayPageTemplateFolder, long groupId,
+		HttpServletRequest httpServletRequest) {
+
+		ServiceContext serviceContext = ServiceContextBuilder.create(
+			groupId, httpServletRequest, null
+		).build();
+
+		serviceContext.setCreateDate(
+			displayPageTemplateFolder.getDateCreated());
+		serviceContext.setModifiedDate(
+			displayPageTemplateFolder.getDateModified());
+		serviceContext.setUuid(displayPageTemplateFolder.getUuid());
+
+		return serviceContext;
+	}
+
+}
