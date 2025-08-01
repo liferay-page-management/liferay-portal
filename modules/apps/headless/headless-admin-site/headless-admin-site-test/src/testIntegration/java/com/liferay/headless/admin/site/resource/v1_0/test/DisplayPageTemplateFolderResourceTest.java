5e71d05a2604eb1432deb9357f1243a5648cb7c3
Rubén Pulido <ruben.pulido@liferay.com>
2025-08-01 18:06:52 +0200

    LPD-61754 Add helper methods to convert between client and server DisplayPageTemplateFolder DTOs

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/DisplayPageTemplateFolderResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/DisplayPageTemplateFolderResourceTest.java
@@ -368,6 +368,32 @@ public class DisplayPageTemplateFolderResourceTest
 			testPutSiteDisplayPageTemplateFolderPermissionsPage_addDisplayPageTemplateFolder();
 	}
 
+	private static
+		com.liferay.headless.admin.site.dto.v1_0.DisplayPageTemplateFolder
+			_toDisplayPageTemplateFolder(
+				DisplayPageTemplateFolder displayPageTemplateFolder) {
+
+		if (displayPageTemplateFolder == null) {
+			return null;
+		}
+
+		return com.liferay.headless.admin.site.dto.v1_0.
+			DisplayPageTemplateFolder.toDTO(
+				displayPageTemplateFolder.toString());
+	}
+
+	private static DisplayPageTemplateFolder _toDisplayPageTemplateFolder(
+		com.liferay.headless.admin.site.dto.v1_0.DisplayPageTemplateFolder
+			displayPageTemplateFolder) {
+
+		if (displayPageTemplateFolder == null) {
+			return null;
+		}
+
+		return DisplayPageTemplateFolder.toDTO(
+			displayPageTemplateFolder.toString());
+	}
+
 	private void _assertNoParentDisplayPageTemplateFolder(
 			DisplayPageTemplateFolder displayPageTemplateFolder)
 		throws Exception {
