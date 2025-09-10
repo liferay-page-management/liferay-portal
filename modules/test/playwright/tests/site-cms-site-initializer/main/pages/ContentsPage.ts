179aa7674c5888932b673be55e482907bead6114
Sandro Chinea <sandrochinea@gmail.com>
2025-09-10 13:30:50 +0100

    LPD-64979 Add playwright test

--- modules/test/playwright/tests/site-cms-site-initializer/main/pages/ContentsPage.ts
+++ modules/test/playwright/tests/site-cms-site-initializer/main/pages/ContentsPage.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {Locator, Page} from '@playwright/test';
+import {Locator, Page, expect} from '@playwright/test';
 
 import {clickAndExpectToBeHidden} from '../../../../utils/clickAndExpectToBeHidden';
 import {clickAndExpectToBeVisible} from '../../../../utils/clickAndExpectToBeVisible';
@@ -163,5 +163,21 @@ export class ContentsPage {
 			timeout: 5000,
 			trigger: this.publishButton,
 		});
+	}
+
+	async translateContent(title: string) {
+		const card = this.page
+			.locator('tr', {hasText: title})
+			.or(this.page.locator('.card-row', {hasText: title}));
+
+		await clickAndExpectToBeVisible({
+			autoClick: true,
+			target: this.page.getByRole('menuitem', {name: 'Translate'}),
+			trigger: card.locator('button'),
+		});
+
+		await expect(
+			this.page.locator('.management-bar').getByText('Publish')
+		).toBeVisible();
 	}
 }
