e2750e6660a48fee0fa35d477118ec199f38cc89
Georgel Pop <georgel.pop@liferay.com>
2025-09-15 14:01:34 +0200

    LPD-63444 Remove unused methods in FragmentEntryLinkLocalServiceImpl

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLinkLocalServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLinkLocalServiceImpl.java
@@ -486,24 +486,6 @@ public class FragmentEntryLinkLocalServiceImpl
 	}
 
 	@Override
-	public List<FragmentEntryLink> getFragmentEntryLinksByFragmentEntryId(
-		long fragmentEntryId, boolean deleted) {
-
-		ResultERC resultERC = _getResultERC(fragmentEntryId);
-
-		if (resultERC.isFragmentEntryScopeGlobal()) {
-			return fragmentEntryLinkPersistence.findByFEERC_FESERC_D(
-				resultERC.getFragmentEntryERC(),
-				resultERC.getFragmentEntryScopeERC(), deleted);
-		}
-
-		return fragmentEntryLinkPersistence.findByG_FEERC_FESERC_D(
-			resultERC.getFragmentEntryGroupId(),
-			resultERC.getFragmentEntryERC(),
-			resultERC.getFragmentEntryScopeERC(), deleted);
-	}
-
-	@Override
 	public List<FragmentEntryLink> getFragmentEntryLinksByPlid(
 		long groupId, long plid) {
 
@@ -790,21 +772,6 @@ public class FragmentEntryLinkLocalServiceImpl
 
 	@Override
 	public FragmentEntryLink updateFragmentEntryLink(
-			long userId, long fragmentEntryLinkId, int position)
-		throws PortalException {
-
-		FragmentEntryLink fragmentEntryLink = fetchFragmentEntryLink(
-			fragmentEntryLinkId);
-
-		_checkUnlockedLayout(fragmentEntryLink.getPlid(), userId);
-
-		fragmentEntryLink.setPosition(position);
-
-		return fragmentEntryLinkPersistence.update(fragmentEntryLink);
-	}
-
-	@Override
-	public FragmentEntryLink updateFragmentEntryLink(
 			long userId, long fragmentEntryLinkId,
 			long originalFragmentEntryLinkId, long fragmentEntryId, long plid,
 			String css, String html, String js, String configuration,
@@ -850,15 +817,6 @@ public class FragmentEntryLinkLocalServiceImpl
 		fragmentEntryLink.setType(type);
 
 		return fragmentEntryLinkPersistence.update(fragmentEntryLink);
-	}
-
-	@Override
-	public FragmentEntryLink updateFragmentEntryLink(
-			long userId, long fragmentEntryLinkId, String editableValues)
-		throws PortalException {
-
-		return updateFragmentEntryLink(
-			userId, fragmentEntryLinkId, editableValues, true);
 	}
 
 	@Override
