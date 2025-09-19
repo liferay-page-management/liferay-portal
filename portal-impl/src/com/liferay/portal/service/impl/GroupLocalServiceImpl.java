43124f3c6c63cb05855a66061c799621f8c6b242
Georgel Pop <georgel.pop@liferay.com>
2025-09-19 13:13:55 +0200

    LPD-63444 Add GUEST_FRIENDLY_URL in GroupConstants.java, replace usages

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -869,7 +869,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 					site = false;
 				}
 				else if (groupKey.equals(GroupConstants.GUEST)) {
-					friendlyURL = "/guest";
+					friendlyURL = GroupConstants.GUEST_FRIENDLY_URL;
 					typeSettingsUnicodeProperties =
 						UnicodePropertiesBuilder.create(
 							true
