9fe87437f2d16650749c6b6bbde897af639da4e3
Mikel Lorza <mikel.lorza@liferay.com>
2025-09-23 12:56:32 +0200

    LPD-65996 Set module external values

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/ModulePageElementDefinitionDTOConverter.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/ModulePageElementDefinitionDTOConverter.java
@@ -1,14 +1,24 @@
 /**
- * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.headless.admin.site.internal.dto.v1_0.converter;
 
 import com.liferay.headless.admin.site.dto.v1_0.ModulePageElementDefinition;
+import com.liferay.headless.admin.site.dto.v1_0.ModuleViewport;
+import com.liferay.headless.admin.site.dto.v1_0.ModuleViewportDefinition;
+import com.liferay.headless.admin.site.internal.dto.v1_0.util.ViewportUtil;
 import com.liferay.layout.util.structure.ColumnLayoutStructureItem;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -36,10 +46,103 @@ public class ModulePageElementDefinitionDTOConverter
 
 		return new ModulePageElementDefinition() {
 			{
+				setModuleViewports(
+					() -> _toModuleViewports(columnLayoutStructureItem));
 				setSize(columnLayoutStructureItem::getSize);
 				setType(Type.MODULE);
 			}
 		};
+	}
+
+	private ModuleViewport _toModuleViewport(
+		Map<String, JSONObject> columnViewportConfigurationJSONObjects,
+		ModuleViewport.Id moduleViewportId) {
+
+		String viewportIdInternalValue = ViewportUtil.toInternalValue(
+			moduleViewportId.getValue());
+
+		if (!columnViewportConfigurationJSONObjects.containsKey(
+				viewportIdInternalValue)) {
+
+			return null;
+		}
+
+		JSONObject columnViewportConfigurationJSONObject =
+			columnViewportConfigurationJSONObjects.get(viewportIdInternalValue);
+
+		if (JSONUtil.isEmpty(columnViewportConfigurationJSONObject)) {
+			return null;
+		}
+
+		return new ModuleViewport() {
+			{
+				setId(() -> moduleViewportId);
+				setModuleViewportDefinition(
+					() -> _toModuleViewportDefinition(
+						columnViewportConfigurationJSONObject));
+			}
+		};
+	}
+
+	private ModuleViewportDefinition _toModuleViewportDefinition(
+		JSONObject columnViewportConfigurationJSONObject) {
+
+		return new ModuleViewportDefinition() {
+			{
+				setSize(
+					() -> {
+						if (!columnViewportConfigurationJSONObject.has(
+								"size")) {
+
+							return null;
+						}
+
+						return columnViewportConfigurationJSONObject.getInt(
+							"size");
+					});
+			}
+		};
+	}
+
+	private ModuleViewport[] _toModuleViewports(
+		ColumnLayoutStructureItem columnLayoutStructureItem) {
+
+		Map<String, JSONObject> columnViewportConfigurationJSONObjects =
+			columnLayoutStructureItem.getViewportConfigurationJSONObjects();
+
+		if (MapUtil.isEmpty(columnViewportConfigurationJSONObjects)) {
+			return null;
+		}
+
+		List<ModuleViewport> moduleViewports = new ArrayList<>() {
+			{
+				ModuleViewport moduleViewport = _toModuleViewport(
+					columnViewportConfigurationJSONObjects,
+					ModuleViewport.Id.LANDSCAPE_MOBILE);
+
+				if (moduleViewport != null) {
+					add(moduleViewport);
+				}
+
+				moduleViewport = _toModuleViewport(
+					columnViewportConfigurationJSONObjects,
+					ModuleViewport.Id.PORTRAIT_MOBILE);
+
+				if (moduleViewport != null) {
+					add(moduleViewport);
+				}
+
+				moduleViewport = _toModuleViewport(
+					columnViewportConfigurationJSONObjects,
+					ModuleViewport.Id.TABLET);
+
+				if (moduleViewport != null) {
+					add(moduleViewport);
+				}
+			}
+		};
+
+		return moduleViewports.toArray(new ModuleViewport[0]);
 	}
 
 }
