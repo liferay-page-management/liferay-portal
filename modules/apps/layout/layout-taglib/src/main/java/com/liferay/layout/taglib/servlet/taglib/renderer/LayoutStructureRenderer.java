a34a3d5c607eb54d86ef528e38b9e2df2c17984f
Victor Galan <victor.galan@liferay.com>
2025-09-25 14:46:23 +0200

    LPD-60410 Set the ERC to the request to be used in the name of the input

--- modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/servlet/taglib/renderer/LayoutStructureRenderer.java
+++ modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/servlet/taglib/renderer/LayoutStructureRenderer.java
@@ -41,6 +41,7 @@ import com.liferay.layout.display.page.constants.LayoutDisplayPageWebKeys;
 import com.liferay.layout.helper.structure.LayoutStructureRulesHelper;
 import com.liferay.layout.list.retriever.ListObjectReference;
 import com.liferay.layout.responsive.ResponsiveLayoutStructureUtil;
+import com.liferay.layout.taglib.constants.LayoutStructureRendererConstants;
 import com.liferay.layout.taglib.internal.display.context.RenderCollectionLayoutStructureItemDisplayContext;
 import com.liferay.layout.taglib.internal.display.context.RenderLayoutStructureDisplayContext;
 import com.liferay.layout.taglib.internal.servlet.ServletContextUtil;
@@ -964,9 +965,75 @@ public class LayoutStructureRenderer {
 
 		jspWriter.write("\">");
 
-		_renderLayoutStructure(
-			formRelationshipStyledLayoutStructureItem.getChildrenItemIds(),
-			infoForm);
+		LayoutDisplayPageObjectProvider<?> layoutDisplayPageObjectProvider =
+			(LayoutDisplayPageObjectProvider<?>)
+				_httpServletRequest.getAttribute(
+					LayoutDisplayPageWebKeys.
+						LAYOUT_DISPLAY_PAGE_OBJECT_PROVIDER);
+
+		if (layoutDisplayPageObjectProvider == null) {
+			_renderFormRelationshipStyledLayoutStructureItem(
+				infoForm, formRelationshipStyledLayoutStructureItem);
+		}
+		else {
+			List<? extends LayoutDisplayPageObjectProvider<?>>
+				relatedLayoutDisplayPageObjectProviders =
+					layoutDisplayPageObjectProvider.
+						getRelatedLayoutDisplayPageObjectProviders(
+							formRelationshipStyledLayoutStructureItem.
+								getContentType());
+
+			if (ListUtil.isEmpty(relatedLayoutDisplayPageObjectProviders)) {
+				_renderLayoutStructure(
+					formRelationshipStyledLayoutStructureItem.
+						getChildrenItemIds(),
+					infoForm);
+
+				return;
+			}
+
+			String currentParentItemExternalReferenceCode =
+				(String)_httpServletRequest.getAttribute(
+					LayoutStructureRendererConstants.
+						LAYOUT_PARENT_ITEM_EXTERNAL_REFERENCE_CODE);
+			String currentRelatedItemExternalReferenceCode =
+				(String)_httpServletRequest.getAttribute(
+					LayoutStructureRendererConstants.
+						LAYOUT_RELATED_ITEM_EXTERNAL_REFERENCE_CODE);
+
+			try {
+				for (LayoutDisplayPageObjectProvider<?>
+						relatedLayoutDisplayPageObjectProvider :
+							relatedLayoutDisplayPageObjectProviders) {
+
+					_httpServletRequest.setAttribute(
+						LayoutStructureRendererConstants.
+							LAYOUT_PARENT_ITEM_EXTERNAL_REFERENCE_CODE,
+						relatedLayoutDisplayPageObjectProvider.
+							getParentExternalReferenceCode());
+					_httpServletRequest.setAttribute(
+						LayoutStructureRendererConstants.
+							LAYOUT_RELATED_ITEM_EXTERNAL_REFERENCE_CODE,
+						relatedLayoutDisplayPageObjectProvider.
+							getExternalReferenceCode());
+
+					_renderLayoutStructure(
+						formRelationshipStyledLayoutStructureItem.
+							getChildrenItemIds(),
+						infoForm);
+				}
+			}
+			finally {
+				_httpServletRequest.setAttribute(
+					LayoutStructureRendererConstants.
+						LAYOUT_PARENT_ITEM_EXTERNAL_REFERENCE_CODE,
+					currentParentItemExternalReferenceCode);
+				_httpServletRequest.setAttribute(
+					LayoutStructureRendererConstants.
+						LAYOUT_RELATED_ITEM_EXTERNAL_REFERENCE_CODE,
+					currentRelatedItemExternalReferenceCode);
+			}
+		}
 
 		_renderReactComponent(
 			"{FormRelationshipAddButton} from layout-taglib/render",
