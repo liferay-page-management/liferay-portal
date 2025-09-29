d1410e50420585eee7610a2f25be8b2850f83f30
Alberto Chaparro <alberto.chaparro@liferay.com>
2025-09-29 23:01:05 +0200

    LPD-66795 Add a temporary way to register optional references until the Headless framework is ready (see LPD-65084)

---
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/util/LogUtil.java
@@ -0,0 +1,32 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.site.internal.util;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+/**
+ * @author Alberto Chaparro
+ */
+public class LogUtil {
+
+	public static void logOptionalReference(
+		Class<?> modelClass, String modelExternalReferenceCode) {
+
+		if (_log.isWarnEnabled()) {
+			StringBundler sb = new StringBundler(4);
+
+			sb.append("Optional reference generated for missing ");
+			sb.append(modelClass.getSimpleName());
+			sb.append(" with external reference code ");
+			sb.append(modelExternalReferenceCode);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(LogUtil.class);
+
+}
