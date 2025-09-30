dec34e3853fdccd0d79041146678d4726a09f33b
Georgel Pop <georgel.pop@liferay.com>
2025-09-30 18:26:39 +0200

    LPD-63444 Add new methods getScopeExternalReferenceCode in FragmentEntryImpl.java and getFragmentEntryGroupId in FragmentEntryLinkImpl.java

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/model/impl/FragmentEntryLinkImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/model/impl/FragmentEntryLinkImpl.java
@@ -66,6 +66,25 @@ public class FragmentEntryLinkImpl extends FragmentEntryLinkBaseImpl {
 		return _editableValuesJSONObject;
 	}
 
+	@Override
+	public long getFragmentEntryGroupId() {
+		if (Validator.isNotNull(getFragmentEntryScopeExternalReferenceCode())) {
+			try {
+				Group fragmentEntryGroup =
+					GroupLocalServiceUtil.getGroupByExternalReferenceCode(
+						getFragmentEntryScopeExternalReferenceCode(),
+						getCompanyId());
+
+				return fragmentEntryGroup.getGroupId();
+			}
+			catch (PortalException portalException) {
+				throw new RuntimeException(portalException);
+			}
+		}
+
+		return getGroupId();
+	}
+
 	/**
 	 * @deprecated As of Cavanaugh (7.4.x), with no direct replacement
 	 */
