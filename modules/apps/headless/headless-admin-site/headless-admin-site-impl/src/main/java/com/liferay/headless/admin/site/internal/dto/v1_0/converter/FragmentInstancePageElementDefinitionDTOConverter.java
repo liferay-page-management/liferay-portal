86fc2b6e467ac53ff23337434400ec74a2e74e1c
Rubén Pulido <ruben.pulido@liferay.com>
2025-10-08 22:43:41 +0200

    LPD-67257 Don't set type when instantiating FragmentInstancePageElementDefinition since it is already output by Jackson based on the class.
    If set, type is output twice, as follows:
    ```
    {
     "type" : "Fragment",
     ...
     "type" : "Fragment"
    }
    ```

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/FragmentInstancePageElementDefinitionDTOConverter.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/FragmentInstancePageElementDefinitionDTOConverter.java
@@ -165,8 +165,6 @@ public class FragmentInstancePageElementDefinitionDTOConverter
 			fragmentStyledLayoutStructureItem::getName);
 		fragmentInstancePageElementDefinition.setNamespace(
 			fragmentEntryLink::getNamespace);
-		fragmentInstancePageElementDefinition.setType(
-			PageElementDefinition.Type.FRAGMENT);
 		fragmentInstancePageElementDefinition.setUuid(
 			fragmentEntryLink::getUuid);
 
