5514b0211fd8cf0e602f822c1041cd87aeae2a45
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-10-17 10:23:07 +0200

    LPD-68099 Add modal for the display date

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/SchedulePublicationModal.tsx
@@ -0,0 +1,72 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayButton from '@clayui/button';
+import ClayModal, {useModal} from '@clayui/modal';
+import {sub} from 'frontend-js-web';
+import React from 'react';
+
+import ScheduleField, {dateConfig} from './ScheduleField';
+
+export default function SchedulePublicationModal({
+	date,
+	onCloseModal,
+}: {
+	date: string;
+	onCloseModal: () => void;
+}) {
+	const {observer, onClose} = useModal({
+		onClose: () => onCloseModal(),
+	});
+
+	const onCancel = () => {
+		onClose();
+	};
+
+	const onSchedule = () => {
+		onClose();
+	};
+
+	return (
+		<ClayModal observer={observer}>
+			<ClayModal.Header>
+				{Liferay.Language.get('schedule-publication')}
+			</ClayModal.Header>
+
+			<ClayModal.Body>
+				<p className="text-secondary">
+					{sub(
+						Liferay.Language.get(
+							'set-the-date-and-time-for-publishing-the-x'
+						),
+						'content'
+					)}
+				</p>
+
+				<ScheduleField
+					date={date}
+					dateConfig={dateConfig}
+					label={Liferay.Language.get('date-and-time')}
+					name="displayDate"
+					updateFieldData={() => null}
+				/>
+			</ClayModal.Body>
+
+			<ClayModal.Footer
+				last={
+					<ClayButton.Group spaced>
+						<ClayButton displayType="secondary" onClick={onCancel}>
+							{Liferay.Language.get('cancel')}
+						</ClayButton>
+
+						<ClayButton onClick={onSchedule}>
+							{Liferay.Language.get('schedule')}
+						</ClayButton>
+					</ClayButton.Group>
+				}
+			/>
+		</ClayModal>
+	);
+}
