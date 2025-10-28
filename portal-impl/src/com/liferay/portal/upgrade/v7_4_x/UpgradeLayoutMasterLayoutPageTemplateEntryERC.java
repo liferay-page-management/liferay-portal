48f570f06d2bd37efc200f606c98be33297c95fd
Alberto Chaparro <alberto.chaparro@liferay.com>
2025-10-28 20:38:22 +0100

    LPD-68031 Add upgrade process

---
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradeLayoutMasterLayoutPageTemplateEntryERC.java
@@ -0,0 +1,65 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.upgrade.v7_4_x;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
+import com.liferay.portal.kernel.upgrade.UpgradeStep;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author Alberto Chaparro
+ */
+public class UpgradeLayoutMasterLayoutPageTemplateEntryERC
+	extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
+				StringBundler.concat(
+					"select LayoutPageTemplateEntry.externalReferenceCode, ",
+					"Layout.plid from Layout inner join ",
+					"LayoutPageTemplateEntry on Layout.masterLayoutPlid = ",
+					"LayoutPageTemplateEntry.plid where ",
+					"Layout.masterLayoutPlid > 0"));
+			ResultSet resultSet = preparedStatement1.executeQuery();
+			PreparedStatement preparedStatement2 =
+				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
+					connection,
+					"update Layout set MLayoutPageTemplateEntryERC = ? " +
+						"where plid = ?")) {
+
+			while (resultSet.next()) {
+				preparedStatement2.setString(1, resultSet.getString(1));
+				preparedStatement2.setLong(2, resultSet.getLong(2));
+
+				preparedStatement2.addBatch();
+			}
+
+			preparedStatement2.executeBatch();
+		}
+	}
+
+	@Override
+	protected UpgradeStep[] getPostUpgradeSteps() {
+		return new UpgradeStep[] {
+			UpgradeProcessFactory.dropColumns("Layout", "masterLayoutPlid")
+		};
+	}
+
+	@Override
+	protected UpgradeStep[] getPreUpgradeSteps() {
+		return new UpgradeStep[] {
+			UpgradeProcessFactory.addColumns(
+				"Layout", "MLayoutPageTemplateEntryERC VARCHAR(75) null")
+		};
+	}
+
+}
