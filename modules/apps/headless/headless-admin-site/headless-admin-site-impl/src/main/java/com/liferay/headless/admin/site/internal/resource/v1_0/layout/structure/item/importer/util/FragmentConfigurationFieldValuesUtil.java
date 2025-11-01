ba8a2591d7abf795886997769ade600e926bea06
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-11-01 21:05:20 +0100

    LPD-69116 Add a Util to import the fragment configuration field values with support for the types: Checkbox, Length, Select, and Text

---
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/layout/structure/item/importer/util/FragmentConfigurationFieldValuesUtil.java
@@ -0,0 +1,150 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.site.internal.resource.v1_0.layout.structure.item.importer.util;
+
+import com.liferay.fragment.util.configuration.FragmentConfigurationField;
+import com.liferay.fragment.util.configuration.FragmentEntryConfigurationParserUtil;
+import com.liferay.headless.admin.site.dto.v1_0.CheckboxFragmentConfigurationFieldValue;
+import com.liferay.headless.admin.site.dto.v1_0.FragmentConfigurationFieldValue;
+import com.liferay.headless.admin.site.dto.v1_0.LengthFragmentConfigurationFieldValue;
+import com.liferay.headless.admin.site.dto.v1_0.SelectFragmentConfigurationFieldValue;
+import com.liferay.headless.admin.site.dto.v1_0.TextFragmentConfigurationFieldValue;
+import com.liferay.headless.admin.site.internal.dto.v1_0.util.FragmentConfigurationFieldValueTypeUtil;
+import com.liferay.headless.admin.site.internal.dto.v1_0.util.LocalizedValueUtil;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class FragmentConfigurationFieldValuesUtil {
+
+	public static JSONObject getFreeMarkerFragmentEntryProcessorJSONObject(
+			String configuration,
+			Map<String, FragmentConfigurationFieldValue>
+				fragmentConfigurationFieldValuesMap)
+		throws Exception {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		if (fragmentConfigurationFieldValuesMap == null) {
+			return jsonObject;
+		}
+
+		JSONObject configurationJSONObject = JSONFactoryUtil.createJSONObject(
+			configuration);
+
+		for (FragmentConfigurationField fragmentConfigurationField :
+				FragmentEntryConfigurationParserUtil.
+					getFragmentConfigurationFields(configurationJSONObject)) {
+
+			FragmentConfigurationFieldValue fragmentConfigurationFieldValue =
+				fragmentConfigurationFieldValuesMap.get(
+					fragmentConfigurationField.getName());
+
+			if (fragmentConfigurationFieldValue == null) {
+				continue;
+			}
+
+			if (!Objects.equals(
+					fragmentConfigurationFieldValue.getType(),
+					FragmentConfigurationFieldValueTypeUtil.toExternalType(
+						fragmentConfigurationField.getType()))) {
+
+				throw new UnsupportedOperationException();
+			}
+
+			jsonObject.put(
+				fragmentConfigurationField.getName(),
+				_fromFragmentConfigurationFieldValue(
+					fragmentConfigurationFieldValue,
+					fragmentConfigurationField));
+		}
+
+		return jsonObject;
+	}
+
+	private static Object _fromFragmentConfigurationFieldValue(
+		FragmentConfigurationFieldValue fragmentConfigurationFieldValue,
+		FragmentConfigurationField fragmentConfigurationField) {
+
+		if (Objects.equals(
+				fragmentConfigurationFieldValue.getType(),
+				FragmentConfigurationFieldValue.Type.CHECKBOX)) {
+
+			CheckboxFragmentConfigurationFieldValue
+				checkboxFragmentConfigurationFieldValue =
+					(CheckboxFragmentConfigurationFieldValue)
+						fragmentConfigurationFieldValue;
+
+			return _getConfigurationObject(
+				fragmentConfigurationField.isLocalizable(),
+				checkboxFragmentConfigurationFieldValue.getValue(),
+				checkboxFragmentConfigurationFieldValue.getValue_i18n());
+		}
+
+		if (Objects.equals(
+				fragmentConfigurationFieldValue.getType(),
+				FragmentConfigurationFieldValue.Type.LENGTH)) {
+
+			LengthFragmentConfigurationFieldValue
+				lengthFragmentConfigurationFieldValue =
+					(LengthFragmentConfigurationFieldValue)
+						fragmentConfigurationFieldValue;
+
+			return _getConfigurationObject(
+				fragmentConfigurationField.isLocalizable(),
+				lengthFragmentConfigurationFieldValue.getValue(),
+				lengthFragmentConfigurationFieldValue.getValue_i18n());
+		}
+
+		if (Objects.equals(
+				fragmentConfigurationFieldValue.getType(),
+				FragmentConfigurationFieldValue.Type.SELECT)) {
+
+			SelectFragmentConfigurationFieldValue
+				selectFragmentConfigurationFieldValue =
+					(SelectFragmentConfigurationFieldValue)
+						fragmentConfigurationFieldValue;
+
+			return _getConfigurationObject(
+				fragmentConfigurationField.isLocalizable(),
+				selectFragmentConfigurationFieldValue.getValue(),
+				selectFragmentConfigurationFieldValue.getValue_i18n());
+		}
+
+		if (Objects.equals(
+				fragmentConfigurationFieldValue.getType(),
+				FragmentConfigurationFieldValue.Type.TEXT)) {
+
+			TextFragmentConfigurationFieldValue
+				textFragmentConfigurationFieldValue =
+					(TextFragmentConfigurationFieldValue)
+						fragmentConfigurationFieldValue;
+
+			return _getConfigurationObject(
+				fragmentConfigurationField.isLocalizable(),
+				textFragmentConfigurationFieldValue.getValue(),
+				textFragmentConfigurationFieldValue.getValue_i18n());
+		}
+
+		return null;
+	}
+
+	private static <T> Object _getConfigurationObject(
+		boolean localizable, T value, Map<String, T> valuesMap) {
+
+		if (!localizable) {
+			return value;
+		}
+
+		return LocalizedValueUtil.toJSONObject(valuesMap, curValue -> curValue);
+	}
+
+}
