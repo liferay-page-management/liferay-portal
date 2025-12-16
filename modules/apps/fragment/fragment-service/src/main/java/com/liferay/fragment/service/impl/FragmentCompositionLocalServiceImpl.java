58e44ec7a91f8e0ebafa19e92672db9d6387c9a3
Victor Galan <victor.galan@liferay.com>
2025-12-16 23:19:50 +0100

    LPD-74556 Move preview deletion to service for FragmentComposition
    - Inject PortletFileRepository into FragmentCompositionLocalServiceImpl
    - Delete previous preview file entry when clearing preview (previewFileEntryId = 0)
    - Update DeleteFragmentCompositionPreviewMVCActionCommand to delegate to service

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentCompositionLocalServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentCompositionLocalServiceImpl.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.model.ModelHintsUtil;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepository;
 import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
 import com.liferay.portal.kernel.service.ResourceLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -308,6 +309,15 @@ public class FragmentCompositionLocalServiceImpl
 			fragmentCompositionPersistence.findByPrimaryKey(
 				fragmentCompositionId);
 
+
+		long previousPreviewFileEntryId =
+			fragmentComposition.getPreviewFileEntryId();
+
+		if ((previewFileEntryId == 0) && (previousPreviewFileEntryId > 0)) {
+			_portletFileRepository.deletePortletFileEntry(
+				previousPreviewFileEntryId);
+		}
+
 		fragmentComposition.setModifiedDate(new Date());
 		fragmentComposition.setPreviewFileEntryId(previewFileEntryId);
 
@@ -428,6 +438,9 @@ public class FragmentCompositionLocalServiceImpl
 
 	@Reference
 	private ResourceLocalService _resourceLocalService;
+
+	@Reference
+	private PortletFileRepository _portletFileRepository;
 
 	@Reference
 	private UserLocalService _userLocalService;
