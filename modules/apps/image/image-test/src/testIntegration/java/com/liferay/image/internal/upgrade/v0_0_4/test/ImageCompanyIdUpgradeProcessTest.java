f66d39ff3197862cb039c335eb52ae22c7aa4935
Georgel Pop <georgel.pop@liferay.com>
2026-02-06 10:57:02 +0100

    LPD-76573 Add test for new upgrade ImageCompanyIdUpgradeProcessTest.java

---
+++ modules/apps/image/image-test/src/testIntegration/java/com/liferay/image/internal/upgrade/v0_0_4/test/ImageCompanyIdUpgradeProcessTest.java
@@ -0,0 +1,109 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.image.internal.upgrade.v0_0_4.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.portal.kernel.cache.MultiVMPool;
+import com.liferay.portal.kernel.dao.orm.EntityCache;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Image;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.ImageLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.version.Version;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Georgel Pop
+ */
+@RunWith(Arquillian.class)
+public class ImageCompanyIdUpgradeProcessTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testUpgrade() throws Exception {
+		Layout layout = LayoutTestUtil.addTypePortletLayout(_group);
+
+		layout = _layoutLocalService.updateIconImage(
+			layout.getPlid(),
+			FileUtil.getBytes(getClass(), "dependencies/liferay.jpg"));
+
+		Image image = _imageLocalService.getImage(layout.getIconImageId());
+
+		image.setCompanyId(RandomTestUtil.randomLong());
+
+		image = _imageLocalService.updateImage(image);
+
+		_runUpgrade();
+
+		Image expectedImage = _imageLocalService.getImage(image.getImageId());
+
+		Assert.assertEquals(
+			expectedImage.getCompanyId(), layout.getCompanyId());
+	}
+
+	private void _runUpgrade() throws Exception {
+		UpgradeProcess[] upgradeProcesses = UpgradeTestUtil.getUpgradeSteps(
+			_upgradeStepRegistrator, Version.parseVersion("0.0.4"));
+
+		UpgradeProcess upgradeProcess = upgradeProcesses[0];
+
+		upgradeProcess.upgrade();
+
+		_entityCache.clearCache();
+		_multiVMPool.clear();
+	}
+
+	@Inject(
+		filter = "(&(component.name=com.liferay.image.internal.upgrade.registry.ImageServiceUpgradeStepRegistrator))"
+	)
+	private static UpgradeStepRegistrator _upgradeStepRegistrator;
+
+	@Inject
+	private EntityCache _entityCache;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private ImageLocalService _imageLocalService;
+
+	@Inject
+	private LayoutLocalService _layoutLocalService;
+
+	@Inject
+	private MultiVMPool _multiVMPool;
+
+}
