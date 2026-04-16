de3416120a7ef69716ae970d302d28b00f7540f7
Juanjo Fernandez <juanjo.fernandez@liferay.com>
2026-04-16 09:25:21 +0200

    LPD-78935 - Set today as min date for the expiration date filter

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/filter/ExpirationDateRangeFDSFilter.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/filter/ExpirationDateRangeFDSFilter.java
@@ -11,6 +11,8 @@ import com.liferay.frontend.data.set.filter.DateFDSFilterItem;
 import com.liferay.frontend.data.set.filter.FDSFilter;
 import com.liferay.site.cms.site.initializer.internal.constants.CMSSiteInitializerFDSNames;
 
+import java.util.Calendar;
+
 import org.osgi.service.component.annotations.Component;
 
 /**
@@ -54,7 +56,11 @@ public class ExpirationDateRangeFDSFilter extends BaseDateRangeFDSFilter {
 
 	@Override
 	public DateFDSFilterItem getMinDateFDSFilterItem() {
-		return new DateFDSFilterItem(0, 0, 0);
+		Calendar calendar = Calendar.getInstance();
+
+		return new DateFDSFilterItem(
+			calendar.get(Calendar.DAY_OF_MONTH),
+			calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.YEAR));
 	}
 
 }
