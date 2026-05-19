1e3cdd8f752e660feeac75df97eefe4f534d4e21
Rubén Pulido <ruben.pulido@liferay.com>
2026-05-19 11:31:25 +0200

    LPD-88748 Use the shared ThumbnailURLReferenceUtil from the site DTOConverters
    Switch MasterPage, DisplayPageTemplate, PageTemplate, and
    UtilityPage DTOConverters to the headless-common-spi
    ThumbnailURLReferenceUtil.

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/MasterPageDTOConverter.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/MasterPageDTOConverter.java
@@ -7,8 +7,8 @@ package com.liferay.headless.admin.site.internal.dto.v1_0.converter;
 
 import com.liferay.headless.admin.site.dto.v1_0.MasterPage;
 import com.liferay.headless.admin.site.internal.dto.v1_0.util.AssetUtil;
-import com.liferay.headless.admin.site.internal.dto.v1_0.util.ThumbnailURLReferenceUtil;
 import com.liferay.headless.admin.user.dto.v1_0.Creator;
+import com.liferay.headless.common.spi.util.ThumbnailURLReferenceUtil;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.User;
