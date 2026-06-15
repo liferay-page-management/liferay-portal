6b0891c27473059eb120d55a965f0307060395d1
Rubén Pulido <ruben.pulido@liferay.com>
2026-06-16 00:08:39 +0200

    LPD-88489 Add the read (GET) ResourceFolder resource methods

--- modules/apps/headless/headless-admin-fragment/headless-admin-fragment-impl/src/main/java/com/liferay/headless/admin/fragment/internal/resource/v1_0/ResourceFolderResourceImpl.java
+++ modules/apps/headless/headless-admin-fragment/headless-admin-fragment-impl/src/main/java/com/liferay/headless/admin/fragment/internal/resource/v1_0/ResourceFolderResourceImpl.java
@@ -5,6 +5,7 @@
 
 package com.liferay.headless.admin.fragment.internal.resource.v1_0;
 
+import com.liferay.document.library.constants.DLFolderSearchConstants;
 import com.liferay.document.library.kernel.model.DLFolder;
 import com.liferay.document.library.kernel.service.DLAppService;
 import com.liferay.document.library.kernel.service.DLFolderLocalService;
@@ -13,19 +14,38 @@ import com.liferay.fragment.service.FragmentCollectionLocalService;
 import com.liferay.fragment.service.FragmentCollectionService;
 import com.liferay.headless.admin.fragment.dto.v1_0.FragmentSet;
 import com.liferay.headless.admin.fragment.dto.v1_0.ResourceFolder;
+import com.liferay.headless.admin.fragment.internal.odata.entity.v1_0.ResourceFolderEntityModel;
 import com.liferay.headless.admin.fragment.internal.resource.v1_0.util.FragmentSetUtil;
 import com.liferay.headless.admin.fragment.internal.resource.v1_0.util.ServiceContextUtil;
 import com.liferay.headless.admin.fragment.internal.util.EnabledUtil;
 import com.liferay.headless.admin.fragment.resource.v1_0.ResourceFolderResource;
 import com.liferay.headless.common.spi.util.GroupUtil;
+import com.liferay.petra.function.UnsafeConsumer;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.lazy.referencing.LazyReferencingThreadLocal;
 import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.kernel.search.filter.TermFilter;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.odata.entity.EntityModel;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.util.SearchUtil;
+
+import jakarta.ws.rs.core.MultivaluedMap;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -39,6 +59,104 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = ResourceFolderResource.class
 )
 public class ResourceFolderResourceImpl extends BaseResourceFolderResourceImpl {
+
+	@Override
+	public EntityModel getEntityModel(MultivaluedMap multivaluedMap) {
+		return _entityModel;
+	}
+
+	@Override
+	public Page<ResourceFolder> getSiteFragmentSetResourceFoldersPage(
+			String siteExternalReferenceCode,
+			String fragmentSetExternalReferenceCode, Pagination pagination)
+		throws Exception {
+
+		EnabledUtil.checkEnabled(contextCompany);
+
+		long groupId = GroupUtil.getGroupId(
+			true, true, contextCompany.getCompanyId(),
+			siteExternalReferenceCode);
+
+		FragmentCollection fragmentCollection =
+			_fragmentCollectionService.
+				getFragmentCollectionByExternalReferenceCode(
+					fragmentSetExternalReferenceCode, groupId);
+
+		long resourcesFolderId = fragmentCollection.getResourcesFolderId(false);
+
+		if (resourcesFolderId <= 0) {
+			return Page.of(Collections.emptyList());
+		}
+
+		return _searchResourceFolders(groupId, pagination, resourcesFolderId);
+	}
+
+	@Override
+	public ResourceFolder getSiteResourceFolder(
+			String siteExternalReferenceCode,
+			String resourceFolderExternalReferenceCode)
+		throws Exception {
+
+		EnabledUtil.checkEnabled(contextCompany);
+
+		Folder folder = _dlAppService.getFolderByExternalReferenceCode(
+			resourceFolderExternalReferenceCode,
+			GroupUtil.getGroupId(
+				true, true, contextCompany.getCompanyId(),
+				siteExternalReferenceCode));
+
+		return _toResourceFolder(
+			_dlFolderLocalService.getDLFolder(folder.getFolderId()));
+	}
+
+	@Override
+	public Page<ResourceFolder> getSiteResourceFolderResourceFoldersPage(
+			String siteExternalReferenceCode,
+			String resourceFolderExternalReferenceCode, Pagination pagination)
+		throws Exception {
+
+		EnabledUtil.checkEnabled(contextCompany);
+
+		long groupId = GroupUtil.getGroupId(
+			true, true, contextCompany.getCompanyId(),
+			siteExternalReferenceCode);
+
+		Folder folder = _dlAppService.getFolderByExternalReferenceCode(
+			resourceFolderExternalReferenceCode, groupId);
+
+		return _searchResourceFolders(
+			groupId, pagination, folder.getFolderId());
+	}
+
+	@Override
+	public Page<ResourceFolder> getSiteResourceFoldersPage(
+			String siteExternalReferenceCode, Filter filter,
+			Pagination pagination)
+		throws Exception {
+
+		EnabledUtil.checkEnabled(contextCompany);
+
+		long groupId = GroupUtil.getGroupId(
+			true, true, contextCompany.getCompanyId(),
+			siteExternalReferenceCode);
+
+		List<Long> resourcesFolderIds = new ArrayList<>();
+
+		for (FragmentCollection fragmentCollection :
+				_fragmentCollectionLocalService.getFragmentCollections(
+					groupId)) {
+
+			long resourcesFolderId = fragmentCollection.getResourcesFolderId(
+				false);
+
+			if (resourcesFolderId > 0) {
+				resourcesFolderIds.add(resourcesFolderId);
+			}
+		}
+
+		return _searchResourceFolders(
+			filter, groupId, pagination, resourcesFolderIds);
+	}
 
 	@Override
 	public ResourceFolder postSiteFragmentSetResourceFolder(
@@ -233,6 +351,79 @@ public class ResourceFolderResourceImpl extends BaseResourceFolderResourceImpl {
 				fragmentSet.getDateModified(), contextUser.getUserId()));
 	}
 
+	private Page<ResourceFolder> _searchResourceFolders(
+			Filter filter, long groupId, Pagination pagination,
+			List<Long> resourcesFolderIds)
+		throws Exception {
+
+		if (resourcesFolderIds.isEmpty()) {
+			return Page.of(Collections.emptyList());
+		}
+
+		return _searchResourceFolders(
+			booleanQuery -> {
+				BooleanFilter booleanFilter =
+					booleanQuery.getPreBooleanFilter();
+
+				BooleanFilter treePathBooleanFilter = new BooleanFilter();
+
+				for (long resourcesFolderId : resourcesFolderIds) {
+					booleanFilter.add(
+						new TermFilter(
+							Field.ENTRY_CLASS_PK,
+							String.valueOf(resourcesFolderId)),
+						BooleanClauseOccur.MUST_NOT);
+					treePathBooleanFilter.add(
+						new TermFilter(
+							Field.TREE_PATH, String.valueOf(resourcesFolderId)),
+						BooleanClauseOccur.SHOULD);
+				}
+
+				booleanFilter.add(
+					treePathBooleanFilter, BooleanClauseOccur.MUST);
+			},
+			filter, groupId, pagination);
+	}
+
+	private Page<ResourceFolder> _searchResourceFolders(
+			long groupId, Pagination pagination, long parentFolderId)
+		throws Exception {
+
+		return _searchResourceFolders(
+			booleanQuery -> {
+				BooleanFilter booleanFilter =
+					booleanQuery.getPreBooleanFilter();
+
+				booleanFilter.add(
+					new TermFilter(
+						Field.FOLDER_ID, String.valueOf(parentFolderId)),
+					BooleanClauseOccur.MUST);
+			},
+			null, groupId, pagination);
+	}
+
+	private Page<ResourceFolder> _searchResourceFolders(
+			UnsafeConsumer<BooleanQuery, Exception> booleanQueryUnsafeConsumer,
+			Filter filter, long groupId, Pagination pagination)
+		throws Exception {
+
+		return SearchUtil.search(
+			Collections.emptyMap(), booleanQueryUnsafeConsumer, filter,
+			DLFolder.class.getName(), null, pagination,
+			queryConfig -> queryConfig.setSelectedFieldNames(
+				Field.ENTRY_CLASS_PK),
+			searchContext -> {
+				searchContext.setAttribute(
+					DLFolderSearchConstants.SHOW_HIDDEN, Boolean.TRUE);
+				searchContext.setCompanyId(contextCompany.getCompanyId());
+				searchContext.setGroupIds(new long[] {groupId});
+			},
+			null,
+			document -> _toResourceFolder(
+				_dlFolderLocalService.getDLFolder(
+					GetterUtil.getLong(document.get(Field.ENTRY_CLASS_PK)))));
+	}
+
 	private ResourceFolder _toResourceFolder(DLFolder dlFolder)
 		throws Exception {
 
@@ -244,6 +435,9 @@ public class ResourceFolderResourceImpl extends BaseResourceFolderResourceImpl {
 				contextUser),
 			dlFolder);
 	}
+
+	private static final EntityModel _entityModel =
+		new ResourceFolderEntityModel();
 
 	@Reference
 	private DLAppService _dlAppService;
